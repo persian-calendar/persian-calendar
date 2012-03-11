e0032a0a0b4f102a3abf1c609e339baa0eaf0fac
Ebrahim Byagowi <ebraminio@gmail.com>
2012-03-11 05:21:30 +0330

    + Animation on sliding

--- src/com/byagowi/persiancalendar/PersianCalendarActivity.java
+++ src/com/byagowi/persiancalendar/PersianCalendarActivity.java
@@ -5,14 +5,20 @@ import calendar.DateConverter;
 import calendar.DayOutOfRangeException;
 import calendar.PersianDate;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 /**
  * Program activity for android.
@@ -24,40 +30,48 @@ public class PersianCalendarActivity extends Activity {
 
 	int currentPersianYear = 0;
 	int currentPersianMonth = 0;
+	int currentCalendarIndex = 0;
 	TextView currentMonthTextView = null;
 	PersianDate nowDate = null;
-	
+	ViewFlipper calendarPlaceholder = null;
+	Animation slideInLeftAnimation = null;
+	Animation slideOutLeftAnimation = null;
+	Animation slideInRightAnimation = null;
+	Animation slideOutRightAnimation = null;
+	Animation fadeOutAnimation = null;
 
-    private static final int SWIPE_MIN_DISTANCE = 120;
-    private static final int SWIPE_MAX_OFF_PATH = 250;
-    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-    
-    SimpleOnGestureListener simpleOnGestureListener = new SimpleOnGestureListener() {
-    	@Override
-    	public boolean onDown(MotionEvent e) {
-    	    return true;        
-    	}
-    	
+	private static final int SWIPE_MIN_DISTANCE = 120;
+	private static final int SWIPE_MAX_OFF_PATH = 250;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+	SimpleOnGestureListener simpleOnGestureListener = new SimpleOnGestureListener() {
 		@Override
-	    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-	        try {
-	            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-	                return false;
-	            // right to left swipe
-	            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-	            	nextMonth(null);
-	            }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+		public boolean onDown(MotionEvent e) {
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			try {
+				if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+					return false;
+				// right to left swipe
+				if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
 					previousMonth(null);
-	            }
-	        } catch (Exception e) {
-	            // nothing
-	        }
-	        return super.onFling(e1, e2, velocityX, velocityY);
-	    }
+				} else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+						&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+					nextMonth(null);
+				}
+			} catch (Exception e) {
+				// nothing
+			}
+			return super.onFling(e1, e2, velocityX, velocityY);
+		}
 	};
-    
+
 	// ViewFlipper viewFlipper = null;
-	View.OnTouchListener gestureListener;
 	GestureDetector gestureDetector;
 
 	@Override
@@ -96,21 +110,37 @@ public class PersianCalendarActivity extends Activity {
 
 		setMonth();
 
-		showCalendarOfMonthYear(currentPersianYear, currentPersianMonth);
-		
-	    gestureDetector = new GestureDetector(simpleOnGestureListener);
-		gestureListener = new View.OnTouchListener() {
+		gestureDetector = new GestureDetector(simpleOnGestureListener);
+		calendarPlaceholder = (ViewFlipper) findViewById(R.id.calendar_placeholder);
+		calendarPlaceholder.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				return gestureDetector.onTouchEvent(event);
 			}
-		};
-		findViewById(R.id.calendarTable).setOnTouchListener(gestureListener);
-	}	
+		});
 
-	private void showCalendarOfMonthYear(int year, int month) {
+		slideInLeftAnimation = AnimationUtils.loadAnimation(this,
+				R.anim.slide_in_left);
+		slideOutLeftAnimation = AnimationUtils.loadAnimation(this,
+				R.anim.slide_out_left);
+		slideInRightAnimation = AnimationUtils.loadAnimation(this,
+				R.anim.slide_in_right);
+		slideOutRightAnimation = AnimationUtils.loadAnimation(this,
+				R.anim.slide_out_right);
+		fadeOutAnimation = AnimationUtils.loadAnimation(this,
+				R.anim.fade_out);
+		
+		
+		View calendar = getLayoutInflater().inflate(R.layout.calendar_table,
+				null);
+		showCalendarOfMonthYear(currentPersianYear, currentPersianMonth,
+				calendar);
+		calendarPlaceholder.addView(calendar, currentCalendarIndex);
+	}
 
-		calendarCleanUp();
+	private void showCalendarOfMonthYear(int year, int month, View calendar) {
+
+		calendarCleanUp(calendar);
 
 		PersianDate persian = new PersianDate(year, month, 1);
 		persian.setMonth(month);
@@ -121,8 +151,9 @@ public class PersianCalendarActivity extends Activity {
 			try {
 				persian.setDayOfMonth(i);
 
-				TextView textView = getTextViewFromNameId(String.format(
-						"calendarCell%d%d", weekOfMonth, dayOfWeek + 1));
+				TextView textView = getTextViewInView(String.format(
+						"calendarCell%d%d", weekOfMonth, dayOfWeek + 1),
+						calendar);
 
 				textView.setText(PersianUtils.getPersianNumber(i));
 
@@ -147,54 +178,61 @@ public class PersianCalendarActivity extends Activity {
 
 		currentMonthTextView.setText(persian.getMonthName() + " "
 				+ PersianUtils.getPersianNumber(persian.getYear()));
+
 	}
 
-	private void calendarCleanUp() {
+	private void calendarCleanUp(View calendar) {
 		for (int i = 1; i < 7; i++) {
 			for (int j = 1; j <= 7; j++) {
-				getTextViewFromNameId(String.format("calendarCell%d%d", i, j))
-						.setText("");
+				getTextViewInView(String.format("calendarCell%d%d", i, j),
+						calendar).setText("");
 			}
 		}
 	}
 
-	private TextView getTextViewFromNameId(String name) {
-		TextView textView = null;
+	private TextView getTextViewInView(String name, View view) {
 		try {
-			textView = (TextView) findViewById(R.id.class.getField(name)
+			return (TextView) view.findViewById(R.id.class.getField(name)
 					.getInt(null)); // null for static field of classes
 		} catch (Exception e) {
 			Log.e("com.byagowi.persiancalendar", "Error on retriving cell: "
 					+ e.getMessage());
+			return null;
 		}
-		return textView;
 	}
 
 	public void nextMonth(View v) {
-		nextMonth();
-		showCalendarOfMonthYear(currentPersianYear, currentPersianMonth);
-	}
-
-	public void previousMonth(View v) {
-		previousMonth();
-		showCalendarOfMonthYear(currentPersianYear, currentPersianMonth);
-	}
-
-	private void nextMonth() {
-		
 		currentPersianMonth++;
 		if (currentPersianMonth == 13) {
 			currentPersianMonth = 1;
 			currentPersianYear++;
 		}
+		currentCalendarIndex++;
+		setViewOnCalnedarPlaceholder();
+		calendarPlaceholder.setInAnimation(slideInLeftAnimation);
+		calendarPlaceholder.setOutAnimation(fadeOutAnimation);
+		calendarPlaceholder.showNext();
 	}
 
-	private void previousMonth() {
+	public void previousMonth(View v) {
 		currentPersianMonth--;
 		if (currentPersianMonth == 0) {
 			currentPersianMonth = 12;
 			currentPersianYear--;
 		}
+		currentCalendarIndex++;
+		setViewOnCalnedarPlaceholder();
+		calendarPlaceholder.setInAnimation(slideInRightAnimation);
+		calendarPlaceholder.setOutAnimation(fadeOutAnimation);
+		calendarPlaceholder.showNext();
+	}
+
+	public void setViewOnCalnedarPlaceholder() {
+		View calendar = getLayoutInflater().inflate(R.layout.calendar_table,
+				null);
+		showCalendarOfMonthYear(currentPersianYear, currentPersianMonth,
+				calendar);
+		calendarPlaceholder.addView(calendar, currentCalendarIndex);
 	}
 
 	private void setMonth() {
