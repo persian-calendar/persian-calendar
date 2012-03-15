2c7510ee3df417e5ca045d34fef751c850571a1d
Ebrahim Byagowi <ebraminio@gmail.com>
2012-03-15 15:54:18 +0330

    I am a bit lazy :D v1.9 :)
    Former-commit-id: adaf393066afaea833b2824f4f4a4e8ba097e13f [formerly d7c5dea45434c4598e8584671001a627a69e7245]
    Former-commit-id: 65ab9cb3e38427332ebd335640227f686309be3e

--- src/com/byagowi/persiancalendar/PersianCalendarSplashScreen.java
+++ src/com/byagowi/persiancalendar/PersianCalendarSplashScreen.java
@@ -2,8 +2,12 @@ package com.byagowi.persiancalendar;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.TextView;
 
 // borrowed from http://www.codeproject.com/Articles/113831/An-Advanced-Splash-Screen-for-Android-App :)
 public class PersianCalendarSplashScreen extends Activity {
@@ -18,8 +22,19 @@ public class PersianCalendarSplashScreen extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 		// Splash screen view
 		setContentView(R.layout.splash);
+
+		try {
+			String versionTitle = "نسخهٔ "
+					+ PersianUtils.getPersianNumber(this.getPackageManager().getPackageInfo(
+							this.getPackageName(), 0).versionName);
+			((TextView) findViewById(R.id.version)).setText(versionTitle);
+		} catch (Exception e) {
+			// okay, okay, was not important, go go :D
+		}
 
 		// The thread to wait for splash screen events
 		mSplashThread = new Thread() {
