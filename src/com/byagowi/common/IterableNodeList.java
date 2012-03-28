2cf6d270b4c85a2f22e875316c85b6361be2f738
Ebrahim Byagowi <ebraminio@gmail.com>
2012-03-28 15:51:01 +0430

    Implementing "Python"ic `range`s (`xrange`) for reducing logical errors. Implementing IterableNodeList, when java will add this?!?
    Former-commit-id: 2d43d9b3abac946387aa0fe760740e655ae53400 [formerly 07dd00f01b7e82677ad33167ab129216a5fcb0f1]
    Former-commit-id: f504acf1f8b1d20d3f2fef721edd18f954c1de86

---
+++ src/com/byagowi/common/IterableNodeList.java
@@ -0,0 +1,56 @@
+/*
+ * March 2012
+ *
+ * In place of a legal notice, here is a blessing:
+ *
+ *    May you do good and not evil.
+ *    May you find forgiveness for yourself and forgive others.
+ *    May you share freely, never taking more than you give.
+ *
+ */
+package com.byagowi.common;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+ 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Iterable NodeList.
+ * 
+ * @author ebraminio
+ * 
+ */
+public class IterableNodeList implements Iterable<Node> {
+	private final NodeList nodeList;
+
+	public IterableNodeList(NodeList nodeList) {
+		this.nodeList = nodeList;
+	}
+
+	/** {@inheritDoc} */
+	public Iterator<Node> iterator() {
+		return new Iterator<Node>() {
+			private int index = 0;
+			
+			/** {@inheritDoc} */
+			public boolean hasNext() {
+				return index < nodeList.getLength();
+			}
+
+			/** {@inheritDoc} */
+			public Node next() {
+				if(!hasNext()) {
+			         throw new NoSuchElementException();
+			      }
+			      return nodeList.item(index++);
+			}
+
+			/** {@inheritDoc} */
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}
