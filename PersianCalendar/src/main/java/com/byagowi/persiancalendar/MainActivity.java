69c087461a62f9bb2869b73e92de9ae4057950d0
Behdad Abedi <behdad.222@gmail.com>
2015-10-15 14:26:49 +0330

    remove calendar from main activity

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainActivity.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainActivity.java
@@ -1,38 +1,20 @@
 package com.byagowi.persiancalendar;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.byagowi.persiancalendar.locale.CalendarStrings;
 import com.byagowi.persiancalendar.Adapter.DrawerAdapter;
 import com.byagowi.persiancalendar.Interface.ClickListener;
-import com.byagowi.persiancalendar.view.MonthFragment;
-
-import calendar.CivilDate;
-import calendar.DateConverter;
-import calendar.PersianDate;
 
 /**
  * Program activity for android
@@ -40,48 +22,19 @@ import calendar.PersianDate;
  * @author ebraminio
  */
 public class MainActivity extends AppCompatActivity implements ClickListener {
-    private static final String TAG = "MainActivity";
-    // I know, it is ugly, but user will not notify this and this will not have
-    // a memory problem
-    private static final int MONTHS_LIMIT = 1200;
     public Utils utils = Utils.getInstance();
-    private ViewPager viewPager;
-    private TextView calendarInfo;
-    private Button resetButton;
-    private PrayTimeActivityHelper prayTimeActivityHelper;
 
-    private Toolbar toolbar;
-    private RecyclerView navigation;
-    private DrawerAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        utils.setTheme(this);
-        utils.loadLanguageFromSettings(this);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
 
         startService(new Intent(this, ApplicationService.class));
 
-        boolean removeTitle = true;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            if (!hasPermanentMenuKey()) {
-                removeTitle = false;
-            }
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB
-                && Build.VERSION.SDK_INT <= Build.VERSION_CODES.HONEYCOMB_MR2) {
-//             if a tablet version is installed
-            removeTitle = false;
-        }
-        if (removeTitle) {
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-        }
         setContentView(R.layout.calendar);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -89,17 +42,16 @@ public class MainActivity extends AppCompatActivity implements ClickListener {
             w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
-
-        navigation = (RecyclerView) findViewById(R.id.navigation_view);
+        RecyclerView navigation = (RecyclerView) findViewById(R.id.navigation_view);
         navigation.setHasFixedSize(true);
-        adapter = new DrawerAdapter(this, this);
+        DrawerAdapter adapter = new DrawerAdapter(this, this);
         navigation.setAdapter(adapter);
 
-        layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         navigation.setLayoutManager(layoutManager);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer){
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
             @Override
             public void onDrawerOpened(View drawerView) {
@@ -112,181 +64,35 @@ public class MainActivity extends AppCompatActivity implements ClickListener {
             }
         };
 
-
         drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
-
-
-        calendarInfo = (TextView) findViewById(R.id.calendar_info);
-
-        // Pray Time
-        prayTimeActivityHelper = new PrayTimeActivityHelper(this);
-        prayTimeActivityHelper.fillPrayTime();
-
-        // Load Holidays
-        utils.loadHolidays(getResources().openRawResource(R.raw.holidays));
-
-        // Reset button
-        resetButton = (Button) findViewById(R.id.reset_button);
-        resetButton.setText(utils.getString(CalendarStrings.TODAY));
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                bringTodayYearMonth();
-                setFocusedDay(Utils.getToday());
-            }
-        });
-        resetButton.setTypeface(Typeface.createFromAsset(this.getAssets(),
-                "fonts/NotoNaskhArabic-Regular.ttf"));
-
-        // Initializing the viewPager
-        viewPager = (ViewPager) findViewById(R.id.calendar_pager);
-        viewPager.setAdapter(createCalendarAdaptor());
-        viewPager.setCurrentItem(MONTHS_LIMIT / 2);
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageSelected(int arg0) {
-                updateResetButtonState();
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-            }
-
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-            }
-        });
-
-        fillCalendarInfo(Utils.getToday());
-        utils.setAthanRepeater(this);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        fillCalendarInfo(Utils.getToday());
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int itemId = item.getItemId();
+//        if (itemId == R.id.menu_dateconverter) {
+//            startActivity(new Intent(this, ConverterActivity.class));
+//        } else if (itemId == R.id.menu_compass) {
+//            startActivity(new Intent(this, CompassActivity.class));
+//        } else if (itemId == R.id.menu_settings) {
+//            startActivityForResult(
+//                    new Intent(this, ApplicationPreference.class), 0);
+//        } else if (itemId == R.id.menu_about) {
+//            startActivity(new Intent(this, AboutActivity.class));
+//        } else if (itemId == R.id.menu_exit) {
+//            finish();
+//        }
+//        return false;
+//    }
 
-    private void updateResetButtonState() {
-        if (viewPager.getCurrentItem() == MONTHS_LIMIT / 2) {
-            resetButton.setVisibility(View.GONE);
-            fillCalendarInfo(Utils.getToday());
-        } else {
-            resetButton.setVisibility(View.VISIBLE);
-            clearInfo();
-        }
-    }
-
-    private void clearInfo() {
-        calendarInfo.setText("");
-        prayTimeActivityHelper.clearInfo();
-    }
-
-    private void bringTodayYearMonth() {
-        if (viewPager.getCurrentItem() != MONTHS_LIMIT / 2) {
-            viewPager.setCurrentItem(MONTHS_LIMIT / 2);
-            fillCalendarInfo(Utils.getToday());
-        }
-    }
-
-    public void setFocusedDay(PersianDate persianDate) {
-        fillCalendarInfo(persianDate);
-    }
-
-    private PagerAdapter createCalendarAdaptor() {
-        return new FragmentPagerAdapter(
-                getSupportFragmentManager()) {
-            @Override
-            public int getCount() {
-                return MONTHS_LIMIT;
-            }
-
-            @Override
-            public Fragment getItem(int position) {
-                MonthFragment fragment = new MonthFragment();
-                Bundle args = new Bundle();
-                args.putInt("offset", position - MONTHS_LIMIT / 2);
-                fragment.setArguments(args);
-                return fragment;
-            }
-        };
-    }
-
-    private boolean isToday(CivilDate civilDate) {
-        CivilDate today = new CivilDate();
-        return today.getYear() == civilDate.getYear()
-                && today.getMonth() == civilDate.getMonth()
-                && today.getDayOfMonth() == civilDate.getDayOfMonth();
-    }
-
-    private void fillCalendarInfo(PersianDate persianDate) {
-        CivilDate civilDate = DateConverter.persianToCivil(persianDate);
-        char[] digits = utils.preferredDigits(this);
-        utils.prepareTextView(calendarInfo);
-        StringBuilder sb = new StringBuilder();
-
-        if (isToday(civilDate)) {
-            sb.append(utils.getString(CalendarStrings.TODAY)).append(":\n");
-            resetButton.setVisibility(View.GONE);
-        } else {
-            resetButton.setVisibility(View.VISIBLE);
-        }
-
-        sb.append(utils.getWeekDayName(persianDate)).append(Utils.PERSIAN_COMMA)
-                .append(" ")
-                .append(utils.dateToString(persianDate, digits))
-                .append("\n\n")
-                .append(utils.getString(CalendarStrings.EQUALS_WITH))
-                .append(":\n")
-                .append(utils.dateToString(civilDate, digits))
-                .append("\n")
-                .append(utils.dateToString(DateConverter.civilToIslamic(civilDate), digits))
-                .append("\n");
-        calendarInfo.setText(Utils.textShaper(sb.toString()));
-
-        prayTimeActivityHelper.setDate(civilDate.getYear(),
-                civilDate.getMonth() - 1, civilDate.getDayOfMonth());
-        prayTimeActivityHelper.fillPrayTime();
-    }
-
-
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private boolean hasPermanentMenuKey() {
-        return ViewConfiguration.get(this).hasPermanentMenuKey();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.menu_dateconverter) {
-            startActivity(new Intent(this, ConverterActivity.class));
-        } else if (itemId == R.id.menu_compass) {
-            startActivity(new Intent(this, CompassActivity.class));
-        } else if (itemId == R.id.menu_settings) {
-            startActivityForResult(
-                    new Intent(this, ApplicationPreference.class), 0);
-        } else if (itemId == R.id.menu_about) {
-            startActivity(new Intent(this, AboutActivity.class));
-        } else if (itemId == R.id.menu_exit) {
-            finish();
-        }
-        return false;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//    @Override  // TODO: 10/15/15  this cod for reload app after setting
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //         Restart activity
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
-    }
+//        Intent intent = getIntent();
+//        finish();
+//        startActivity(intent);
+//    }
 
     @Override
     public void onClickItem(View v, int position) {
