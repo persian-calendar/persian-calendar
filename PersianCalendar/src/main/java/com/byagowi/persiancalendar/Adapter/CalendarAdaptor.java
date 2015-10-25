3aa87f9dc785d4765fb2c7a6c04b98e07790d39c
Behdad Abedi <behdad.222@gmail.com>
2015-10-25 17:24:01 +0330

    add view pager for new design

---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/Adapter/CalendarAdaptor.java
@@ -0,0 +1,32 @@
+package com.byagowi.persiancalendar.Adapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.byagowi.persiancalendar.view.Fragment.CalendarNewFragment;
+import com.byagowi.persiancalendar.view.MonthFragment;
+
+/**
+ * Created by behdad on 10/25/15.
+ */
+public class CalendarAdaptor extends FragmentPagerAdapter {
+    public CalendarAdaptor(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        MonthFragment fragment = new MonthFragment();
+        Bundle args = new Bundle();
+        args.putInt("offset", position - CalendarNewFragment.MONTHS_LIMIT / 2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return CalendarNewFragment.MONTHS_LIMIT;
+    }
+}
--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarNewFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarNewFragment.java
@@ -4,22 +4,46 @@ package com.byagowi.persiancalendar.view.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.byagowi.persiancalendar.Adapter.CalendarAdaptor;
 import com.byagowi.persiancalendar.R;
 
 /**
  * Created by behdad on 10/25/15.
  */
-public class CalendarNewFragment extends Fragment {
+public class CalendarNewFragment extends Fragment implements ViewPager.OnPageChangeListener {
+    public static final int MONTHS_LIMIT = 1200;
+    private ViewPager viewPager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_calendar, container, false);
 
+        viewPager = (ViewPager) view.findViewById(R.id.calendar_pager);
+        viewPager.setAdapter(new CalendarAdaptor(getActivity().getSupportFragmentManager()));
+        viewPager.setCurrentItem(MONTHS_LIMIT / 2);
+        viewPager.addOnPageChangeListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
