cc5c7e2a566ba84a583c2f9440a84b0229a0d188
Behdad Abedi <behdad.222@gmail.com>
2015-11-02 01:56:09 +0330

    add view day

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/Adapter/DrawerAdapter.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/Adapter/DrawerAdapter.java
@@ -1,6 +1,5 @@
 package com.byagowi.persiancalendar.Adapter;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
