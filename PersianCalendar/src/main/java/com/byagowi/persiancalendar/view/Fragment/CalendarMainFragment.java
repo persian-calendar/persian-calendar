da8b58e3d02e20c772578563f033134f7efc80e9
Behdad Abedi <behdad.222@gmail.com>
2015-11-02 14:38:26 +0330

    expand day info

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarMainFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarMainFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.byagowi.persiancalendar.Adapter.CalendarAdapter;
@@ -18,10 +19,11 @@ import calendar.CivilDate;
 import calendar.DateConverter;
 import calendar.PersianDate;
 
-public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageChangeListener {
+public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageChangeListener, View.OnClickListener {
     public static final int MONTHS_LIMIT = 1200;
     private ViewPager viewPager;
     private final Utils utils = Utils.getInstance();
+    private RelativeLayout infoDay;
     private TextView weekDayName;
     private TextView shamsiDate;
     private TextView miladiDate;
@@ -32,12 +34,15 @@ public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageC
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_calendar, container, false);
 
+        infoDay = (RelativeLayout) view.findViewById(R.id.info_day);
         weekDayName = (TextView) view.findViewById(R.id.week_day_name);
         shamsiDate = (TextView) view.findViewById(R.id.shamsi_date);
         miladiDate = (TextView) view.findViewById(R.id.miladi_date);
         ghamariDate = (TextView) view.findViewById(R.id.ghamari_date);
 
         utils.loadHolidays(getResources().openRawResource(R.raw.holidays));
+
+        infoDay.setOnClickListener(this);
 
         viewPager = (ViewPager) view.findViewById(R.id.calendar_pager);
         viewPager.setAdapter(new CalendarAdapter(getActivity().getSupportFragmentManager(), this));
@@ -74,5 +79,11 @@ public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageC
         shamsiDate.setText(utils.dateToString(persianDate, digits));
         miladiDate.setText(utils.dateToString(civilDate, digits));
         ghamariDate.setText(utils.dateToString(DateConverter.civilToIslamic(civilDate), digits));
+    }
+
+    @Override
+    public void onClick(View v) {
+        miladiDate.setVisibility(View.VISIBLE);
+        ghamariDate.setVisibility(View.VISIBLE);
     }
 }
