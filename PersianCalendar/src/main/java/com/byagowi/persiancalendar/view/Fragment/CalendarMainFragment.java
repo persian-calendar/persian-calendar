09f9a9b3657657d65da992615545692a31753428
Behdad Abedi <behdad.222@gmail.com>
2015-11-02 14:28:54 +0330

    show info day

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarMainFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/Fragment/CalendarMainFragment.java
@@ -7,20 +7,35 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.byagowi.persiancalendar.Adapter.CalendarAdapter;
 import com.byagowi.persiancalendar.R;
 import com.byagowi.persiancalendar.Utils;
+import com.byagowi.persiancalendar.locale.CalendarStrings;
+
+import calendar.CivilDate;
+import calendar.DateConverter;
+import calendar.PersianDate;
 
 public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageChangeListener {
     public static final int MONTHS_LIMIT = 1200;
     private ViewPager viewPager;
     private final Utils utils = Utils.getInstance();
+    private TextView weekDayName;
+    private TextView shamsiDate;
+    private TextView miladiDate;
+    private TextView ghamariDate;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_calendar, container, false);
+
+        weekDayName = (TextView) view.findViewById(R.id.week_day_name);
+        shamsiDate = (TextView) view.findViewById(R.id.shamsi_date);
+        miladiDate = (TextView) view.findViewById(R.id.miladi_date);
+        ghamariDate = (TextView) view.findViewById(R.id.ghamari_date);
 
         utils.loadHolidays(getResources().openRawResource(R.raw.holidays));
 
@@ -49,5 +64,15 @@ public class CalendarMainFragment extends Fragment  implements ViewPager.OnPageC
 
     public void changeMonth(int position) {
         viewPager.setCurrentItem(viewPager.getCurrentItem() + position, true);
+    }
+
+    public void selectDay(PersianDate persianDate) {
+        CivilDate civilDate = DateConverter.persianToCivil(persianDate);
+        char[] digits = utils.preferredDigits(getContext());
+
+        weekDayName.setText(utils.getWeekDayName(persianDate));
+        shamsiDate.setText(utils.dateToString(persianDate, digits));
+        miladiDate.setText(utils.dateToString(civilDate, digits));
+        ghamariDate.setText(utils.dateToString(DateConverter.civilToIslamic(civilDate), digits));
     }
 }
