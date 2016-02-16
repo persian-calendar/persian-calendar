47e55b9e2697718feb02c80fb680a001d476d618
Behdad Abedi <behdad.222@gmail.com>
2016-02-16 14:26:14 +0330

    location dialog -> recyclerview

---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/adapter/LocationAdapter.java
@@ -0,0 +1,74 @@
+package com.byagowi.persiancalendar.adapter;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.byagowi.persiancalendar.R;
+import com.byagowi.persiancalendar.Utils;
+import com.byagowi.persiancalendar.view.dialog.LocationPreferenceDialog;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHolder> {
+    private String locale;
+    private LocationPreferenceDialog locationPreferenceDialog;
+    List<Utils.City> cities;
+    private final Utils utils = Utils.getInstance();
+
+    public LocationAdapter(LocationPreferenceDialog locationPreferenceDialog) {
+        this.locationPreferenceDialog = locationPreferenceDialog;
+        cities = Arrays.asList(utils.getAllCities(locationPreferenceDialog.getContext()));
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(locationPreferenceDialog.getContext());
+        this.locale = prefs.getString("AppLanguage", "fa");
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView country;
+        private TextView city;
+
+        public ViewHolder(View itemView) {
+            super(itemView);;
+            itemView.setOnClickListener(this);
+            city = (TextView) itemView.findViewById(R.id.text1);
+            country = (TextView) itemView.findViewById(R.id.text2);
+        }
+
+        @Override
+        public void onClick(View view) {
+            locationPreferenceDialog.selectItem(getAdapterPosition());
+        }
+    }
+
+    @Override
+    public LocationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_city_name, parent, false);
+
+        return new ViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        utils.prepareTextView(locationPreferenceDialog.getContext(), holder.city);
+        holder.city.setText(utils.textShaper(locale.equals("en")
+                ? cities.get(position).en
+                : cities.get(position).fa));
+
+        utils.prepareTextView(locationPreferenceDialog.getContext(), holder.country);
+        holder.country.setText(utils.textShaper(locale.equals("en")
+                ? cities.get(position).en
+                : cities.get(position).fa));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cities.size();
+    }
+}
--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/LocationPreferenceDialog.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/LocationPreferenceDialog.java
@@ -10,6 +10,9 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceDialogFragmentCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,8 @@ import android.widget.TextView;
 
 import com.byagowi.persiancalendar.R;
 import com.byagowi.persiancalendar.Utils;
+import com.byagowi.persiancalendar.adapter.LocationAdapter;
+import com.byagowi.persiancalendar.adapter.MonthAdapter;
 import com.byagowi.persiancalendar.view.fragment.ApplicationPreferenceFragment;
 
 import java.util.Arrays;
@@ -58,25 +63,13 @@ public class LocationPreferenceDialog extends PreferenceDialogFragmentCompat {
         super.onPrepareDialogBuilder(builder);
         LocationPreference preference = getPreference();
 
-
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View view = inflater.inflate(R.layout.preference_location, (ViewGroup) getView(), false);
-        preference.listLocations = (ListView) view.findViewById(R.id.list);
 
-        final CityNameAdapter locationsAdapter = new CityNameAdapter(getContext(), R.layout.list_item_city_name);
-        preference.listLocations.setAdapter(locationsAdapter);
-        preference.listLocations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                // fish fish
-            }
-        });
-        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialogInterface) {
-                // fish fish
-            }
-        });
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new LocationAdapter(this));
 
         builder.setPositiveButton("", null);
         builder.setNegativeButton("", null);
@@ -95,75 +88,79 @@ public class LocationPreferenceDialog extends PreferenceDialogFragmentCompat {
         getPreference().close(true);
     }
 
-    private class CityNameAdapter extends ArrayAdapter {
+    public void selectItem(int position) {
 
-        private Utils utils;
-
-        private Map<String, Utils.City> cityMap = new HashMap<>();
-        List<Utils.City> cities;
-        private int spinnerResource;
-        Context context;
-        String locale;
-        LayoutInflater inflater;
-
-        public CityNameAdapter(Context context, int resource) {
-            super(context, resource);
-            utils = Utils.getInstance();
-            cities = Arrays.asList(utils.getAllCities(context));
-            spinnerResource = resource;
-            this.context = context;
-            this.inflater = (LayoutInflater)
-                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-            this.locale = prefs.getString("AppLanguage", "fa");
-        }
-
-        @Override
-        public int getCount() {
-            return cities.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return cities.get(position);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if (convertView == null) {
-                convertView = inflater.inflate(spinnerResource, null);
-
-                holder = new ViewHolder();
-                holder.city = (TextView) convertView.findViewById(R.id.text1);
-                holder.country = (TextView) convertView.findViewById(R.id.text2);
-
-                convertView.setTag(holder);
-
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.city.setText(locale.equals("en")
-                    ? cities.get(position).en
-                    : cities.get(position).fa);
-
-            utils.prepareShapeTextView(context, holder.city);
-
-            holder.country.setText(locale.equals("en")
-                    ? cities.get(position).countryEn
-                    : cities.get(position).countryFa);
-            utils.prepareShapeTextView(context, holder.country);
-
-
-            return convertView;
-        }
     }
 
-    static class ViewHolder {
-        TextView country;
-        TextView city;
-    }
+//    private class CityNameAdapter extends ArrayAdapter {
+//
+//        private Utils utils;
+//
+//        private Map<String, Utils.City> cityMap = new HashMap<>();
+//        List<Utils.City> cities;
+//        private int spinnerResource;
+//        Context context;
+//        String locale;
+//        LayoutInflater inflater;
+//
+//        public CityNameAdapter(Context context, int resource) {
+//            super(context, resource);
+//            utils = Utils.getInstance();
+//            cities = Arrays.asList(utils.getAllCities(context));
+//            spinnerResource = resource;
+//            this.context = context;
+//            this.inflater = (LayoutInflater)
+//                    context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+//            this.locale = prefs.getString("AppLanguage", "fa");
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return cities.size();
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return cities.get(position);
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            ViewHolder holder;
+//
+//            if (convertView == null) {
+//                convertView = inflater.inflate(spinnerResource, null);
+//
+//                holder = new ViewHolder();
+//                holder.city = (TextView) convertView.findViewById(R.id.text1);
+//                holder.country = (TextView) convertView.findViewById(R.id.text2);
+//
+//                convertView.setTag(holder);
+//
+//            } else {
+//                holder = (ViewHolder) convertView.getTag();
+//            }
+//
+//            holder.city.setText(locale.equals("en")
+//                    ? cities.get(position).en
+//                    : cities.get(position).fa);
+//
+//            utils.prepareShapeTextView(context, holder.city);
+//
+//            holder.country.setText(locale.equals("en")
+//                    ? cities.get(position).countryEn
+//                    : cities.get(position).countryFa);
+//            utils.prepareShapeTextView(context, holder.country);
+//
+//
+//            return convertView;
+//        }
+//    }
+//
+//    static class ViewHolder {
+//        TextView country;
+//        TextView city;
+//    }
 }
