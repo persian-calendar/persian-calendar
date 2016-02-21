9c28803ee5fbfb9aa92ef92b67914375dd9bce10
Ebrahim Byagowi <ebrahim@gnu.org>
2016-02-21 17:23:19 +0330

    Allow dot insertion after its removal

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/AthanNumericDialog.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/AthanNumericDialog.java
@@ -24,9 +24,10 @@ public class AthanNumericDialog extends EditTextPreferenceDialogFragmentCompat {
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
         EditText editText = (EditText) view.findViewById(android.R.id.edit);
-        editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+        editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED |
+                InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
-        // on platforms supporting direction
+        // on platforms supporting direction as LTR direction is more handy on editing numbers
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
             editText.setTextDirection(View.TEXT_DIRECTION_LTR);
             editText.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
