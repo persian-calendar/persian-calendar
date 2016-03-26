fac35a34b4f41b0d6c10775d02cbc46473d15041
Ebrahim Byagowi <ebrahim@gnu.org>
2016-03-27 03:28:09 +0430

    Use generics on ShapedArrayAdapter to resolve lint complain

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/adapter/ShapedArrayAdapter.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/adapter/ShapedArrayAdapter.java
@@ -13,50 +13,30 @@ import java.util.List;
 /**
  * Created by ebraminio on 2/17/16.
  */
-public class ShapedArrayAdapter extends ArrayAdapter {
+public class ShapedArrayAdapter<T> extends ArrayAdapter {
     private Utils utils;
-
-    public ShapedArrayAdapter(Context context, int resource) {
-        super(context, resource);
-        utils = Utils.getInstance(context);
-    }
 
     public ShapedArrayAdapter(Context context, int resource, List objects) {
         super(context, resource, objects);
         utils = Utils.getInstance(context);
     }
 
-    public ShapedArrayAdapter(Context context, int resource, Object[] objects) {
+    public ShapedArrayAdapter(Context context, int resource, T[] objects) {
         super(context, resource, objects);
-        utils = Utils.getInstance(context);
-    }
-
-    public ShapedArrayAdapter(Context context, int resource, int textViewResourceId) {
-        super(context, resource, textViewResourceId);
-        utils = Utils.getInstance(context);
-    }
-
-    public ShapedArrayAdapter(Context context, int resource, int textViewResourceId, List objects) {
-        super(context, resource, textViewResourceId, objects);
-        utils = Utils.getInstance(context);
-    }
-
-    public ShapedArrayAdapter(Context context, int resource, int textViewResourceId, Object[] objects) {
-        super(context, resource, textViewResourceId, objects);
         utils = Utils.getInstance(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = super.getView(position, convertView, parent);
-        if (view instanceof TextView) utils.setFontShapeAndGravity((TextView) view);
+        if (view instanceof TextView) utils.setFontAndShape((TextView) view);
         return view;
     }
 
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         View view = super.getDropDownView(position, convertView, parent);
-        if (view instanceof TextView) utils.setFontShapeAndGravity((TextView) view);
+        if (view instanceof TextView) utils.setFontAndShape((TextView) view);
         return view;
     }
 }
