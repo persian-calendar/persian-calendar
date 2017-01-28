47e967c3aee993bad583cdfe8c7610e87ada3ec1
Ebrahim Byagowi <ebrahim@gnu.org>
2017-01-28 18:23:53 +0330

    v5.2.1

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/SelectDayDialog.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/SelectDayDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
