1cf32803897ff301d59a1d8ffe70686166ce4ae3
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2018-06-12 04:03:09 +0430

    - utils from ShapedArrayAdapter

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/adapter/ShapedArrayAdapter.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/adapter/ShapedArrayAdapter.java
@@ -8,34 +8,33 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.byagowi.persiancalendar.R;
-import com.byagowi.persiancalendar.util.Utils;
 
 /**
  * Created by ebraminio on 2/17/16.
  */
 public class ShapedArrayAdapter<String> extends ArrayAdapter<String> {
-    private Utils utils;
 
     // preferred drop down list item padding, used for spinners and such
     int padding;
 
     public ShapedArrayAdapter(Context context, int resource, String[] objects) {
         super(context, resource, objects);
-        utils = Utils.getInstance(context);
-        padding = (int)getContext().getResources().getDimension(R.dimen.listPreferredItemPadding);
+        padding = (int) getContext().getResources().getDimension(R.dimen.listPreferredItemPadding);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = super.getView(position, convertView, parent);
-        if (view instanceof TextView) ((TextView) view).setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+        if (view instanceof TextView)
+            ((TextView) view).setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
         return view;
     }
 
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
         View view = super.getDropDownView(position, convertView, parent);
-        if (view instanceof TextView) ((TextView) view).setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+        if (view instanceof TextView)
+            ((TextView) view).setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
         view.setPadding(padding, 0, padding, 0);
         return view;
     }
