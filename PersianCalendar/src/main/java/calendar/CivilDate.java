d379b8e9e1707aec65ff529b58579d3730f8aa6d
aysha319 <a.aminnejad@gmail.com>
2018-08-01 15:12:17 +0430

    Update CivilDate.java

--- PersianCalendar/src/main/java/calendar/CivilDate.java
+++ PersianCalendar/src/main/java/calendar/CivilDate.java
@@ -89,8 +89,15 @@ public class CivilDate extends AbstractDate {
         this.month = month;
     }
 
-    public int getWeekOfMonth() {
-        throw new RuntimeException(Constants.NOT_IMPLEMENTED_YET);
+    public int getWeekOfMonth(int firstDayOfWeek) {
+        Calendar cal = Calendar.getInstance();
+        cal.setFirstDayOfWeek(firstDayOfWeek);
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month -1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.getTimeInMillis();
+
+        return cal.get(Calendar.WEEK_OF_MONTH);
     }
 
     public int getWeekOfYear() {
