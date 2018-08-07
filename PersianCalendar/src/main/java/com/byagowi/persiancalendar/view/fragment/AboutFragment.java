911a22cbffdd72b60672cb263d17b0d7de9ebca4
MEHDIMYADI <mahdi2006d@gmail.com>
2018-08-07 13:30:40 +0430

    Rebuild about fragment

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
@@ -1,19 +1,27 @@
 package com.byagowi.persiancalendar.view.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.util.Linkify;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.byagowi.persiancalendar.R;
 import com.byagowi.persiancalendar.util.Utils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -23,33 +31,58 @@ import androidx.fragment.app.Fragment;
  */
 public class AboutFragment extends Fragment {
 
-    public String programVersion() {
-        try {
-            return getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.e(AboutFragment.class.getName(), "Name not found on PersianCalendarUtils.programVersion");
-            return "";
-        }
-    }
-
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         Utils.setActivityTitleAndSubtitle(getActivity(), getString(R.string.about), "");
 
+        //version
         String version = programVersion();
-
-        TextView versionTextView = view.findViewById(R.id.version2);
+        TextView versionTextView = view.findViewById(R.id.version);
         versionTextView.setText(getString(R.string.version) + " " +
                 Utils.formatNumber(version.split("-")[0]));
 
-        TextView licenseTextView = view.findViewById(R.id.license);
-        licenseTextView.setText("Android Persian Calendar Version " + version + "\n" +
-                Utils.readRawResource(getContext(), R.raw.credits));
+        //licenses
+        RelativeLayout licenses = view.findViewById(R.id.licenses);
+        licenses.setOnClickListener(arg -> {
+            WebView wv = new WebView(getActivity());
+            WebSettings settings = wv.getSettings();
+            settings.setDefaultTextEncodingName("utf-8");
+            wv.loadUrl("file:///android_res/raw/credits.txt");
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(getResources().getString(R.string.license));
+            builder.setView(wv);
+            builder.setCancelable(true);
+            builder.setNegativeButton(R.string.license_dialog_close, (dialog, which) -> {
+            });
+            builder.show();
+        });
 
-        Linkify.addLinks(licenseTextView, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+        //report bug
+        RelativeLayout bug = view.findViewById(R.id.reportBug);
+        bug.setOnClickListener(arg -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", getString(R.string.mailto), null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
+            try {
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "\n\n\n\n\n\n\n" +
+                        "===Device Information===\nManufacturer: " + Build.MANUFACTURER + "\nModel: " + Build.MODEL + "\nAndroid Version: " + Build.VERSION.RELEASE + "\nApp Version Code: " + version.split("-")[0]);
+                startActivity(Intent.createChooser(emailIntent, getString(R.string.sendMail)));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(getActivity(), getString(R.string.noClient), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
+    }
+
+    private String programVersion() {
+        try {
+            return getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(AboutFragment.class.getName(), "Name not found on PersianCalendarUtils.programVersion");
+            return "";
+        }
     }
 }
