d1580338cbe676988b7c3d10ee64844d706dc6e0
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-11 12:53:15 +0430

    Enable compat vector drawable through using setCompatVectorFromResourcesEnabled

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
@@ -1,7 +1,6 @@
 package com.byagowi.persiancalendar.view.fragment;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
