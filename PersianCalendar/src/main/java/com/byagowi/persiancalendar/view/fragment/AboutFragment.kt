1c34b233ec9298e3b3707db1ddb595fa6919f49a
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-11 14:10:45 +0430

    Minor

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.kt
@@ -12,7 +12,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
