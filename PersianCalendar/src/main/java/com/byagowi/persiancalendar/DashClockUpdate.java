18d1373d25bc0e2636b147530417fcd04d7f6202
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-12 13:32:35 +0430

    Move DashClock update logic into its class

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/DashClockUpdate.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/DashClockUpdate.java
@@ -1,15 +1,30 @@
 package com.byagowi.persiancalendar;
 
+import android.content.Intent;
+
+import com.byagowi.persiancalendar.util.CalendarUtils;
 import com.byagowi.persiancalendar.util.UpdateUtils;
+import com.byagowi.persiancalendar.util.Utils;
+import com.byagowi.persiancalendar.view.activity.MainActivity;
 import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+import calendar.AbstractDate;
+import calendar.CalendarType;
 
 public class DashClockUpdate extends DashClockExtension {
-
     @Override
     protected void onUpdateData(int reason) {
         setUpdateWhenScreenOn(true);
-        UpdateUtils.update(getApplicationContext(), false);
-        publishUpdate(UpdateUtils.getExtensionData());
+        CalendarType mainCalendar = Utils.getMainCalendar();
+        long jdn = CalendarUtils.getTodayJdn();
+        AbstractDate date = CalendarUtils.getDateFromJdnOfCalendar(mainCalendar, jdn);
+        publishUpdate(new ExtensionData().visible(true)
+                .icon(Utils.getDayIconResource(date.getDayOfMonth()))
+                .status(CalendarUtils.getMonthName(date))
+                .expandedTitle(CalendarUtils.dayTitleSummary(date))
+                .expandedBody(CalendarUtils.dateStringOfOtherCalendar(mainCalendar, jdn))
+                .clickIntent(new Intent(getApplicationContext(), MainActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)));
     }
-
 }
