35c52e890e25b98fe01981c53883213da1584a38
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-15 00:07:43 +0430

    Ebrahim's proposed improvements over leak canary integration

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainApplication.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainApplication.java
@@ -3,18 +3,12 @@ package com.byagowi.persiancalendar;
 import android.app.Application;
 
 import com.byagowi.persiancalendar.util.Utils;
-import com.squareup.leakcanary.LeakCanary;
 
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
+        ReleaseDebugDifference.mainApplication(this);
         Utils.initUtils(getApplicationContext());
     }
 }
