c0d4e6d659b47a0f0b5ea77026f036a3bda31b5a
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-16 03:31:31 +0430

    Fix GPSDiagDialog

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/GPSDiagnosticDialog.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/dialog/GPSDiagnosticDialog.java
@@ -1,6 +1,5 @@
 package com.byagowi.persiancalendar.view.dialog;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +9,8 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.byagowi.persiancalendar.R;
 import com.byagowi.persiancalendar.databinding.DialogAccessBinding;
@@ -51,23 +52,23 @@ public class GPSDiagnosticDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Activity activity = getActivity();
-        if (activity == null) return null;
+        Context context = getContext();
+        if (context == null) return null;
 
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
-        DialogAccessBinding binding = DataBindingUtil.inflate(LayoutInflater.from(activity),
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        DialogAccessBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context),
                 R.layout.dialog_access, null, false);
         dialogBuilder.setView(binding.getRoot());
 
         boolean gpsEnabled = false;
+        NetworkInfo info = null;
         try {
             // check whether gps provider and network providers are enabled or not
             LocationManager gps = (LocationManager)
-                    activity.getSystemService(Context.LOCATION_SERVICE);
+                    context.getSystemService(Context.LOCATION_SERVICE);
             ConnectivityManager connectivityManager = (ConnectivityManager)
-                    activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            NetworkInfo info = null;
             if (connectivityManager != null) {
                 info = connectivityManager.getActiveNetworkInfo();
             }
@@ -80,20 +81,43 @@ public class GPSDiagnosticDialog extends DialogFragment {
         } catch (Exception ignore) {
         }
 
+        if (!gpsEnabled && info == null) {
+            Toast.makeText(context, R.string.internet_location_enable, Toast.LENGTH_SHORT).show();
+            binding.dialogButtonGPS.setVisibility(View.VISIBLE);
+            binding.dialogButtonWiFi.setVisibility(View.VISIBLE);
+            binding.dialogButtonGPRS.setVisibility(View.VISIBLE);
+        } else if (!gpsEnabled) {
+            Toast.makeText(context, R.string.location_enable, Toast.LENGTH_SHORT).show();
+            binding.dialogButtonGPRS.setVisibility(View.GONE);
+            binding.dialogButtonWiFi.setVisibility(View.GONE);
+        } else if (info == null) {
+            Toast.makeText(context, R.string.internet_enable, Toast.LENGTH_SHORT).show();
+            binding.dialogButtonGPS.setVisibility(View.GONE);
+        }
+
         binding.dialogButtonGPS.setOnClickListener(v -> {
-            activity.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            Context ctx = getContext();
+            if (ctx != null) {
+                ctx.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
             dismiss();
             // get gps
         });
 
         binding.dialogButtonWiFi.setOnClickListener(v -> {
-            activity.startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            Context ctx = getContext();
+            if (ctx != null) {
+                ctx.startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+            }
             dismiss();
             // get wifi
         });
 
         binding.dialogButtonGPRS.setOnClickListener(v -> {
-            activity.startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            Context ctx = getContext();
+            if (ctx != null) {
+                ctx.startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }
             dismiss();
             // get gprs
         });
