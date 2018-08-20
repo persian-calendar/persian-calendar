73f57215ff45fba1a74dbba0c8d3203a36f6989d
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-21 00:50:25 +0430

    [test] Test specific dates

--- PersianCalendar/src/test/java/com/byagowi/persiancalendar/TestDateCalendar.java
+++ PersianCalendar/src/test/java/com/byagowi/persiancalendar/TestDateCalendar.java
@@ -3,17 +3,32 @@ package com.byagowi.persiancalendar;
 import org.junit.Test;
 
 import calendar.DateConverter;
+import calendar.IslamicDate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestDateCalendar {
     @Test
-    public void iterate_over_all_years() {
-        long startJdn = DateConverter.islamicToJdn(1426, 1, 1);
-        long endJdn = DateConverter.islamicToJdn(1441, 1, 1);
-        for (long jdn = startJdn; jdn <= endJdn; ++jdn) {
-            long result = DateConverter.islamicToJdn(DateConverter.jdnToIslamic(jdn));
-            assertEquals(jdn, result);
+    public void islamic_converter_test() {
+        int[][] tests = {
+                {2453767, 1427, 1, 1},
+                {2455658, 1432, 5, 2},
+                {2458579, 1440, 7, 29},
+                // Note the Jdn, this shouldn't be like this
+                {2458581, 1440, 8, 1}
+        };
+        for (int[] test : tests) {
+            IslamicDate reference = new IslamicDate(test[1], test[2], test[3]);
+            assertEquals(test[0], DateConverter.islamicToJdn(reference));
+
+            assertEquals(test[1], DateConverter.jdnToIslamic(test[0]).getYear());
+            assertEquals(test[2], DateConverter.jdnToIslamic(test[0]).getMonth());
+            assertEquals(test[3], DateConverter.jdnToIslamic(test[0]).getDayOfMonth());
+
+            assertEquals(test[0], DateConverter.islamicToJdn(DateConverter.jdnToIslamic(test[0])));
+            assertTrue(reference.equals(
+                    DateConverter.jdnToIslamic(DateConverter.islamicToJdn(reference))));
         }
     }
 
