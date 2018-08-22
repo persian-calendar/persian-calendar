898d70fe2be06e58010cf8b823f8c52d283770b5
Ebrahim Byagowi <ebrahim@gnu.org>
2018-08-22 23:46:48 +0430

    Improve tabs icons colors

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/CalendarFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/CalendarFragment.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -19,6 +21,7 @@ import android.text.TextPaint;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -51,6 +54,7 @@ import com.github.praytimes.Clock;
 import com.github.praytimes.Coordinate;
 import com.github.praytimes.PrayTime;
 import com.github.praytimes.PrayTimesCalculator;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -63,6 +67,7 @@ import java.util.Set;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -131,6 +136,38 @@ public class CalendarFragment extends Fragment implements View.OnClickListener {
         if (coordinate != null) {
             mainBinding.tabLayout.getTabAt(2).setIcon(R.drawable.ic_access_time);
         }
+
+        Resources.Theme theme = context.getTheme();
+        TypedValue value = new TypedValue();
+
+        theme.resolveAttribute(R.attr.colorAccent, value, true);
+        int selectedColor = ContextCompat.getColor(context, value.resourceId);
+
+        theme.resolveAttribute(R.attr.colorTextSecond, value, true);
+        int unselectedColor = ContextCompat.getColor(context, value.resourceId);
+
+        // https://stackoverflow.com/a/35461201
+        mainBinding.tabLayout.addOnTabSelectedListener(
+                new TabLayout.ViewPagerOnTabSelectedListener(mainBinding.tabContent) {
+
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        super.onTabSelected(tab);
+                        tab.getIcon().setColorFilter(selectedColor, PorterDuff.Mode.SRC_IN);
+                    }
+
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+                        super.onTabUnselected(tab);
+                        tab.getIcon().setColorFilter(unselectedColor, PorterDuff.Mode.SRC_IN);
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+                        super.onTabReselected(tab);
+                    }
+                }
+        );
 
         // https://stackoverflow.com/a/49455239 but obviously a hack we will try to remove
         if (isRTL) {
