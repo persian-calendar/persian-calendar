171b2bb69fb24afbe8cc1bb1b4dd509851053249
Ebrahim Byagowi <ebrahim@gnu.org>
2018-09-23 16:07:08 +0330

    [minor] rename the test

--- PersianCalendar/src/test/java/com/byagowi/persiancalendar/MainLogicTests.java
+++ PersianCalendar/src/test/java/com/byagowi/persiancalendar/MainLogicTests.java
@@ -22,7 +22,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestDateCalendar {
+public class MainLogicTests {
     @Test
     public void islamic_converter_test() {
         int[][] tests = {
