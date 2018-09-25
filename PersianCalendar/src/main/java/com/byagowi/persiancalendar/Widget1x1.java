54e617f50f38c89392b8fe4aa2d23da7c9c01aa5
Ebrahim Byagowi <ebrahim@gnu.org>
2018-09-25 11:13:50 +0330

    Minor

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widget1x1.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widget1x1.java
@@ -7,11 +7,6 @@ import android.content.Intent;
 import com.byagowi.persiancalendar.util.UpdateUtils;
 import com.byagowi.persiancalendar.util.Utils;
 
-/**
- * 1x1 widget provider, implementation is on {@code CalendarWidget}
- *
- * @author ebraminio
- */
 public class Widget1x1 extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widget4x1.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widget4x1.java
@@ -7,11 +7,6 @@ import android.content.Intent;
 import com.byagowi.persiancalendar.util.UpdateUtils;
 import com.byagowi.persiancalendar.util.Utils;
 
-/**
- * 4x1 widget provider, implementation is on {@code CalendarWidget}
- *
- * @author ebraminio
- */
 public class Widget4x1 extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
