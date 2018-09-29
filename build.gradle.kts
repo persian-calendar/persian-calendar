1f02a8cdbdf3a84c8fa733882136a9a3f54ffe94
Ebrahim Byagowi <ebrahim@gnu.org>
2018-09-29 09:55:42 +0330

    Update to a newer Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   }
   dependencies {
     classpath("com.android.tools.build:gradle:3.3.0-alpha11")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.70")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.71")
 
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle files
