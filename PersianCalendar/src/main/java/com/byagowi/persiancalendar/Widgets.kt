ff4a4ed77470709bcaa99dbd3bf87c1943bccb64
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-03-01 11:10:07 +0330

    Merge Widget classes into one file

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widgets.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/Widgets.kt
@@ -3,35 +3,19 @@ package com.byagowi.persiancalendar
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import androidx.annotation.Nullable
 
 import com.byagowi.persiancalendar.utils.UpdateUtils
 import com.byagowi.persiancalendar.utils.Utils
 
-class Widget1x1 : AppWidgetProvider() {
-    override fun onReceive(context: Context, @Nullable intent: Intent) {
+abstract class WidgetProvider : AppWidgetProvider() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null) return
         Utils.startEitherServiceOrWorker(context)
         UpdateUtils.update(context, false)
     }
 }
 
-class Widget2x2 : AppWidgetProvider() {
-    override fun onReceive(context: Context, @Nullable intent: Intent) {
-        Utils.startEitherServiceOrWorker(context)
-        UpdateUtils.update(context, false)
-    }
-}
-
-class Widget4x1 : AppWidgetProvider() {
-    override fun onReceive(context: Context, @Nullable intent: Intent) {
-        Utils.startEitherServiceOrWorker(context)
-        UpdateUtils.update(context, false)
-    }
-}
-
-class Widget4x2 : AppWidgetProvider() {
-    override fun onReceive(context: Context, @Nullable intent: Intent) {
-        Utils.startEitherServiceOrWorker(context)
-        UpdateUtils.update(context, false)
-    }
-}
+class Widget1x1 : WidgetProvider()
+class Widget2x2 : WidgetProvider()
+class Widget4x1 : WidgetProvider()
+class Widget4x2 : WidgetProvider()
