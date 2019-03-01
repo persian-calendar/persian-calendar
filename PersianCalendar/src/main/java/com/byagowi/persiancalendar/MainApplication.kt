745bae4f9d3f51da7be0b32f486f89d5d102fe90
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-03-02 00:31:30 +0330

    Convert MainApplication,DashClockUpdate to Kotlin

---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainApplication.kt
@@ -0,0 +1,19 @@
+package com.byagowi.persiancalendar
+
+import com.byagowi.persiancalendar.di.DaggerAppComponent
+import com.byagowi.persiancalendar.utils.Utils
+
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class MainApplication : DaggerApplication() {
+  override fun onCreate() {
+    super.onCreate()
+    ReleaseDebugDifference.mainApplication(this)
+    Utils.initUtils(applicationContext)
+  }
+
+  override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+    return DaggerAppComponent.builder().create(this)
+  }
+}
