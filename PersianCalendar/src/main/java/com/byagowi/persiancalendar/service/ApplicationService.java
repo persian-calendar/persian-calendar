797721a6c17aa2b1833a13d54fe564d76d542a09
Keivan Esbati <keivan.esbati@gmail.com>
2019-03-04 12:55:28 +0330

    Fixes #409 application service leak by unregistering receiver in onDestroy

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/ApplicationService.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/ApplicationService.java
@@ -22,6 +22,7 @@ import androidx.annotation.Nullable;
 public class ApplicationService extends Service {
 
     private static WeakReference<ApplicationService> instance;
+    private final BroadcastReceivers receiver = new BroadcastReceivers();
 
     @Nullable
     public static ApplicationService getInstance() {
@@ -44,12 +45,18 @@ public class ApplicationService extends Service {
         intentFilter.addAction(Intent.ACTION_TIME_CHANGED);
         intentFilter.addAction(Intent.ACTION_SCREEN_ON);
 //        intentFilter.addAction(Intent.ACTION_TIME_TICK);
-        registerReceiver(new BroadcastReceivers(), intentFilter);
+        registerReceiver(receiver, intentFilter);
 
         Utils.updateStoredPreference(getApplicationContext());
         Utils.loadApp(this);
         UpdateUtils.update(getApplicationContext(), true);
 
         return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
     }
 }
