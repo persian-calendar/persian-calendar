a20edf3a6355003270f0aa1dd7b8939fbe1fa967
Ebrahim Byagowi <ebrahim@gnu.org>
2019-03-08 14:24:31 +0330

    Update WorkManager to 2.0-rc01 for more compatibility with AndroidX

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
   implementation("androidx.core:core-ktx:1.0.1")
   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0")
 
-  implementation("android.arch.work:work-runtime:1.0.0")
+  implementation("androidx.work:work-runtime:2.0.0-rc01")
   // Unnecessary size addition as it adds co-routine apparently also
   // implementation("android.arch.work:work-runtime-ktx:1.0.0-beta03")
 
