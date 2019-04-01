6013e25c6b587364dd2e59dd5908979e1080cf9f
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2019-04-02 00:38:12 +0430

    Refactor

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/converter/ConverterFragment.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/converter/ConverterFragment.kt
@@ -4,54 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.databinding.FragmentConverterBinding
 import com.byagowi.persiancalendar.di.dependencies.MainActivityDependency
-import com.byagowi.persiancalendar.ui.shared.DayPickerView
-import com.byagowi.persiancalendar.utils.CalendarType
 import com.byagowi.persiancalendar.utils.Utils
-
-import javax.inject.Inject
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
 class ConverterFragment : DaggerFragment() {
     @Inject
-    internal var mainActivityDependency: MainActivityDependency? = null
+    lateinit var mainActivityDependency: MainActivityDependency
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        mainActivityDependency!!.mainActivity.setTitleAndSubtitle(getString(R.string.date_converter), "")
+        mainActivityDependency.mainActivity.setTitleAndSubtitle(getString(R.string.date_converter), "")
 
-        val binding = FragmentConverterBinding.inflate(inflater,
-                container, false)
-        val dayPickerView = binding.dayPickerView
+        FragmentConverterBinding.inflate(inflater, container, false).apply {
 
-        binding.calendarsView.expand(true)
-        binding.calendarsView.hideMoreIcon()
-        binding.todayButton.setOnClickListener { v -> dayPickerView.setDayJdnOnView(Utils.getTodayJdn()) }
-        binding.calendarsView.setOnShowHideTodayButton { show ->
-            if (show)
-                binding.todayButton.show()
-            else
-                binding.todayButton.hide()
-        }
-
-        dayPickerView.setOnSelectedDayChangedListener { jdn ->
-            if (jdn == -1L) {
-                binding.calendarsView.visibility = View.GONE
-            } else {
-                binding.calendarsView.visibility = View.VISIBLE
-                val selectedCalendarType = dayPickerView.selectedCalendarType
-                val orderedCalendarTypes = Utils.getOrderedCalendarTypes()
-                orderedCalendarTypes.remove(selectedCalendarType)
-                binding.calendarsView.showCalendars(jdn, selectedCalendarType, orderedCalendarTypes)
+            calendarsView.expand(true)
+            calendarsView.hideMoreIcon()
+            calendarsView.setOnShowHideTodayButton { show ->
+                if (show) todayButton.show() else todayButton.hide()
             }
-        }
-        dayPickerView.setDayJdnOnView(Utils.getTodayJdn())
 
-        return binding.root
+            todayButton.setOnClickListener { dayPickerView.setDayJdnOnView(Utils.getTodayJdn()) }
+
+            dayPickerView.setOnSelectedDayChangedListener { jdn ->
+                if (jdn == -1L) {
+                    calendarsView.visibility = View.GONE
+                } else {
+                    calendarsView.visibility = View.VISIBLE
+                    val selectedCalendarType = dayPickerView.selectedCalendarType
+                    val orderedCalendarTypes = Utils.getOrderedCalendarTypes()
+                    if (selectedCalendarType != null && orderedCalendarTypes != null) {
+                        orderedCalendarTypes.remove(selectedCalendarType)
+                        calendarsView.showCalendars(jdn, selectedCalendarType, orderedCalendarTypes)
+                    }
+                }
+            }
+            dayPickerView.setDayJdnOnView(Utils.getTodayJdn())
+
+            return root
+        }
     }
 }
