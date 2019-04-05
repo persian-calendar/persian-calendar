e119ae9c6fdb8e4bd676bd50293fd9d1dd27db92
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2019-04-05 22:32:51 +0430

    numeric package initial kotlin conversion

---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/locationathan/numeric/NumericDialog.kt
@@ -0,0 +1,28 @@
+package com.byagowi.persiancalendar.ui.preferences.locationathan.numeric
+
+import android.os.Build
+import android.text.InputType
+import android.view.View
+import android.widget.EditText
+
+import androidx.preference.EditTextPreferenceDialogFragmentCompat
+
+/**
+ * Created by ebraminio on 2/21/16.
+ */
+class NumericDialog : EditTextPreferenceDialogFragmentCompat() {
+
+    override fun onBindDialogView(view: View) {
+        super.onBindDialogView(view)
+
+        val editText = view.findViewById<EditText>(android.R.id.edit)
+        editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED or
+                InputType.TYPE_NUMBER_FLAG_DECIMAL
+
+        // on platforms supporting direction as LTR direction is more handy on editing numbers
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            editText.textDirection = View.TEXT_DIRECTION_LTR
+            editText.layoutDirection = View.LAYOUT_DIRECTION_LTR
+        }
+    }
+}
---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/locationathan/numeric/NumericPreference.kt
@@ -0,0 +1,46 @@
+package com.byagowi.persiancalendar.ui.preferences.locationathan.numeric
+
+import android.content.Context
+import android.util.AttributeSet
+
+import androidx.preference.EditTextPreference
+
+/**
+ * Created by ebraminio on 2/16/16.
+ */
+class NumericPreference : EditTextPreference {
+
+    private var mDouble: Double? = null
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context) : super(context) {}
+
+    override fun getText(): String? {
+        return if (mDouble != null) mDouble!!.toString() else null
+    }
+
+    // http://stackoverflow.com/a/10848393
+    override fun setText(text: String) {
+        val wasBlocking = shouldDisableDependents()
+        mDouble = parseDouble(text)
+        persistString(if (mDouble != null) mDouble!!.toString() else null)
+        val isBlocking = shouldDisableDependents()
+        if (isBlocking != wasBlocking) notifyDependencyChange(isBlocking)
+    }
+
+    private fun parseDouble(text: String): Double? {
+        try {
+            return java.lang.Double.parseDouble(text)
+        } catch (e: NumberFormatException) {
+            return null
+        } catch (e: NullPointerException) {
+            return null
+        }
+
+    }
+}
