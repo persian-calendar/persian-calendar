a1146b5559bafbb951136ee9ecd67bd3b940c337
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-06-05 00:45:27 +0430

    Update AGP(3.4.0 -> 3.4.1) & Kotlin(1.3.30 -> 1.3.31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.30"))
+        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
