773d1c8c10fa23ea31d90deb4e4afe81a0467b63
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-08-21 15:41:21 +0430

    Update AGP(3.4.2 -> 3.5.0), Gradle(5.2.1 -> 5.6.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.2")
+        classpath("com.android.tools.build:gradle:3.5.0")
         classpath(kotlin("gradle-plugin", version = "1.3.41"))
 
         // NOTE: Do not place your application dependencies here; they belong
