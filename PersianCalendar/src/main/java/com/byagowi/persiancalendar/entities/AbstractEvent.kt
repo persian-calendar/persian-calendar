f52a5430a7530260d27a0b5fd2e04e2dee33aec7
ftadev <akhlaghi.fatemeh@gmail.com>
2019-09-08 23:51:01 +0430

    Convert AbstractEvent to Kotlin

---
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/entities/AbstractEvent.kt
@@ -0,0 +1,13 @@
+package com.byagowi.persiancalendar.entities
+
+import com.byagowi.persiancalendar.calendar.AbstractDate
+
+abstract class AbstractEvent<T : AbstractDate> {
+    lateinit var date: T
+    lateinit var title: String
+    var isHoliday: Boolean = false
+
+    override fun toString(): String {
+        return title
+    }
+}
