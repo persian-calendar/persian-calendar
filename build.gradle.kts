d78564ae93e6bc5490c63f56be8984f52eb7f25b
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-10-04 18:02:44 +0330

    Move Proguard to its correct place

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.1")
         classpath(kotlin("gradle-plugin", version = "1.3.50"))
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
