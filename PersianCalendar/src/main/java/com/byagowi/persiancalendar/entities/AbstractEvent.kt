d66b3b8a7ba4b4400c6314e133709b785dac2c67
Ebrahim Byagowi <ebrahim@gnu.org>
2019-10-26 18:23:12 +0330

    Fix device events bug by converting back to Java

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/entities/AbstractEvent.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/entities/AbstractEvent.kt
@@ -4,7 +4,7 @@ import io.github.persiancalendar.calendar.AbstractDate
 
 abstract class AbstractEvent<T : AbstractDate> {
     lateinit var date: T
-    lateinit var title: String
+    open lateinit var title: String
     var isHoliday: Boolean = false
 
     override fun toString(): String = title
