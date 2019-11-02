2429d2c5c530b5cdb194b687ca915220b5054847
Ebrahim Byagowi <ebrahim@gnu.org>
2019-11-02 12:36:09 +0330

    Use ViewPager2 in PreferencesFragment

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/PreferencesFragment.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/PreferencesFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.databinding.FragmentSettingsBinding
 import com.byagowi.persiancalendar.di.MainActivityDependency
 import com.byagowi.persiancalendar.ui.preferences.interfacecalendar.FragmentInterfaceCalendar
 import com.byagowi.persiancalendar.ui.preferences.locationathan.FragmentLocationAthan
 import com.byagowi.persiancalendar.ui.preferences.widgetnotification.FragmentWidgetNotification
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -33,37 +33,32 @@ class PreferencesFragment : DaggerFragment() {
 
         FragmentSettingsBinding.inflate(LayoutInflater.from(container?.context), container, false)
             .apply {
-                viewPager.adapter = ViewPagerAdapter(childFragmentManager, 3)
-                tabLayout.setupWithViewPager(viewPager)
+                viewPager.adapter = ViewPagerAdapter(3)
+                TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                    tab.setText(
+                        when (position) {
+                            0 -> R.string.pref_header_interface_calendar
+                            1 -> R.string.pref_header_widget_location
+                            2 -> R.string.pref_header_location_athan
+                            else -> R.string.pref_header_location_athan
+                        }
+                    )
+                }.attach()
 
                 return root
             }
     }
 
-    internal inner class ViewPagerAdapter(manager: FragmentManager, private var pageCount: Int) :
-        FragmentPagerAdapter(manager) {
-
-        override fun getItem(position: Int): Fragment {
-            var fragment = Fragment()
-            when (position) {
-                0 -> fragment = FragmentInterfaceCalendar()
-                1 -> fragment = FragmentWidgetNotification()
-                2 -> fragment = FragmentLocationAthan()
-                else -> {
-                }
-            }
-            return fragment
-        }
-
-        override fun getCount(): Int = pageCount
-
-        override fun getPageTitle(position: Int): CharSequence? {
+    internal inner class ViewPagerAdapter(private var pageCount: Int) : FragmentStateAdapter(this) {
+        override fun createFragment(position: Int): Fragment {
             return when (position) {
-                0 -> resources.getString(R.string.pref_header_interface_calendar)
-                1 -> resources.getString(R.string.pref_header_widget_location)
-                2 -> resources.getString(R.string.pref_header_location_athan)
-                else -> resources.getString(R.string.pref_header_location_athan)
+                0 -> FragmentInterfaceCalendar()
+                1 -> FragmentWidgetNotification()
+                2 -> FragmentLocationAthan()
+                else -> Fragment()
             }
         }
+
+        override fun getItemCount(): Int = pageCount
     }
 }
