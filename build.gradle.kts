a02550da9760f294007689e7e138ab54dd8a6159
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-12-20 03:10:40 +0330

    Update kotlin(1.3.50 -> 1.3.61)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.0-beta05")
-        classpath(kotlin("gradle-plugin", version = "1.3.50"))
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
     }
 }
 
