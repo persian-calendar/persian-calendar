a392f9f0794748deb9ec98935fe787f68f668957
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2020-01-03 03:22:09 +0330

    Update praytimes(1.0.0 -> 1.0.1)

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -79,7 +79,7 @@ android {
 dependencies {
     implementation(project(":equinox"))
     implementation(project(":calendar"))
-    implementation ("com.github.persian-calendar:praytimes:1.0.0")
+    implementation ("com.github.persian-calendar:praytimes:1.0.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
 
