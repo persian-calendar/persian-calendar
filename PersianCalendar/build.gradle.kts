694f1c86a535d546bc19d86ec6dd8b7845bd6239
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2020-01-14 01:22:48 +0330

    Minor

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -79,7 +79,7 @@ android {
 dependencies {
     implementation(project(":equinox"))
     implementation("com.github.persian-calendar:calendar:1.0.0")
-    implementation ("com.github.persian-calendar:praytimes:1.0.1")
+    implementation("com.github.persian-calendar:praytimes:1.0.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
 
