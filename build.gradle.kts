a76edb6715fd7f16498df0984fff5359f9dc6639
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2020-03-24 13:44:25 +0430

    Update kotlin(1.3.70 -> 1.3.71)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.1")
-        classpath(kotlin("gradle-plugin", version = "1.3.70"))
+        classpath(kotlin("gradle-plugin", version = "1.3.71"))
     }
 }
 
