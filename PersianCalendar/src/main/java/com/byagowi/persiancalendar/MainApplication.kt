aacfb1f7d9c6386b5ea3a969c7e8ad8ab200f774
Ebrahim Byagowi <ebrahim@gnu.org>
2020-05-18 14:50:50 +0430

    Copy crash stacktrace to clipboard

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainApplication.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/MainApplication.kt
@@ -1,12 +1,29 @@
 package com.byagowi.persiancalendar
 
 import android.app.Application
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.util.Log
+import androidx.core.content.getSystemService
 import com.byagowi.persiancalendar.utils.initUtils
+import kotlin.system.exitProcess
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ReleaseDebugDifference.mainApplication(this)
         initUtils(applicationContext)
+
+        val oldHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            try {
+                getSystemService<ClipboardManager>()?.setPrimaryClip(
+                    ClipData.newPlainText("", Log.getStackTraceString(throwable))
+                )
+            } catch (e: Exception) {
+            }
+
+            oldHandler?.uncaughtException(thread, throwable) ?: exitProcess(2)
+        }
     }
 }
