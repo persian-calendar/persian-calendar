7b7fdec9fa0fa47cd880dc5cec4074702d5fd036
Ebrahim Byagowi <ebrahim@gnu.org>
2020-06-21 20:39:32 +0430

    Improve formatCoordinateISO6709

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/utils/Functions.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/utils/Functions.kt
@@ -303,13 +303,14 @@ fun formatCoordinate(context: Context, coordinate: Coordinate, separator: String
 
 // https://stackoverflow.com/a/62499553
 // https://en.wikipedia.org/wiki/ISO_6709#Representation_at_the_human_interface_(Annex_D)
-fun formatCoordinateISO6709(lat: Double, long: Double, alt: Double? = null) =
-    (listOf(abs(lat) to if (lat >= 0) "N" else "S", abs(long) to if (long >= 0) "E" else "W").map {
-        val degrees = it.first.toInt()
-        val minutes = ((it.first - degrees) * 60).toInt()
-        val seconds = ((it.first - degrees) * 3600 % 60).toInt()
-        "$degrees°$minutes′$seconds${it.second}"
-    } + (alt?.let { "${alt}m" } ?: "")).joinToString(" ")
+fun formatCoordinateISO6709(lat: Double, long: Double, alt: Double? = null) = listOf(
+    abs(lat) to if (lat >= 0) "N" else "S", abs(long) to if (long >= 0) "E" else "W"
+).joinToString(" ") {
+    val degrees = it.first.toInt()
+    val minutes = ((it.first - degrees) * 60).toInt()
+    val seconds = ((it.first - degrees) * 3600 % 60).toInt()
+    "%d°%02d′%02d%s".format(Locale.US, degrees, minutes, seconds, it.second)
+} + (alt?.let { " %s%.1fm".format(Locale.US, if (alt < 0) "−" else "", abs(alt)) } ?: "")
 
 fun getCityName(context: Context, fallbackToCoord: Boolean): String =
     getCityFromPreference(context)?.let {
