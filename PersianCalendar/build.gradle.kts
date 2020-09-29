b628885faeb877ff0a976f8900575d8d1cd7829c
Ebrahim Byagowi <ebrahim@gnu.org>
2020-09-30 01:21:17 +0330

    Update to 1400 version of calendar library

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -95,7 +95,7 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
     implementation("com.github.persian-calendar:equinox:1.0.0")
-    implementation("com.github.persian-calendar:calendar:1.0.0")
+    implementation("com.github.persian-calendar:calendar:e1f10e4") // Should be 1.0.1
     implementation("com.github.persian-calendar:praytimes:1.0.0")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${KotlinCompilerVersion.VERSION}")
