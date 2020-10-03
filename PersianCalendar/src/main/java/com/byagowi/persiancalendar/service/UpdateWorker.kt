afa4ed572ef511b6dc8117a6aee5a9fde92dd71a
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2020-10-03 13:34:57 +0330

    Tag UpdateWorker errors

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/UpdateWorker.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/UpdateWorker.kt
@@ -1,12 +1,15 @@
 package com.byagowi.persiancalendar.service
 
 import android.content.Context
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.byagowi.persiancalendar.utils.setChangeDateWorker
 import com.byagowi.persiancalendar.utils.update
 import com.byagowi.persiancalendar.utils.updateStoredPreference
 import kotlinx.coroutines.coroutineScope
+
+const val TAG = "UpdateWorker"
 
 class UpdateWorker(context: Context, workerParams: WorkerParameters) :
     CoroutineWorker(context, workerParams) {
@@ -17,7 +20,7 @@ class UpdateWorker(context: Context, workerParams: WorkerParameters) :
             update(applicationContext, true)
             Result.success()
         } catch (e: Throwable) {
-            e.printStackTrace()
+            Log.e(TAG, "Error running UpdateWorker: ", e)
             Result.failure()
         }
     }
