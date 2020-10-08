68db050829a5250c57b232ac4074d035d8c1eb6a
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2020-10-08 13:26:22 +0330

    Update androidx.browser dependency(1.0.0 -> 1.2.0)

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.2.5")
     implementation("androidx.activity:activity-ktx:1.1.0")
 
-    implementation("androidx.browser:browser:1.0.0") // Update needs minSdkVersion > 15
+    implementation("androidx.browser:browser:1.2.0")
 
     implementation("androidx.work:work-runtime-ktx:2.4.0")
 
