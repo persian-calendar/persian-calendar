db01657547a060ff062ee3b2c5792f0ce5bd57d7
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2020-12-10 12:46:38 +0330

    Update kotlin(1.4.10 -> 1.4.21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.2")
-        classpath(kotlin("gradle-plugin", version = "1.4.10"))
+        classpath(kotlin("gradle-plugin", version = "1.4.21"))
     }
 }
 
