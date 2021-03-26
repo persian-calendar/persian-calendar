753993cee9d6ca4f6114f401b571f301741407ef
Ebrahim Byagowi <ebrahim@gnu.org>
2021-03-26 21:20:16 +0430

    Update Android Build Tools to 4.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.android.tools.build:gradle:4.1.3")
         classpath(kotlin("gradle-plugin", version = "1.4.30"))
     }
 }
