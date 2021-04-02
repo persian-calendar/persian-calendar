58696bc166ac78da27b0c64be2af68e9e599b3fa
Farhad Beigirad <f.beigirad@gmail.com>
2021-04-02 22:03:53 +0430

    retrieve app version from BuildConfig.
    because they are the same.
    https://developer.android.com/reference/android/content/pm/PackageInfo#versionName

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/about/AboutFragment.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/about/AboutFragment.kt
@@ -1,6 +1,5 @@
 package com.byagowi.persiancalendar.ui.about
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Typeface
 import android.net.Uri
@@ -8,7 +7,12 @@ import android.os.Build
 import android.os.Bundle
 import android.text.util.Linkify
 import android.util.TypedValue
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
@@ -19,18 +23,35 @@ import androidx.core.net.toUri
 import androidx.core.view.setMargins
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
-import com.byagowi.persiancalendar.*
+import com.byagowi.persiancalendar.BuildConfig
+import com.byagowi.persiancalendar.LANG_AZB
+import com.byagowi.persiancalendar.LANG_EN_IR
+import com.byagowi.persiancalendar.LANG_FA
+import com.byagowi.persiancalendar.LANG_FA_AF
+import com.byagowi.persiancalendar.LANG_GLK
+import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.databinding.DialogEmailBinding
 import com.byagowi.persiancalendar.databinding.FragmentAboutBinding
 import com.byagowi.persiancalendar.ui.MainActivity
-import com.byagowi.persiancalendar.utils.*
+import com.byagowi.persiancalendar.utils.bringMarketPage
+import com.byagowi.persiancalendar.utils.formatNumber
+import com.byagowi.persiancalendar.utils.language
+import com.byagowi.persiancalendar.utils.logException
+import com.byagowi.persiancalendar.utils.readRawResource
+import com.byagowi.persiancalendar.utils.supportedYearOfIranCalendar
 import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 class AboutFragment : Fragment() {
 
+    private val appVersionList
+        get() = BuildConfig.VERSION_NAME.split("-")
+            .mapIndexed { i, x -> if (i == 0) formatNumber(x) else x }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         val mainActivity = activity as MainActivity
         mainActivity.setTitleAndSubtitle(getString(R.string.about), "")
@@ -39,10 +60,7 @@ class AboutFragment : Fragment() {
         val binding = FragmentAboutBinding.inflate(inflater, container, false)
 
         // version
-        val version = programVersion(mainActivity).split("-")
-            .mapIndexed { i, x -> if (i == 0) formatNumber(x) else x }
-        binding.version.text =
-            getString(R.string.version).format(version.joinToString("\n"))
+        binding.version.text = getString(R.string.version).format(appVersionList.joinToString("\n"))
 
         // licenses
         binding.licenses.setOnClickListener {
@@ -112,7 +130,7 @@ class AboutFragment : Fragment() {
 Manufacturer: ${Build.MANUFACTURER}
 Model: ${Build.MODEL}
 Android Version: ${Build.VERSION.RELEASE}
-App Version Code: ${version[0]}"""
+App Version Code: ${appVersionList[0]}"""
                         )
                         startActivity(
                             Intent.createChooser(
@@ -234,8 +252,4 @@ App Version Code: ${version[0]}"""
         }
         return true
     }
-
-    private fun programVersion(context: Context): String = runCatching {
-        context.packageManager.getPackageInfo(context.packageName, 0).versionName
-    }.onFailure(logException).getOrDefault("")
 }
