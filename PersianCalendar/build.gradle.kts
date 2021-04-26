717e95653aca77594d1a681a34252ffd98982815
Ebrahim Byagowi <ebrahim@gnu.org>
2021-04-27 00:34:02 +0430

    Update dependencies

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -114,7 +114,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.fragment:fragment-ktx:1.3.2")
+    implementation("androidx.fragment:fragment-ktx:1.3.3")
     implementation("androidx.activity:activity-ktx:1.2.2")
 
     implementation("androidx.browser:browser:1.3.0")
