b517c5602591e735659116232fe3e1f3dcf67a71
Ebrahim Byagowi <ebrahim@gnu.org>
2021-05-03 05:35:30 +0430

    [compose/shiftwork] Implement dropdowns

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/calendar/dialogs/ShiftWorkDialogList.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/calendar/dialogs/ShiftWorkDialogList.kt
@@ -2,6 +2,8 @@ package com.byagowi.persiancalendar.ui.calendar.dialogs
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -9,9 +11,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.entities.ShiftWorkRecord
@@ -41,17 +48,45 @@ private fun ShiftWorkDialogList(
     )
 ) {
     val state = remember { mutableStateOf(record) }
+    val selectedTypeDropdownIndex = remember { mutableStateOf(-1) }
+    val selectedLengthDropdownIndex = remember { mutableStateOf(-1) }
+
+    val shiftWorkLengthHeader = stringResource(R.string.shift_work_days_head)
+    val resultTemplate = stringResource(R.string.shift_work_record_title)
+    val shiftWorkTitles = stringArrayResource(R.array.shift_work)
+
     Column(modifier = Modifier.fillMaxWidth()) {
-        state.value.mapIndexed { position: Int, (type: String, length: Int) ->
+        state.value.forEachIndexed { position: Int, (type: String, length: Int) ->
             Row(verticalAlignment = Alignment.CenterVertically) {
+                Spacer(modifier = Modifier.width(4.dp))
                 Text(
                     text = "%s:".format(formatNumber(position + 1)),
                     modifier = Modifier.width(10.dp)
                 )
                 TextField(
+                    modifier = Modifier.onFocusChanged {
+                        if (it == FocusState.Active)
+                            selectedTypeDropdownIndex.value = position
+                        else if (selectedTypeDropdownIndex.value == position)
+                            selectedTypeDropdownIndex.value = -1
+                    },
                     value = type,
                     onValueChange = { state.value = state.value.modifyTypeOfRecord(it, position) }
                 )
+                DropdownMenu(
+                    expanded = selectedTypeDropdownIndex.value == position,
+                    onDismissRequest = { selectedTypeDropdownIndex.value = -1 },
+                    offset = DpOffset(20.dp, 0.dp)
+                ) {
+                    shiftWorkTitles.forEach { item ->
+                        DropdownMenuItem(
+                            onClick = {
+                                selectedTypeDropdownIndex.value = -1
+                                state.value = state.value.modifyTypeOfRecord(item, position)
+                            }
+                        ) { Text(item) }
+                    }
+                }
                 Spacer(modifier = Modifier.width(4.dp))
                 TextField(
                     value = length.toString(),
@@ -60,14 +95,34 @@ private fun ShiftWorkDialogList(
                         state.value =
                             state.value.modifyLengthOfRecord(it.toIntOrNull() ?: 0, position)
                     },
-                    modifier = Modifier.width(70.dp)
+                    modifier = Modifier
+                        .width(70.dp)
+                        .onFocusChanged {
+                            if (it == FocusState.Active)
+                                selectedLengthDropdownIndex.value = position
+                            else if (selectedLengthDropdownIndex.value == position)
+                                selectedLengthDropdownIndex.value = -1
+                        }
                 )
+                DropdownMenu(
+                    expanded = selectedLengthDropdownIndex.value == position,
+                    onDismissRequest = { selectedLengthDropdownIndex.value = -1 },
+                    offset = DpOffset(220.dp, 0.dp)
+                ) {
+                    (0..7).map { length ->
+                        DropdownMenuItem(
+                            onClick = {
+                                selectedLengthDropdownIndex.value = -1
+                                state.value = state.value.modifyLengthOfRecord(length, position)
+                            }
+                        ) { Text(if (length == 0) shiftWorkLengthHeader else formatNumber(length)) }
+                    }
+                }
             }
         }
 
         val recordsToShow = state.value.filter { it.length != 0 }
         if (recordsToShow.isNotEmpty()) {
-            val resultTemplate = stringResource(R.string.shift_work_record_title)
             Text(text = recordsToShow.joinToString(spacedComma) {
                 resultTemplate.format(formatNumber(it.length), shiftWorkKeyToString(it.type))
             }, modifier = Modifier.align(Alignment.CenterHorizontally))
