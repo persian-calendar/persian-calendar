9319d83904ac1f4910b28c42be4fd82bcb016592
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2021-05-06 02:18:13 +0430

    Update kotlin(1.4.32 -> 1.5.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.0")
-        classpath(kotlin("gradle-plugin", version = "1.4.32"))
+        classpath(kotlin("gradle-plugin", version = "1.5.0"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
     }
 }
