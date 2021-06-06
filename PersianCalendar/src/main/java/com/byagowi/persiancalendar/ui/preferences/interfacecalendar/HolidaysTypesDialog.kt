11a3f0adf5acd165bef04a329fcb8d364d873f74
Ebrahim Byagowi <ebrahim@gnu.org>
2021-06-06 09:41:28 +0430

    Define holidays hierarchy once and use it in the rest

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/interfacecalendar/HolidaysTypesDialog.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/interfacecalendar/HolidaysTypesDialog.kt
@@ -9,13 +9,17 @@ import com.byagowi.persiancalendar.PREF_HOLIDAY_TYPES
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.databinding.HolidaysTypesDialogBinding
 import com.byagowi.persiancalendar.utils.appPrefs
-import com.google.android.material.checkbox.MaterialCheckBox
 import kotlin.random.Random
 
 fun Fragment.showHolidaysTypesDialog() {
     val context = context ?: return
 
     val binding = HolidaysTypesDialogBinding.inflate(layoutInflater)
+
+    val hierarchy = listOf(
+        binding.iran to listOf(binding.iranHolidays, binding.iranOthers),
+        binding.afghanistan to listOf(binding.afghanistanHolidays, binding.afghanistanOthers)
+    )
 
     // Update labels
     listOf(
@@ -32,68 +36,48 @@ fun Fragment.showHolidaysTypesDialog() {
 
     // Update view from stored settings
     val initial =
-        (context.appPrefs.getStringSet(PREF_HOLIDAY_TYPES, null) ?: setOf("iran_holidays"))
-            .toList().map {
-                if (it == "iran_islamic") "iran_others" else it // update the legacy key
-            }.toSet()
+        (context.appPrefs.getStringSet(PREF_HOLIDAY_TYPES, null) ?: setOf("iran_holidays")).map {
+            if (it == "iran_islamic") "iran_others" else it // update the legacy key
+        }.toSet()
     binding.iranHolidays.isChecked = "iran_holidays" in initial
     binding.iranOthers.isChecked = "iran_others" in initial
     binding.afghanistanHolidays.isChecked = "afghanistan_holidays" in initial
     binding.afghanistanOthers.isChecked = "afghanistan_others" in initial
-    fun updateGroupsChecks() {
-        binding.iran.isChecked =
-            binding.iranHolidays.isChecked || binding.iranOthers.isChecked
-        binding.afghanistan.isChecked =
-            binding.afghanistanHolidays.isChecked || binding.afghanistanOthers.isChecked
-    }
-    updateGroupsChecks()
-    fun updateGroupsMixedIndicator() {
-        fun MaterialCheckBox.setMixedIndicator(value: Boolean) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                this.buttonDrawable?.alpha = if (value) 127 else 255
-            } else {
-                this.alpha = if (value) .5f else 1f
-            }
-        }
-        binding.iran.setMixedIndicator(
-            binding.iranHolidays.isChecked xor binding.iranOthers.isChecked
-        )
-        binding.afghanistan.setMixedIndicator(
-            binding.afghanistanHolidays.isChecked xor binding.afghanistanOthers.isChecked
-        )
-    }
-    updateGroupsMixedIndicator()
     binding.iranAncient.isChecked = "iran_ancient" in initial
     binding.international.isChecked = "international" in initial
 
+    // Hierarchy updates logics
+    fun updateGroupsChecks() = hierarchy.forEach { (parent, children) ->
+        parent.isChecked = children.fold(false) { acc, child -> acc || child.isChecked }
+    }
+    updateGroupsChecks()
+    fun updateGroupsMixedIndicator() = hierarchy.forEach { (parent, children) ->
+        val isMixed = children.fold(false) { acc, child -> acc xor child.isChecked }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            parent.buttonDrawable?.alpha = if (isMixed) 127 else 255
+        } else {
+            parent.alpha = if (isMixed) .5f else 1f
+        }
+    }
+    updateGroupsMixedIndicator()
+
     // Install events listeners
-    val disabledEvents = Random.nextInt()
-    binding.iran.setOnCheckedChangeListener { _, isChecked ->
-        if (binding.iran.getTag(disabledEvents) == true) return@setOnCheckedChangeListener
-        val dest = (binding.iranHolidays.isChecked xor binding.iranOthers.isChecked)
-                || isChecked
-        binding.iranHolidays.isChecked = dest
-        binding.iranOthers.isChecked = dest
-        updateGroupsMixedIndicator()
-    }
-    binding.afghanistan.setOnCheckedChangeListener { _, isChecked ->
-        if (binding.afghanistan.getTag(disabledEvents) == true) return@setOnCheckedChangeListener
-        val dest = (binding.afghanistanHolidays.isChecked xor binding.afghanistanOthers.isChecked)
-                || isChecked
-        binding.afghanistanHolidays.isChecked = dest
-        binding.afghanistanOthers.isChecked = dest
-        updateGroupsMixedIndicator()
-    }
-    listOf(
-        binding.iranHolidays to binding.iran, binding.iranOthers to binding.iran,
-        binding.afghanistanHolidays to binding.afghanistan,
-        binding.afghanistanOthers to binding.afghanistan
-    ).forEach { (child, parent) ->
-        child.setOnCheckedChangeListener { _, _ ->
-            parent.setTag(disabledEvents, true)
-            updateGroupsChecks()
-            parent.setTag(disabledEvents, false)
+    val disabledEventsTag = Random.nextInt()
+    hierarchy.forEach { (parent, children) ->
+        parent.setOnCheckedChangeListener { _, isChecked ->
+            if (parent.getTag(disabledEventsTag) == true) return@setOnCheckedChangeListener
+            children.map { it.isChecked }
+            val dest = children.fold(false) { acc, child -> acc xor child.isChecked } || isChecked
+            children.forEach { it.isChecked = dest }
             updateGroupsMixedIndicator()
+        }
+        children.forEach {
+            it.setOnCheckedChangeListener { _, _ ->
+                parent.setTag(disabledEventsTag, true)
+                updateGroupsChecks()
+                parent.setTag(disabledEventsTag, false)
+                updateGroupsMixedIndicator()
+            }
         }
     }
 
