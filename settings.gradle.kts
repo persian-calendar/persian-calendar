8567fc6e3e1f9518e589d698dbce6449c8780f29
Ebrahim Byagowi <ebrahim@gnu.org>
2021-06-08 18:51:50 +0430

    Revert to jitpack for dependencies fetch
    Android Studio/Inttelij support doesn't seem to work perfectly,
    and sees the dependencies missing in IDE, the build and gradle
    doesn't have any issue which feels weird.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,2 @@
-import java.net.URI
-
-sourceControl {
-    listOf("equinox", "calendar", "praytimes").forEach {
-        gitRepository(URI.create("https://github.com/persian-calendar/$it.git")) {
-            producesModule("io.github.persiancalendar:$it")
-        }
-    }
-}
-
 rootProject.buildFileName = "build.gradle.kts"
 include(":PersianCalendar")
