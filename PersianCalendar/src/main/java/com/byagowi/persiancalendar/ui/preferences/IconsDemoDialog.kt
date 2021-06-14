4a92f4dcb98a51692ef84a002f283c7376c6aa0c
Ebrahim Byagowi <ebrahim@gnu.org>
2021-06-14 10:34:10 +0430

    Simplify IconsDemoDialog view hierarchy
    After consulting with @beigirad and learning about ShapeableImageView, thanks!

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/IconsDemoDialog.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/preferences/IconsDemoDialog.kt
@@ -1,12 +1,9 @@
 package com.byagowi.persiancalendar.ui.preferences
 
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.setPadding
+import androidx.core.view.setMargins
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +11,9 @@ import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.utils.createStatusIcon
 import com.byagowi.persiancalendar.utils.dp
 import com.byagowi.persiancalendar.utils.getDayIconResource
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.ShapeAppearanceModel
 
 // Debug only dialog to check validity of dynamic icons generation
 fun Fragment.showIconsDemoDialog() {
@@ -25,24 +25,18 @@ fun Fragment.showIconsDemoDialog() {
                 override fun getItemCount() = 62
                 override fun getItemViewType(position: Int) = position
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-                    object : RecyclerView.ViewHolder(FrameLayout(context).also { frameLayout ->
-                        frameLayout.setPadding(4.dp)
-                        frameLayout.addView(ImageView(context).also { imageView ->
-                            val gradientDrawable = GradientDrawable()
-                            gradientDrawable.cornerRadius = 10.dp.toFloat()
-                            gradientDrawable.setColor(Color.BLACK)
-                            imageView.background = gradientDrawable
-                            imageView.layoutParams = ViewGroup.LayoutParams(36.dp, 36.dp)
-                        })
-                        frameLayout.addView(ImageView(context).also { imageView ->
-                            val day = viewType / 2 + 1
-                            if (viewType % 2 == 0) {
-                                imageView.setImageResource(getDayIconResource(day))
-                            } else {
-                                imageView.setImageBitmap(createStatusIcon(context, day))
-                            }
-                            imageView.layoutParams = ViewGroup.LayoutParams(36.dp, 36.dp)
-                        })
+                    object : RecyclerView.ViewHolder(ShapeableImageView(context).apply {
+                        val day = viewType / 2 + 1
+                        when (viewType % 2) {
+                            0 -> setImageResource(getDayIconResource(day))
+                            1 -> setImageBitmap(createStatusIcon(context, day))
+                        }
+                        layoutParams = ViewGroup.MarginLayoutParams(36.dp, 36.dp)
+                            .apply { setMargins(4.dp) }
+                        shapeAppearanceModel = ShapeAppearanceModel().toBuilder()
+                            .setAllCorners(CornerFamily.ROUNDED, 8.dp.toFloat())
+                            .build()
+                        setBackgroundColor(Color.DKGRAY)
                     }) {}
             }
             it.layoutManager = GridLayoutManager(context, 8)
