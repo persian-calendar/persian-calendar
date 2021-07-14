dc96128cb6cecf616239775515f9d58656acf485
ebraminio <ebrahim@gnu.org>
2021-07-14 12:20:40 +0430

    Update to 1.5.21 version of Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.2")
-        classpath(kotlin("gradle-plugin", version = "1.5.20"))
+        classpath(kotlin("gradle-plugin", version = "1.5.21"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
     }
 }
