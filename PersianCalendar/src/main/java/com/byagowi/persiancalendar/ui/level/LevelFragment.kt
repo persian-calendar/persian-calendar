a9bfc039189f895c5ec7fe8f31b4cbcd1fc9c354
Ebrahim Byagowi <ebrahim@gnu.org>
2021-07-29 00:10:28 +0430

    Move LevelFragment to calendar's package
    LevelFragment had things more related to the project than things
    were originally brought from level app thus moved it to calendar's
    package to reflect this fact.

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/level/LevelFragment.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/ui/level/LevelFragment.kt
@@ -1,13 +1,9 @@
-package net.androgames.level
+package com.byagowi.persiancalendar.ui.level
 
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.Surface
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.byagowi.persiancalendar.R
@@ -16,27 +12,8 @@ import com.byagowi.persiancalendar.ui.DrawerHost
 import com.byagowi.persiancalendar.utils.getCompatDrawable
 import com.byagowi.persiancalendar.utils.navigateSafe
 import com.byagowi.persiancalendar.utils.onClick
-import com.byagowi.persiancalendar.utils.setupUpNavigation
+import net.androgames.level.OrientationProvider
 
-/*
- * This file is part of Level (an Android Bubble Level).
- * <https://github.com/avianey/Level>
- *
- * Copyright (C) 2014 Antoine Vianey
- *
- * Level is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Level is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Level. If not, see <http://www.gnu.org/licenses/>
- */
 class LevelFragment : Fragment() {
 
     private var provider: OrientationProvider? = null
