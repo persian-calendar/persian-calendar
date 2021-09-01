d5e213632056655efe122ef2603d751feb1d6193
Ebrahim Byagowi <ebrahim@gnu.org>
2021-09-02 03:11:53 +0430

    Upgrade AGP to 7.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath(kotlin("gradle-plugin", version = "1.5.21"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
     }
