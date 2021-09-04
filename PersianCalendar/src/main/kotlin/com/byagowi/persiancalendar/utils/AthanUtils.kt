cf7817f9e6f65666319c425073f35b2d7f81c3fd
Ebrahim Byagowi <ebrahim@gnu.org>
2021-09-04 20:48:45 +0430

    Bring back unified AthanUtils
    It was done just to learn about the difference and
    the experience will be used for rest of the work.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/utils/AthanUtils.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/utils/AthanUtils.kt
@@ -159,6 +159,8 @@ private fun scheduleAlarm(context: Context, alarmTimeName: String, timeInMillis:
     when {
         Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1 ->
             am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent)
+        Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2 ->
+            am.setExact(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent)
         else -> am.set(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent)
     }
 }
