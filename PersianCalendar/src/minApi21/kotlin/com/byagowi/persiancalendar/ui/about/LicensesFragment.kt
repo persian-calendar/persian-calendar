a55ea72beab092a7ae20c4c483c3bd937f1357cb
Ebrahim Byagowi <ebrahim@gnu.org>
2021-09-09 13:02:05 +0430

    [compose] Make LicensesFragment items expandable

--- PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/about/LicensesFragment.kt
+++ PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/about/LicensesFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
@@ -19,6 +22,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,24 +45,29 @@ class LicensesFragment : Fragment() {
         val context = LocalContext.current
         val sections = remember { context.resources.getCreditsSections() }
         CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+            val isOpened = remember { List(sections.size) { false }.toMutableStateList() }
             LazyColumn {
-                items(sections) { (title, license, text) ->
-                    Row(
-                        horizontalArrangement = Arrangement.End,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(title)
-                        Spacer(modifier = Modifier.width(4.dp))
-                        if (license != null) Text(
-                            license,
-                            modifier = Modifier
-                                .background(Color.LightGray, RoundedCornerShape(CornerSize(3.dp)))
-                                .padding(horizontal = 4.dp),
-                            style = TextStyle(Color.DarkGray, 12.sp)
-                        )
+                itemsIndexed(sections) { i, (title, license, text) ->
+                    Column(modifier = Modifier.clickable { isOpened[i] = !isOpened[i] }) {
+                        Row(
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(title)
+                            Spacer(modifier = Modifier.width(4.dp))
+                            if (license != null) Text(
+                                license,
+                                modifier = Modifier
+                                    .background(
+                                        Color.LightGray, RoundedCornerShape(CornerSize(3.dp))
+                                    )
+                                    .padding(horizontal = 4.dp),
+                                style = TextStyle(Color.DarkGray, 12.sp)
+                            )
+                        }
+                        if (isOpened[i]) Text(text)
+                        Divider(color = Color.LightGray)
                     }
-                    Text(text)
-                    Divider(color = Color.LightGray)
                 }
             }
         }
