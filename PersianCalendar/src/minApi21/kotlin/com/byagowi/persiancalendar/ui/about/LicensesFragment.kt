6c1da3e73683642a98ffc4c517a4075f71221a3b
Ebrahim Byagowi <ebrahim@gnu.org>
2021-09-09 15:30:03 +0430

    [compose] Add an arrow to LicensesFragment

--- PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/about/LicensesFragment.kt
+++ PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/about/LicensesFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,6 +20,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -25,9 +28,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.LayoutDirection
@@ -46,17 +52,29 @@ class LicensesFragment : Fragment() {
         val sections = remember { context.resources.getCreditsSections() }
         CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
             val isOpened = remember { mutableStateOf(-1) }
+            val initialDegree =
+                if (LocalLayoutDirection.current == LayoutDirection.Rtl) 90f else -90f
             LazyColumn {
                 itemsIndexed(sections) { i, (title, license, text) ->
                     Column(modifier = Modifier
                         .clickable { isOpened.value = if (isOpened.value == i) -1 else i }
-                        .padding(horizontal = 4.dp)
+                        .padding(6.dp)
+                        .fillMaxWidth()
                         .animateContentSize()
                     ) {
                         Row(
                             horizontalArrangement = Arrangement.End,
                             verticalAlignment = Alignment.CenterVertically
                         ) {
+                            Image(
+                                painter = painterResource(androidx.preference.R.drawable.ic_arrow_down_24dp),
+                                contentDescription = stringResource(R.string.more),
+                                modifier = Modifier.rotate(
+                                    if (isOpened.value == i) 0f else initialDegree
+                                ),
+                                colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
                             Text(title)
                             Spacer(modifier = Modifier.width(4.dp))
                             if (license != null) Text(
@@ -70,8 +88,8 @@ class LicensesFragment : Fragment() {
                             )
                         }
                         if (isOpened.value == i) Text(text)
-                        Divider(color = Color.LightGray)
                     }
+                    Divider(color = Color.LightGray)
                 }
             }
         }
