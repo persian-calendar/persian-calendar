53c78c2837d7820ec9e47473a0409db9cce43056
Ebrahim Byagowi <ebrahim@gnu.org>
2021-09-27 15:37:39 +0330

    Minor simplification

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/level/RulerView.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/level/RulerView.kt
@@ -18,6 +18,10 @@ class RulerView(context: Context, attrs: AttributeSet? = null) : View(context, a
         it.strokeWidth = 1.dp
         it.textSize = textSize
     }
+    private val textSideOffset = 24.dp
+    private val firstLevel = 20.dp
+    private val secondLevel = 10.dp
+    private val thirdLevel = 5.dp
     private val topOffset = 10.dp.toInt()
     private val topTextOffset = topOffset - textSize / 2
     override fun onDraw(canvas: Canvas?) {
@@ -26,16 +30,16 @@ class RulerView(context: Context, attrs: AttributeSet? = null) : View(context, a
         // Inch
         paint.textAlign = Paint.Align.LEFT
         val steps = dpi / 4f
-        (topOffset..height step steps.toInt()).forEachIndexed { i, _ ->
+        (0..(height / steps).toInt()).forEach { i ->
             val y = topOffset + steps * i
             val w = when {
                 i % 4 == 0 -> {
-                    val label = "${i / 4}${if (i == 0) " in" else ""}"
-                    canvas.drawText(label, 54f, y + topTextOffset, paint)
-                    48f
+                    val label = if (i == 0) "0 in" else "${i / 4}"
+                    canvas.drawText(label, textSideOffset, y + topTextOffset, paint)
+                    firstLevel
                 }
-                i % 2 == 0 -> 24f
-                else -> 12f
+                i % 2 == 0 -> secondLevel
+                else -> thirdLevel
             }
             canvas.drawLine(0f, y, w, y, paint)
         }
@@ -43,19 +47,18 @@ class RulerView(context: Context, attrs: AttributeSet? = null) : View(context, a
         // Centimeter
         paint.textAlign = Paint.Align.RIGHT
         val cmSteps = dpi / 2.54 / 10
-        val width = width.toFloat()
-        (topOffset..height step cmSteps.toInt()).forEachIndexed { i, _ ->
+        (0..(height / cmSteps).toInt()).forEach { i ->
             val y = topOffset + cmSteps.toFloat() * i
             val w = when {
                 i % 10 == 0 -> {
-                    val label = "${if (i == 0) "cm " else ""}${i / 10}"
-                    canvas.drawText(label, width - 54f, y + topTextOffset, paint)
-                    48f
+                    val label = if (i == 0) "0 cm" else "${i / 10}"
+                    canvas.drawText(label, width - textSideOffset, y + topTextOffset, paint)
+                    firstLevel
                 }
-                i % 5 == 0 -> 24f
-                else -> 12f
+                i % 5 == 0 -> secondLevel
+                else -> thirdLevel
             }
-            canvas.drawLine(width, y, width - w, y, paint)
+            canvas.drawLine(width.toFloat(), y, width - w, y, paint)
         }
     }
 }
