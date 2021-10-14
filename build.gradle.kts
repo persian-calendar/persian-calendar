f305d74af5a53f643e514b8df3d72fb63e35abd2
Ebrahim Byagowi <ebrahim@gnu.org>
2021-10-14 10:28:03 +0330

    Upgrade compose and Kotlin compiler versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath(kotlin("gradle-plugin", version = "1.5.30"))
+        classpath(kotlin("gradle-plugin", version = "1.5.31"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
     }
 }
