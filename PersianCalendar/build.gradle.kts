cb5753cecaf0fa3813952804cd2d4da7cb4dded9
Ebrahim Byagowi <ebrahim@gnu.org>
2021-11-18 19:00:12 +0330

    Remove German translation
    It isn't ready yet, let's have a rewrite from a volunteer.

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -59,8 +59,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         if (!isMinApi21Build) vectorDrawables.useSupportLibrary = true
         resourceConfigurations += listOf(
-            "en", "fa", "ckb", "ar", "ur", "ps", "glk", "azb", "ja", "fr", "es", "tr", "kmr", "tg",
-            "de"
+            "en", "fa", "ckb", "ar", "ur", "ps", "glk", "azb", "ja", "fr", "es", "tr", "kmr", "tg"
         )
         setProperty("archivesBaseName", "PersianCalendar-$versionName-$gitVersion")
     }
