4f36880d09f035780790ddcb279c14e7a19240fe
Ebrahim Byagowi <ebrahim@gnu.org>
2021-12-12 01:05:24 +0330

    Make progress circular indicators bounce

---
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/shared/BouncyCircularProgressIndicator.kt
@@ -0,0 +1,43 @@
+package com.byagowi.persiancalendar.ui.shared
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.BounceInterpolator
+import android.widget.FrameLayout
+import com.google.android.material.progressindicator.CircularProgressIndicator
+
+class BouncyCircularProgressIndicator(context: Context, attrs: AttributeSet? = null) :
+    FrameLayout(context, attrs), ValueAnimator.AnimatorUpdateListener {
+
+    private val progressIndicator = CircularProgressIndicator(context, attrs).also(::addView)
+
+    var max = 0
+        set(value) {
+            progressIndicator.max = value * accuracyFactor
+            field = value
+        }
+
+    private var animator: ValueAnimator? = null
+
+    var progress = 0
+        set(value) {
+            animator?.removeAllUpdateListeners()
+            ValueAnimator.ofInt(progressIndicator.progress / accuracyFactor, value).also {
+                animator = it
+                it.duration = animationDuration
+                it.interpolator = BounceInterpolator()
+                it.addUpdateListener(this)
+            }.start()
+            field = value
+        }
+
+    override fun onAnimationUpdate(animator: ValueAnimator?) {
+        progressIndicator.progress = ((animator?.animatedValue as? Int) ?: 0) * accuracyFactor
+    }
+
+    companion object {
+        private const val animationDuration = 1000L
+        private const val accuracyFactor = 100
+    }
+}
