c0307660c278360100af8864daf72592f5a7777b
Ebrahim Byagowi <ebrahim@gnu.org>
2021-12-18 03:37:52 +0330

    Add a realistic view of moon
    ic_moon.png is brought from https://github.com/ilius/starcal/blob/main/pixmaps/full_moon_128px.png

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/shared/SolarDraw.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/shared/SolarDraw.kt
@@ -1,8 +1,10 @@
 package com.byagowi.persiancalendar.ui.shared
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.RectF
 import android.os.Build
 import androidx.core.graphics.withScale
@@ -28,26 +30,31 @@ class SolarDraw(context: Context) {
     }
 
     private val sunDrawable = context.getCompatDrawable(R.drawable.ic_compass_sun)
+    private val moonBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.ic_moon)
+
+    private val ovalPath = Path()
 
     fun moon(
         canvas: Canvas, sunMoonPosition: SunMoonPosition, cx: Float, cy: Float, r: Float,
         moonPhase: Float = sunMoonPosition.moonPhase.toFloat()
     ) {
-        // canvas.withRotation(sunMoonPosition.moonPosition.azimuth.toFloat(), cx, cy) {
-        // moonDrawable.setBounds(
-        //     (cx - r).toInt(), (cy - r).toInt(), (cx + r).toInt(), (cy + r).toInt()
-        // )
-        // moonDrawable.draw(canvas)
-        // This is separated from QiblaCompassView with some modifications
+        moonRect.set(cx - r * 1.1f, cy - r * 1.1f, cx + r * 1.1f, cy + r * 1.1f)
+        canvas.drawBitmap(moonBitmap, null, moonRect, moonPaint)
         canvas.withScale(x = if (sunMoonPosition.moonPhaseAscending) -1f else 1f, pivotX = cx) {
             val arcWidth = (moonPhase - .5f) * 2 * r
             // elevation Offset 0 for 0 degree; r for 90 degree
             moonRect.set(cx - r, cy - r, cx + r, cy + r)
-            drawArc(moonRect, 90f, 180f, false, moonBrightPaint)
-            drawArc(moonRect, 270f, 180f, false, moonDarkPaint)
             moonOval.set(cx - abs(arcWidth), cy - r, cx + abs(arcWidth), cy + r)
-            val ovalPaint = if (arcWidth < 0) moonDarkPaint else moonBrightPaint
-            drawArc(moonOval, 0f, 360f, false, ovalPaint)
+            ovalPath.rewind()
+            if (arcWidth < 0) {
+                ovalPath.arcTo(moonOval, 90f, 180f)
+                ovalPath.arcTo(moonRect, 90f, -180f)
+            } else {
+                ovalPath.arcTo(moonOval, 90f, -180f)
+                ovalPath.arcTo(moonRect, 270f, 180f)
+            }
+            ovalPath.close()
+            drawPath(ovalPath, moonPaint)
         }
     }
 
@@ -56,13 +63,8 @@ class SolarDraw(context: Context) {
     private val moonRect = RectF()
     private val moonOval = RectF()
 
-    private val moonBrightPaint = Paint(Paint.ANTI_ALIAS_FLAG).also {
-        it.color = 0xFFDDE7EF.toInt()
-        it.style = Paint.Style.FILL_AND_STROKE
-    }
-
-    private val moonDarkPaint = Paint(Paint.ANTI_ALIAS_FLAG).also {
-        it.color = 0xFF22404C.toInt()
+    private val moonPaint = Paint(Paint.ANTI_ALIAS_FLAG).also {
+        it.color = 0x90000000.toInt()
         it.style = Paint.Style.FILL_AND_STROKE
     }
 
