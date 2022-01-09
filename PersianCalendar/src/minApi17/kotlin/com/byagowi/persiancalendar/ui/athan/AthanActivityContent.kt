005c3dac40501cf4269f8f960e107a33757ae6ef
Ebrahim Byagowi <ebrahim@gnu.org>
2022-01-09 13:55:14 +0330

    Add a fade in animation in Athan activity

--- PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/athan/AthanActivityContent.kt
+++ PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/athan/AthanActivityContent.kt
@@ -3,6 +3,8 @@ package com.byagowi.persiancalendar.ui.athan
 import androidx.activity.ComponentActivity
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.databinding.ActivityAthanBinding
+import com.byagowi.persiancalendar.ui.utils.fadeIn
+import com.byagowi.persiancalendar.utils.TWO_SECONDS_IN_MILLIS
 import com.byagowi.persiancalendar.utils.appPrefs
 import com.byagowi.persiancalendar.utils.cityName
 import com.byagowi.persiancalendar.utils.getPrayTimeName
@@ -11,6 +13,7 @@ fun setAthanActivityContent(activity: ComponentActivity, prayerKey: String, onCl
     val cityName = activity.appPrefs.cityName
     activity.setContentView(ActivityAthanBinding.inflate(activity.layoutInflater).also { binding ->
         binding.athanName.setText(getPrayTimeName(prayerKey))
+        binding.place.fadeIn(TWO_SECONDS_IN_MILLIS)
         binding.root.setOnClickListener { onClick() }
         binding.root.background = PatternDrawable(prayerKey)
         binding.place.text = cityName?.let { activity.getString(R.string.in_city_time, it) }
--- PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/athan/AthanActivityContent.kt
+++ PersianCalendar/src/minApi21/kotlin/com/byagowi/persiancalendar/ui/athan/AthanActivityContent.kt
@@ -2,20 +2,33 @@ package com.byagowi.persiancalendar.ui.athan
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,10 +61,26 @@ private fun AthanActivityContent(prayerKey: String, cityName: String?, onClick: 
                 shadow = Shadow(color = Color.Black, blurRadius = 2f, offset = Offset(1f, 1f))
             )
             Text(stringResource(getPrayTimeName(prayerKey)), fontSize = 36.sp, style = textStyle)
-            if (cityName != null) Text(
-                stringResource(R.string.in_city_time, cityName),
-                fontSize = 18.sp, style = textStyle, modifier = Modifier.padding(top = 10.dp)
-            )
+            if (cityName != null) {
+                val visible = remember { mutableStateOf(false) }
+                val density = LocalDensity.current
+                // Just an exaggerated demo for https://developer.android.com/jetpack/compose/animation#animatedvisibility
+                AnimatedVisibility(
+                    visible = visible.value,
+                    enter = slideInVertically { with(density) { -40.dp.roundToPx() } }
+                            + expandVertically(expandFrom = Alignment.Top)
+                            + fadeIn(initialAlpha = 0.3f),
+                    exit = slideOutVertically() + shrinkVertically() + fadeOut()
+                ) {
+                    Text(
+                        stringResource(R.string.in_city_time, cityName),
+                        fontSize = 18.sp,
+                        style = textStyle,
+                        modifier = Modifier.padding(top = 10.dp).fillMaxWidth().height(200.dp)
+                    )
+                }
+                visible.value = true
+            }
         }
     }
 }
