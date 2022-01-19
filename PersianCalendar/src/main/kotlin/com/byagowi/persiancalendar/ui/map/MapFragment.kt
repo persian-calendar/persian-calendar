92baf991f19131ef6a6aa36bab62e7974807d3c3
Ebrahim Byagowi <ebrahim@gnu.org>
2022-01-19 16:57:04 +0330

    [map] Generate maps on a separate thread

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapFragment.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapFragment.kt
@@ -15,29 +15,57 @@ import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
+import androidx.annotation.WorkerThread
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.graphics.PathParser
 import androidx.core.graphics.set
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.Variants.debugAssertNotNull
 import com.byagowi.persiancalendar.utils.calculatePrayTimes
+import com.byagowi.persiancalendar.utils.logException
 import io.github.persiancalendar.praytimes.Coordinates
-import io.github.persiancalendar.praytimes.PrayTimes
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
 import java.util.*
 import java.util.zip.GZIPInputStream
 
 class MapFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        // XXX: do it in a separate thread, don't show for low memory devices
+        val imageView = ZoomableImageView(inflater.context)
+
+        lifecycleScope.launch {
+            runCatching {
+                val bitmap = withContext(Dispatchers.IO) { createMap() }
+                imageView.setImageDrawable(BitmapDrawable(resources, bitmap))
+                val dayNightMap = withContext(Dispatchers.IO) { createDayNightMap(bitmap) }
+                imageView.setImageDrawable(BitmapDrawable(resources, dayNightMap))
+            }.onFailure(logException).getOrNull().debugAssertNotNull // handle production OOM and so
+        }
+
+        return imageView
+    }
+
+    @WorkerThread
+    private fun createMap(): Bitmap {
         val zippedMapPath = resources.openRawResource(R.raw.worldmap).use { it.readBytes() }
         val mapPathString = String(GZIPInputStream(ByteArrayInputStream(zippedMapPath)).readBytes())
         val mapPath = PathParser.createPathFromPathData(mapPathString)
+        val bitmap = Bitmap.createBitmap(4378, 2435, Bitmap.Config.ARGB_8888)
+        Canvas(bitmap).also {
+            it.drawPath(mapPath, Paint().apply { color = 0xffbcbcbc.toInt() })
+        }
+        return bitmap.copy(Bitmap.Config.ARGB_8888, false)
+    }
 
+    @WorkerThread
+    private fun createDayNightMap(originalMap: Bitmap): Bitmap {
         val nightMask = Bitmap.createBitmap(360, 180, Bitmap.Config.ARGB_8888)
         val calendar = GregorianCalendar()
         (-90 until 90).forEach { lat ->
@@ -48,19 +76,13 @@ class MapFragment : Fragment() {
                     nightMask[long + 180, lat + 90] = Color.BLACK
             }
         }
-        val bitmap = Bitmap.createBitmap(4378, 2435, Bitmap.Config.ARGB_8888)
-        Canvas(bitmap).also {
-            it.drawPath(mapPath, Paint().apply { color = 0xffbcbcbc.toInt() })
-            it.drawBitmap(
-                nightMask,
-                Rect(0, 0, nightMask.width, nightMask.height),
-                Rect(0, 0, bitmap.width, bitmap.height),
-                Paint().apply { alpha = 0xB0 }
-            )
+        val result = originalMap.copy(Bitmap.Config.ARGB_8888, true)
+        Canvas(result).also {
+            val maskRect = Rect(0, 0, nightMask.width, nightMask.height)
+            val originalRect = Rect(0, 0, result.width, result.height)
+            it.drawBitmap(nightMask, maskRect, originalRect, Paint().apply { alpha = 0xB0 })
         }
-        return ZoomableImageView(inflater.context).also {
-            it.setImageDrawable(BitmapDrawable(resources, bitmap))
-        }
+        return result
     }
 }
 
