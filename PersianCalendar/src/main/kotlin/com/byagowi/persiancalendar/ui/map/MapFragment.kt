27931d385e12dffbd4062ebd2cdc00dd2bb401d9
Ebrahim Byagowi <ebrahim@gnu.org>
2022-01-21 14:41:18 +0330

    [map] Avoid bitmap generation memory churn
    Its memory usage should become stable now.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapFragment.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/MapFragment.kt
@@ -1,15 +1,13 @@
 package com.byagowi.persiancalendar.ui.map
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.os.Bundle
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.WorkerThread
+import androidx.core.graphics.BitmapCompat
 import androidx.core.graphics.PathParser
 import androidx.core.graphics.set
 import androidx.core.graphics.withScale
@@ -29,6 +27,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
+import java.nio.ByteBuffer
 import java.util.*
 import java.util.zip.GZIPInputStream
 
@@ -76,41 +75,53 @@ class MapFragment : Fragment() {
         return binding.root
     }
 
-    var inProgress = false
+    private var inProgress = false
     private fun update(binding: FragmentMapBinding, date: GregorianCalendar) {
         lifecycleScope.launch {
             if (inProgress) return@launch
             inProgress = true
             runCatching {
-                val bitmap = withContext(Dispatchers.IO) { createDayNightMap(date) }
-                binding.map.setImageBitmap(bitmap)
+                binding.map.setImageBitmap(withContext(Dispatchers.IO) { createDayNightMap(date) })
                 binding.date.text = date.formatDateAndTime()
             }.onFailure(logException).getOrNull().debugAssertNotNull // handle production OOM and so
             inProgress = false
         }
     }
 
-    private val cachedMap by lazy {
+    private val scaleDownFactor = 4
+    private val sinkWidth = 360 * 16 / scaleDownFactor
+    private val sinkHeight = 180 * 16 / scaleDownFactor
+    private val sinkBitmap = Bitmap.createBitmap(sinkWidth, sinkHeight, Bitmap.Config.ARGB_8888)
+    private var referenceBuffer: ByteBuffer? = null
+    private fun createReferenceBuffer(): ByteBuffer {
         val zippedMapPath = resources.openRawResource(R.raw.worldmap).use { it.readBytes() }
         val mapPathString = String(GZIPInputStream(ByteArrayInputStream(zippedMapPath)).readBytes())
         val mapPath = PathParser.createPathFromPathData(mapPathString)
-        val scale = 4
-        val bitmap =
-            Bitmap.createBitmap(360 * 16 / scale, 180 * 16 / scale, Bitmap.Config.ARGB_8888)
-        Canvas(bitmap).also {
+        // We assume creating reference map will be first use of sink also.
+        Canvas(sinkBitmap).also {
             it.drawColor(0xFF809DB5.toInt())
-            it.withScale(1f / scale, 1f / scale) {
+            it.withScale(1f / scaleDownFactor, 1f / scaleDownFactor) {
                 it.drawPath(mapPath, Paint().apply { color = 0xFFFBF8E5.toInt() })
             }
         }
-        bitmap
+        val buffer = ByteBuffer.allocate(BitmapCompat.getAllocationByteCount(sinkBitmap))
+        sinkBitmap.copyPixelsToBuffer(buffer)
+        return buffer
+    }
+
+    private fun getSinkBitmap(): Bitmap {
+        val src = referenceBuffer ?: createReferenceBuffer().also { referenceBuffer = it }
+        sinkBitmap.copyPixelsFromBuffer(src.also { it.rewind() })
+        return sinkBitmap
     }
 
     private var solarDraw: SolarDraw? = null
 
+    private val nightMask = Bitmap.createBitmap(360, 180, Bitmap.Config.ALPHA_8)
+
     @WorkerThread
     private fun createDayNightMap(date: GregorianCalendar): Bitmap {
-        val nightMask = Bitmap.createBitmap(360, 180, Bitmap.Config.ALPHA_8)
+        nightMask.eraseColor(Color.TRANSPARENT)
         val sunPosition = SunMoonPositionForMap(date)
         var sunLat = .0f
         var sunLong = .0f
@@ -121,9 +132,8 @@ class MapFragment : Fragment() {
         (-90 until 90).forEach { lat ->
             (-180 until 180).forEach { long ->
                 val sunAltitude = sunPosition.sunAltitude(lat.toDouble(), long.toDouble())
-                if (sunAltitude < 0)
-                    nightMask[long + 180, 179 - (lat + 90)] =
-                        (-sunAltitude.toInt()).coerceAtMost(17) * 5 shl 24
+                if (sunAltitude < 0) nightMask[long + 180, 179 - (lat + 90)] =
+                    (-sunAltitude.toInt()).coerceAtMost(17) * 5 shl 24
                 if (sunAltitude > sunAlt) { // find lat/long of a point with maximum sun altitude
                     sunAlt = sunAltitude; sunLat = 179f - (lat + 90); sunLong = long + 180f
                 }
@@ -133,13 +143,13 @@ class MapFragment : Fragment() {
                 }
             }
         }
-        val result = cachedMap.copy(Bitmap.Config.ARGB_8888, true)
-        Canvas(result).also {
-            it.drawBitmap(nightMask, null, Rect(0, 0, result.width, result.height), null)
-            val scale = result.width / 360
+        val sink = getSinkBitmap()
+        Canvas(sink).also {
+            it.drawBitmap(nightMask, null, Rect(0, 0, sink.width, sink.height), null)
+            val scale = sink.width / 360
             solarDraw?.sun(it, sunLong * scale, sunLat * scale, scale * 12.5f)
             solarDraw?.simpleMoon(it, moonLong * scale, moonLat * scale, scale * 8f)
         }
-        return result
+        return sink
     }
 }
