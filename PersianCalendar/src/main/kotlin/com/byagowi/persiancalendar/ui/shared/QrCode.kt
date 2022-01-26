9c3025997f60683eb9ec0adb0003a8d4f994891d
Ebrahim Byagowi <ebrahim@gnu.org>
2022-01-27 00:02:50 +0330

    Add qrcode encode code
    Not used yet though and had 70kb size overhead so probably better
    to write our own or a customized one instead using zxing's.

---
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/shared/QrCode.kt
@@ -0,0 +1,33 @@
+package com.byagowi.persiancalendar.ui.shared
+
+import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.graphics.set
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+
+private fun textToQrCodeBitmap(text: String): Bitmap {
+    val size = 768
+    val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+    val bitMatrix = QRCodeWriter().encode(text, BarcodeFormat.QR_CODE, size, size, mapOf(
+        EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.M,
+        EncodeHintType.MARGIN to 0
+    ))
+    (0 until bitMatrix.height).forEach { y ->
+        (0 until bitMatrix.width).forEach { x ->
+            bitmap[x, y] = if (bitMatrix[x, y]) Color.BLACK else Color.TRANSPARENT
+        }
+    }
+    return bitmap
+}
+
+fun showQrCode(activity: Activity, text: String) {
+    AlertDialog.Builder(activity)
+        .setView(ImageView(activity).also { it.setImageBitmap(textToQrCodeBitmap(text)) })
+        .show()
+}
