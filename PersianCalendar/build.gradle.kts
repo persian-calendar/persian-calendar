c163e5cfbe0551166588473e59a42d6f3d281f07
Ebrahim Byagowi <ebrahim@gnu.org>
2022-01-27 14:53:44 +0330

    Upgrade navigation to 2.4.0
    Fixes #845
    Fixes #844
    Fixes #841

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -180,8 +180,8 @@ dependencies {
     implementation("androidx.dynamicanimation:dynamicanimation:1.0.0")
     implementation("com.google.android.material:material:1.5.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
