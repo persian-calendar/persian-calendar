ee06df2742211c3f3cfcb71acb4a8e3e681ba4d8
Ebrahim Byagowi <ebrahim@gnu.org>
2022-02-14 15:13:25 +0330

    Remove AGP resource removing bug workaround
    Should've been fixed now.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
@@ -1,20 +1,11 @@
 package com.byagowi.persiancalendar
 
 import android.app.Application
-import androidx.annotation.Keep
 import com.byagowi.persiancalendar.global.initGlobal
 
 class MainApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
-        initGlobal(applicationContext) // Used for things should be provided in locale level
+        initGlobal(applicationContext) // mostly used for things should be provided in locale level
     }
-
-    // Can I haz these resources not removed?!
-    // Workaround for weird AGP 4.1.0 >= used resource removal issues
-    @Keep
-    private val heyAndroidBuildToolsWeNeedTheseAndItIsUnbelievableYouAreRemovingThem = listOf(
-        R.drawable.blue_shade_background, R.raw.abdulbasit, R.raw.special
-    )
 }
