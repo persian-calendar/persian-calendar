d4860bf967b46311b3a19b318233fc4b07882ed3
Ebrahim Byagowi <ebrahim@gnu.org>
2022-02-23 02:22:05 +0330

    Tweak Chinese month/year format
    Related to #851

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/Language.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/Language.kt
@@ -69,7 +69,7 @@ enum class Language(val code: String, val nativeName: String) {
     val my: String
         get() = when (this) {
             CKB -> "%1\$sی %2\$s"
-            JA, ZH_CN -> "%2\$s %1\$s"
+            ZH_CN -> "%2\$s 年 %1\$s"
             else -> "%1\$s %2\$s"
         }
     val timeAndDateFormat: String
