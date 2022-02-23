a077ea63d82a1c4b292bec1114412cbb5cff4011
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-02-23 16:03:59 +0330

    Add firebase analytics, performance & crashlytics for nightly & minapi 21 nightly builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,9 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.1.2")
         classpath(kotlin("gradle-plugin", version = "1.6.10"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:perf-plugin:1.4.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
     }
 }
 
