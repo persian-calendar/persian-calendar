8514fddaf4a5494a6e1631a9f11a105672dcd686
Ebrahim Byagowi <ebrahim@gnu.org>
2022-02-24 20:24:50 +0330

    Add Chinese Zodiac compatibilities
    Not used yet and maybe never will but we will see.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/astronomy/ChineseZodiac.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/astronomy/ChineseZodiac.kt
@@ -6,9 +6,7 @@ import androidx.annotation.StringRes
 import com.byagowi.persiancalendar.R
 import io.github.persiancalendar.calendar.PersianDate
 
-// https://en.wikipedia.org/wiki/Chinese_zodiac
-// TODO: Implement 'Chinese Zodiac Compatibility-Conflict-Harm' maybe
-enum class ChineseZodiac(@StringRes val title: Int, val emoji: String) {
+enum class ChineseZodiac(@StringRes private val title: Int, private val emoji: String) {
     MONKEY(R.string.year1, "🐒"),
     ROOSTER(R.string.year2, "🐔"),
     DOG(R.string.year3, "🐕"),
@@ -27,9 +25,56 @@ enum class ChineseZodiac(@StringRes val title: Int, val emoji: String) {
         append(context.getString(title))
     }
 
+    val bestMatches get() = bestMatchesRaw[ordinal]
+    val averageMatches get() = averageMatchesRaw[ordinal]
+    val poorMatch get() = poorMatchRaw[ordinal]
+    val harmfulMatch get() = harmfulMatchRaw[ordinal]
+
     companion object {
-        // e.g. see 'The Chinese-Uighur Animal Calendar in Persian Historiography of the Mongol Period'
+        /**
+         * See also:
+         * * The Chinese-Uighur Animal Calendar in Persian Historiography of the Mongol Period
+         * * https://en.wikipedia.org/wiki/Chinese_zodiac
+         * * https://fa.wikipedia.org/wiki/گاه‌شماری_حیوانی
+         */
         fun fromPersianCalendar(persianDate: PersianDate): ChineseZodiac =
             values().getOrNull((persianDate.year + 9) % 12) ?: MONKEY
+
+        // Compatibilities, https://en.wikipedia.org/wiki/Chinese_zodiac#Compatibility
+        // They should be turned into formula eventually
+        private val bestMatchesRaw = listOf(
+            setOf(DRAGON, MONKEY, RAT),
+            setOf(SNAKE, ROOSTER, OX),
+            setOf(HORSE, DOG, TIGER),
+            setOf(PIG, GOAT, RABBIT),
+            setOf(RAT, MONKEY, DRAGON),
+            setOf(OX, ROOSTER, SNAKE),
+            setOf(DOG, TIGER, HORSE),
+            setOf(RABBIT, PIG, GOAT),
+            setOf(DRAGON, RAT, MONKEY),
+            setOf(OX, SNAKE, ROOSTER),
+            setOf(TIGER, HORSE, DOG),
+            setOf(RABBIT, GOAT, PIG)
+        )
+        private val averageMatchesRaw = listOf(
+            setOf(PIG, TIGER, DOG, SNAKE, RABBIT, ROOSTER, OX),
+            setOf(MONKEY, DOG, RABBIT, TIGER, DRAGON, PIG, RAT),
+            setOf(RABBIT, DRAGON, ROOSTER, RAT, GOAT, OX, PIG),
+            setOf(TIGER, MONKEY, DOG, OX, HORSE, RAT, SNAKE),
+            setOf(TIGER, SNAKE, HORSE, GOAT, PIG, OX, ROOSTER),
+            setOf(HORSE, DRAGON, GOAT, DOG, RABBIT, RAT, MONKEY),
+            setOf(SNAKE, RABBIT, DRAGON, ROOSTER, PIG, MONKEY, GOAT),
+            setOf(SNAKE, RABBIT, DRAGON, MONKEY, ROOSTER, DOG, TIGER),
+            setOf(DRAGON, DOG, OX, GOAT, RABBIT, ROOSTER, HORSE),
+            setOf(HORSE, SNAKE, GOAT, PIG, TIGER, MONKEY, RAT),
+            setOf(MONKEY, PIG, RAT, OX, SNAKE, GOAT, RABBIT),
+            setOf(RAT, ROOSTER, DOG, DRAGON, HORSE, OX, TIGER)
+        )
+        private val poorMatchRaw = listOf(
+            HORSE, GOAT, MONKEY, ROOSTER, DOG, PIG, RAT, OX, TIGER, RABBIT, DRAGON, SNAKE
+        )
+        private val harmfulMatchRaw = listOf(
+            GOAT, HORSE, SNAKE, DRAGON, RABBIT, TIGER, OX, RAT, PIG, DOG, ROOSTER, MONKEY
+        )
     }
 }
