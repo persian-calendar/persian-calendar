897f3db6650c2a762ae256497a420023bc9d9c35
Ebrahim Byagowi <ebrahim@gnu.org>
2022-02-26 23:56:12 +0330

    Use KotlinPoet for code generations
    Those CodeBlock uses might need a rewrite also.

--- gradlePlugins/src/main/java/io/github/persiancalendar/gradle/CodeGenerators.kt
+++ gradlePlugins/src/main/java/io/github/persiancalendar/gradle/CodeGenerators.kt
@@ -1,5 +1,15 @@
 package io.github.persiancalendar.gradle
 
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.typeNameOf
 import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -17,32 +27,42 @@ class CodeGenerators : Plugin<Project> {
             val generateDir =
                 generatedAppSrcDir / "com" / "byagowi" / "persiancalendar" / "generated"
 
-            @OptIn(ExperimentalStdlibApi::class)
-            val actions = buildList {
-                add(generateDir::mkdirs)
-                listOf(
-                    "events" to ::generateEventsCode,
-                    "cities" to ::generateCitiesCode,
-                    "districts" to ::generateDistrictsCode
-                ).map { (name, generator) ->
-                    val input = projectDir / "data" / "$name.json"
-                    inputs.file(input)
-                    val output = generateDir / "${name.capitalized()}.kt"
-                    outputs.file(output)
-                    add { output.writeText(generator(input)) }
-                }
+            val actions = listOf(
+                "events" to ::generateEventsCode,
+                "cities" to ::generateCitiesCode,
+                "districts" to ::generateDistrictsCode
+            )
+            actions.forEach { (name, _) ->
+                val input = projectDir / "data" / "$name.json"
+                inputs.file(input)
+                val output = generateDir / "${name.capitalized()}.kt"
+                outputs.file(output)
             }
 
-            doLast { actions.forEach { it() } }
+            doLast {
+                actions.forEach { (name, generator) ->
+                    val input = projectDir / "data" / "$name.json"
+                    val builder = FileSpec.builder(
+                        "com.byagowi.persiancalendar.generated",
+                        name.capitalized()
+                    )
+                    generator(input, builder)
+                    builder.build().writeTo(generatedAppSrcDir)
+                }
+            }
         }
     }
 
-    private fun generateEventsCode(eventsJson: File): String {
+    // "·" prevents line wraps in KotlinPoet
+    // https://square.github.io/kotlinpoet/#spaces-wrap-by-default
+    private fun String.preventLineWraps(): String = this.replace(" ", "·")
+
+    private fun generateEventsCode(eventsJson: File, builder: FileSpec.Builder) {
         val events = JsonSlurper().parse(eventsJson) as Map<*, *>
         val (persianEvents, islamicEvents, gregorianEvents, nepaliEvents) = listOf(
             "Persian Calendar", "Hijri Calendar", "Gregorian Calendar", "Nepali Calendar"
         ).map { key ->
-            (events[key] as List<*>).joinToString(",\n    ") {
+            (events[key] as List<*>).joinToString(",\n") {
                 val record = it as Map<*, *>
                 "CalendarRecord(title = \"${record["title"]}\"," +
                         " type = EventType.${record["type"]}," +
@@ -52,42 +72,117 @@ class CodeGenerators : Plugin<Project> {
         }
         val irregularRecurringEvents = (events["Irregular Recurring"] as List<*>)
             .mapNotNull { it as Map<*, *> }
-            .joinToString(",\n    ") { event ->
+            .joinToString(",\n") { event ->
                 "mapOf(${event.map { (k, v) -> """"$k" to "$v"""" }.joinToString(", ")})"
             }
-        val eventsSource = (events["Source"] as Map<*, *>).toList()
-            .joinToString(",\n    ") { (k, v) -> """$k("$v")""" }
-        return """package com.byagowi.persiancalendar.generated
-
-enum class EventType(val source: String) {
-    $eventsSource
-}
-
-class CalendarRecord(val title: String, val type: EventType, val isHoliday: Boolean, val month: Int, val day: Int)
-
-val persianEvents = listOf<CalendarRecord>(
-    $persianEvents
-)
-
-val islamicEvents = listOf<CalendarRecord>(
-    $islamicEvents
-)
-
-val gregorianEvents = listOf<CalendarRecord>(
-    $gregorianEvents
-)
-
-val nepaliEvents = listOf<CalendarRecord>(
-    $nepaliEvents
-)
-
-val irregularRecurringEvents = listOf(
-    $irregularRecurringEvents
-)
-"""
+        builder.addType(
+            TypeSpec.enumBuilder("EventType")
+                .primaryConstructor(
+                    FunSpec.constructorBuilder()
+                        .addParameter("source", String::class)
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("source", String::class, KModifier.PUBLIC)
+                        .initializer("source")
+                        .build()
+                )
+                .also {
+                    (events["Source"] as Map<*, *>).toList().forEach { (name, source) ->
+                        it.addEnumConstant(
+                            name as String, TypeSpec.anonymousClassBuilder()
+                                .addSuperclassConstructorParameter("%S", source as String)
+                                .build()
+                        )
+                    }
+                }
+                .build()
+        )
+        builder.addType(
+            TypeSpec.classBuilder("CalendarRecord")
+                .primaryConstructor(
+                    FunSpec.constructorBuilder()
+                        .addParameter("title", String::class)
+                        .addParameter(
+                            "type",
+                            ClassName("com.byagowi.persiancalendar.generated", "EventType")
+                        )
+                        .addParameter("isHoliday", Boolean::class)
+                        .addParameter("month", Int::class)
+                        .addParameter("day", Int::class)
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("title", String::class, KModifier.PUBLIC)
+                        .initializer("title")
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec
+                        .builder(
+                            "type",
+                            ClassName("com.byagowi.persiancalendar.generated", "EventType"),
+                            KModifier.PUBLIC
+                        )
+                        .initializer("type")
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("isHoliday", Boolean::class, KModifier.PUBLIC)
+                        .initializer("isHoliday")
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("month", Int::class, KModifier.PUBLIC)
+                        .initializer("month")
+                        .build()
+                )
+                .addProperty(
+                    PropertySpec.builder("day", Int::class, KModifier.PUBLIC)
+                        .initializer("day")
+                        .build()
+                )
+                .build()
+        )
+        val calendarRecordList = List::class.asClassName().parameterizedBy(
+            ClassName("com.byagowi.persiancalendar.generated", "CalendarRecord")
+        )
+        builder.addProperty(
+            PropertySpec
+                .builder("persianEvents", calendarRecordList)
+                .initializer(CodeBlock.of("listOf(\n$persianEvents\n)".preventLineWraps()))
+                .build()
+        )
+        builder.addProperty(
+            PropertySpec
+                .builder("islamicEvents", calendarRecordList)
+                .initializer(CodeBlock.of("listOf(\n$islamicEvents\n)".preventLineWraps()))
+                .build()
+        )
+        builder.addProperty(
+            PropertySpec
+                .builder("gregorianEvents", calendarRecordList)
+                .initializer(CodeBlock.of("listOf(\n$gregorianEvents\n)".preventLineWraps()))
+                .build()
+        )
+        builder.addProperty(
+            PropertySpec
+                .builder("nepaliEvents", calendarRecordList)
+                .initializer(CodeBlock.of("listOf(\n$nepaliEvents\n)".preventLineWraps()))
+                .build()
+        )
+        @OptIn(ExperimentalStdlibApi::class)
+        builder.addProperty(
+            PropertySpec
+                .builder("irregularRecurringEvents", typeNameOf<List<Map<String, String>>>())
+                .initializer(
+                    CodeBlock.of("listOf(\n$irregularRecurringEvents\n)".preventLineWraps())
+                )
+                .build()
+        )
     }
 
-    private fun generateCitiesCode(citiesJson: File): String {
+    private fun generateCitiesCode(citiesJson: File, builder: FileSpec.Builder) {
         val cities = (JsonSlurper().parse(citiesJson) as Map<*, *>).flatMap { countryEntry ->
             val countryCode = countryEntry.key as String
             val country = countryEntry.value as Map<*, *>
@@ -100,34 +195,38 @@ val irregularRecurringEvents = listOf(
                 val elevation =
                     if (countryCode == "ir") .0 else (city["elevation"] as Number).toDouble()
                 """"$key" to CityItem(
-            key = "$key",
-            en = "${city["en"]}", fa = "${city["fa"]}",
-            ckb = "${city["ckb"]}", ar = "${city["ar"]}",
-            countryCode = "$countryCode",
-            countryEn = "${country["en"]}", countryFa = "${country["fa"]}",
-            countryCkb = "${country["ckb"]}", countryAr = "${country["ar"]}",
-            coordinates = Coordinates($latitude, $longitude, $elevation)
-        )"""
+    key = "$key",
+    en = "${city["en"]}", fa = "${city["fa"]}",
+    ckb = "${city["ckb"]}", ar = "${city["ar"]}",
+    countryCode = "$countryCode",
+    countryEn = "${country["en"]}", countryFa = "${country["fa"]}",
+    countryCkb = "${country["ckb"]}", countryAr = "${country["ar"]}",
+    coordinates = Coordinates($latitude, $longitude, $elevation)
+)"""
             }
-        }.joinToString(",\n    ")
-        return """package com.byagowi.persiancalendar.generated
-
-import com.byagowi.persiancalendar.entities.CityItem
-import io.github.persiancalendar.praytimes.Coordinates
-
-val citiesStore = mapOf(
-    $cities
-)
-"""
+        }.joinToString(",\n")
+        val cityItem = ClassName("com.byagowi.persiancalendar.entities", "CityItem")
+        builder.addImport("com.byagowi.persiancalendar.entities", "CityItem")
+        builder.addImport("io.github.persiancalendar.praytimes", "Coordinates")
+        builder.addProperty(
+            PropertySpec
+                .builder(
+                    "citiesStore", Map::class.asClassName().parameterizedBy(
+                        String::class.asClassName().copy(nullable = true), cityItem
+                    )
+                )
+                .initializer(CodeBlock.of("mapOf(\n$cities\n)".preventLineWraps()))
+                .build()
+        )
     }
 
-    private fun generateDistrictsCode(districtsJson: File): String {
+    private fun generateDistrictsCode(districtsJson: File, builder: FileSpec.Builder) {
         val districts = (JsonSlurper().parse(districtsJson) as Map<*, *>).mapNotNull { province ->
             val provinceName = province.key as String
             if (provinceName.startsWith("#")) return@mapNotNull null
             "\"$provinceName\" to listOf(\n" + (province.value as Map<*, *>).map { county ->
                 val key = county.key as String
-                """       "$key;""" + (county.value as Map<*, *>).map { district ->
+                """    "$key;""" + (county.value as Map<*, *>).map { district ->
                     val coordinates = district.value as Map<*, *>
                     val latitude = (coordinates["lat"] as Number).toDouble()
                     val longitude = (coordinates["long"] as Number).toDouble()
@@ -135,13 +234,13 @@ val citiesStore = mapOf(
                     val name = district.key.toString().split("(")[0]
                     "$name:$latitude:$longitude"
                 }.joinToString(";") + "\""
-            }.joinToString(",\n") + "\n    )"
-        }.joinToString(",\n    ")
-        return """package com.byagowi.persiancalendar.generated
-
-val districtsStore = listOf(
-    $districts
-)
-"""
+            }.joinToString(",\n") + "\n)"
+        }.joinToString(",\n")
+        @OptIn(ExperimentalStdlibApi::class)
+        builder.addProperty(
+            PropertySpec.builder("districtsStore", typeNameOf<List<Pair<String, List<String>>>>())
+                .initializer(CodeBlock.of("listOf(\n$districts\n)".preventLineWraps()))
+                .build()
+        )
     }
 }
