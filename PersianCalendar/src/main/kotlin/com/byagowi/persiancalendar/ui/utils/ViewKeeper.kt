0dcd54bdad2fd8e1fcf12f251ae720d571018529
Ebrahim Byagowi <ebrahim@gnu.org>
2022-04-04 01:24:28 +0430

    Extend commenting of viewKeeper and a little simplification

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/utils/ViewKeeper.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/utils/ViewKeeper.kt
@@ -3,20 +3,26 @@ package com.byagowi.persiancalendar.ui.utils
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KProperty
 
 /**
+ * This relies on view ability to hold arbitrary objects, uses field name hash to cache
+ * result of given lambda query from the view and calls 'bind' lambda lazily
+ * when it misses view's cache.
+ *
+ * As it stores lambda's result inside the view's cache it doesn't need a clean up
+ * on fragment's view lifecycle as the view itself will be purged along the cache store
+ * and whenever the field is needed again and it misses the cache it will execute the
+ * 'bind' lambda again.
+ *
+ * It is inspired from https://github.com/wada811/ViewBinding-ktx
+ *
  * Created by Farhad Beigirad on 3/1/22.
  */
-// inspired by https://github.com/wada811/ViewBinding-ktx
-fun <T> Fragment.viewKeeper(bind: (View) -> T): ReadOnlyProperty<Fragment, T> {
-    return object : ReadOnlyProperty<Fragment, T> {
+fun <T> viewKeeper(bind: (View) -> T): ReadOnlyProperty<Fragment, T> {
+    return ReadOnlyProperty { thisRef, property ->
+        val tag = property.name.hashCode()
+        val view = thisRef.requireView()
         @Suppress("UNCHECKED_CAST")
-        override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-            (requireView().getTag(property.name.hashCode()) as? T)?.let { return it }
-            return bind(requireView()).also {
-                requireView().setTag(property.name.hashCode(), it)
-            }
-        }
+        view.getTag(tag) as? T ?: bind(view).also { view.setTag(tag, it) }
     }
 }
