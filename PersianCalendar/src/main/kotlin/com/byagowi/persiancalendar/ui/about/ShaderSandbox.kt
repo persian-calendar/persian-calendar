b40c9ca4e58bdbc4a3c78a62df33efee0a74b895
Ebrahim Byagowi <ebrahim@gnu.org>
2022-04-04 12:44:53 +0430

    gl: Add GLSurfaceView after dialog initiation
    Hopefully that makes its enter more reliable

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/ShaderSandbox.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/ShaderSandbox.kt
@@ -1,6 +1,7 @@
 package com.byagowi.persiancalendar.ui.about
 
 import android.opengl.GLSurfaceView
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.FragmentActivity
@@ -10,19 +11,23 @@ import com.byagowi.persiancalendar.ui.map.GLRenderer
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 fun showShaderSandboxDialog(activity: FragmentActivity) {
-    val binding = ShaderSandboxBinding.inflate(activity.layoutInflater)
-    binding.glView.setEGLContextClientVersion(2)
-    val renderer = GLRenderer(onError = {
-        activity.runOnUiThread { Toast.makeText(activity, it, Toast.LENGTH_LONG).show() }
-    })
-    binding.glView.setRenderer(renderer)
-    binding.glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
-    binding.inputText.doAfterTextChanged {
-        renderer.fragmentShader = binding.inputText.text?.toString() ?: ""
-        binding.glView.queueEvent { renderer.compileProgram(); binding.glView.requestRender() }
+    val frame = FrameLayout(activity)
+    frame.post {
+        val binding = ShaderSandboxBinding.inflate(activity.layoutInflater)
+        binding.glView.setEGLContextClientVersion(2)
+        val renderer = GLRenderer(onError = {
+            activity.runOnUiThread { Toast.makeText(activity, it, Toast.LENGTH_LONG).show() }
+        })
+        binding.glView.setRenderer(renderer)
+        binding.glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+        binding.inputText.doAfterTextChanged {
+            renderer.fragmentShader = binding.inputText.text?.toString() ?: ""
+            binding.glView.queueEvent { renderer.compileProgram(); binding.glView.requestRender() }
+        }
+        binding.inputText.setText(sandboxFragmentShader)
+        frame.addView(binding.root)
     }
-    binding.inputText.setText(sandboxFragmentShader)
     MaterialAlertDialogBuilder(activity)
-        .setView(binding.root)
+        .setView(frame)
         .show()
 }
--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/GlobeDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/GlobeDialog.kt
@@ -2,20 +2,25 @@ package com.byagowi.persiancalendar.ui.map
 
 import android.graphics.Bitmap
 import android.opengl.GLSurfaceView
+import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 import com.byagowi.persiancalendar.generated.globeFragmentShader
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 fun showGlobeDialog(activity: FragmentActivity, image: Bitmap) {
-    val glView = GLSurfaceView(activity)
-    glView.setOnClickListener { glView.requestRender() }
-    glView.setEGLContextClientVersion(2)
-    val renderer = GLRenderer(onSurfaceCreated = { it.loadTexture(image) })
-    glView.setRenderer(renderer)
-    glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
-    renderer.fragmentShader = globeFragmentShader
-    val dialog = BottomSheetDialog(activity)
-    dialog.setContentView(glView)
-    dialog.show()
+    val frame = FrameLayout(activity)
+    frame.post {
+        val glView = GLSurfaceView(activity)
+        glView.setOnClickListener { glView.requestRender() }
+        glView.setEGLContextClientVersion(2)
+        val renderer = GLRenderer(onSurfaceCreated = { it.loadTexture(image) })
+        glView.setRenderer(renderer)
+        glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+        renderer.fragmentShader = globeFragmentShader
+        frame.addView(glView)
+    }
+    MaterialAlertDialogBuilder(activity)
+        .setView(frame)
+        .show()
 }
