f7d80179321950a3bb90bf4b19fd5f6af7d7880b
Ebrahim Byagowi <ebrahim@gnu.org>
2022-05-12 23:19:51 +0430

    Upgrade Android gradle plugin to 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.1.3" apply false
-    id("com.android.library") version "7.1.3" apply false
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.6.20" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.4.2" apply false
     id("com.google.gms.google-services") version "4.3.10" apply false
