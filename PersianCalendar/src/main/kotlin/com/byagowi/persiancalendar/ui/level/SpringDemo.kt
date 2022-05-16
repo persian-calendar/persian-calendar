7ca3ff5d1c3138eb411c63b9ab9c39af21633103
Ebrahim Byagowi <ebrahim@gnu.org>
2022-05-17 02:50:04 +0430

    Add a demo for SpringAnimation
    Not used anywhere yet but to experiment with the API.
    
    The experiment has indirectly been useful as I discovered animation API of
    CircularProgressIndicator and the removal of our own extension but I look
    forward also to find somewhere to use this one also :)

---
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/level/SpringDemo.kt
@@ -0,0 +1,74 @@
+package com.byagowi.persiancalendar.ui.level
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+import androidx.dynamicanimation.animation.FloatValueHolder
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun showSpringDemoDialog(activity: FragmentActivity) {
+    val x = FloatValueHolder()
+    val horizontalSpring = SpringAnimation(x)
+    horizontalSpring.spring = SpringForce(0f)
+        .setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY)
+        .setStiffness(SpringForce.STIFFNESS_LOW)
+    val y = FloatValueHolder()
+    val verticalSpring = SpringAnimation(y)
+    verticalSpring.spring = SpringForce(0f)
+        .setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY)
+        .setStiffness(SpringForce.STIFFNESS_LOW)
+    val view = object : View(activity) {
+        private var r = 0f
+        private var previousX = 0f
+        private var previousY = 0f
+
+        init {
+            horizontalSpring.addUpdateListener { _, _, _ -> invalidate() }
+            verticalSpring.addUpdateListener { _, _, _ -> invalidate() }
+        }
+
+        override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+            super.onSizeChanged(w, h, oldw, oldh)
+            x.value = w / 2f
+            y.value = h / 2f
+            r = w / 20f
+        }
+
+        private val paint = Paint(Paint.ANTI_ALIAS_FLAG).also { it.color = Color.GRAY }
+        override fun onDraw(canvas: Canvas) {
+            canvas.drawCircle(x.value, y.value, r, paint)
+        }
+
+        override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    horizontalSpring.cancel()
+                    verticalSpring.cancel()
+                    previousX = event.x
+                    previousY = event.y
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    x.value += event.x - previousX
+                    y.value += event.y - previousY
+                    previousX = event.x
+                    previousY = event.y
+                    invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    horizontalSpring.animateToFinalPosition(width / 2f)
+                    verticalSpring.animateToFinalPosition(height / 2f)
+                }
+            }
+            return true
+        }
+    }
+
+    MaterialAlertDialogBuilder(activity)
+        .setView(view)
+        .show()
+}
