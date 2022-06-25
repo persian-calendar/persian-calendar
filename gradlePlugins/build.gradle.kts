6c6edb143fdb99f8d0041ec775b9e1a75fa14d8b
Ebrahim Byagowi <ebrahim@gnu.org>
2022-06-25 18:35:29 +0430

    Upgrade kotlinx serialization used in build dependency

--- gradlePlugins/build.gradle.kts
+++ gradlePlugins/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("com.squareup:kotlinpoet:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
 gradlePlugin {
