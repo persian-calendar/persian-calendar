e7ecb135b8c562549ee93451cb2385f3ba6d12b1
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-17 23:40:53 +0430

    Use a ValueSource for gitVersion

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -52,16 +52,8 @@ android {
         viewBinding = true
     }
 
-    val gitVersion = listOf(
-        "git rev-parse --abbrev-ref HEAD", // branch, e.g. main
-        "git rev-list HEAD --count", // number of commits in history, e.g. 3724
-        "git rev-parse --short HEAD", // git hash, e.g. 2426d51f
-        "git status -s" // i == 3, whether repo's dir is clean, -dirty is appended if smt is uncommitted
-    ).mapIndexedNotNull { i, cmd ->
-        providers.exec {
-            commandLine(cmd.split(" "))
-        }.standardOutput.asText.get().trim().takeIf { it.isNotEmpty() }.let { if (i == 3 && it != null) "dirty" else it }
-    }.joinToString("-")
+    val gitVersionProvider = providers.of(io.github.persiancalendar.gradle.GitVersionValueSource::class) {}
+    val gitVersion = gitVersionProvider.get()
 
     defaultConfig {
         applicationId = "com.byagowi.persiancalendar"
---
+++ gradlePlugins/src/main/kotlin/io/github/persiancalendar/gradle/GitVersionValueSource.kt
@@ -0,0 +1,31 @@
+package io.github.persiancalendar.gradle
+
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+import org.gradle.process.ExecOperations
+import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
+import javax.inject.Inject
+
+abstract class GitVersionValueSource : ValueSource<String, ValueSourceParameters.None> {
+    @get:Inject
+    abstract val execOperations: ExecOperations
+
+    override fun obtain(): String {
+        val output = ByteArrayOutputStream()
+        val gitVersion = listOf(
+            "git rev-parse --abbrev-ref HEAD", // branch, e.g. main
+            "git rev-list HEAD --count", // number of commits in history, e.g. 3724
+            "git rev-parse --short HEAD", // git hash, e.g. 2426d51f
+            "git status -s" // i == 3, whether repo's dir is clean, -dirty is appended if smt is uncommitted
+        ).mapIndexedNotNull { i, cmd ->
+            execOperations.exec {
+                commandLine(cmd.split(" "))
+                standardOutput = output
+            }
+            String(output.toByteArray(), Charset.defaultCharset()).trim().takeIf { it.isNotEmpty() }
+                .let { if (i == 3 && it != null) "dirty" else it }.also { output.reset() }
+        }.joinToString("-")
+        return gitVersion
+    }
+}
