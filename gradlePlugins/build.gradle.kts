794724195f82fed88c3aead316e700b297503058
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-08-12 15:54:07 +0430

    Update gradlePlugins' kotlin

--- gradlePlugins/build.gradle.kts
+++ gradlePlugins/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 repositories {
