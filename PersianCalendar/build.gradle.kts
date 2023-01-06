29808f0de94503e937fc0a303f5d17b6107eab87
Ebrahim Byagowi <ebrahim@gnu.org>
2023-01-06 21:42:57 +0330

    Update material library to 1.8.0-rc01

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -168,7 +168,7 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.dynamicanimation:dynamicanimation:1.0.0")
-    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("com.google.android.material:material:1.8.0-rc01")
 
     val navVersion = "2.5.1"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
