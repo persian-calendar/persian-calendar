19cc86115da3b2d452ba5d91138c63c48242c1e4
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2023-02-07 16:19:21 +0330

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "7.3.1" apply false
+    id("com.android.application") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
 }
