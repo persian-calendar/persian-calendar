ebbbb0e586745f80c492d88ba6a85a822540d8e3
Ebrahim Byagowi <ebrahim@gnu.org>
2023-02-07 21:37:21 +0330

    Update compose material3

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -203,7 +203,7 @@ dependencies {
     minApi21Implementation("com.google.accompanist:accompanist-drawablepainter:$accompanistVersion")
     minApi21Implementation("com.google.accompanist:accompanist-themeadapter-material3:$accompanistVersion")
     minApi21Implementation("androidx.compose.ui:ui:$composeVersion")
-    minApi21Implementation("androidx.compose.material3:material3:1.1.0-alpha04")
+    minApi21Implementation("androidx.compose.material3:material3:1.1.0-alpha05")
     minApi21Implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     if (isMinApi21Build) {
         implementation("androidx.compose.runtime:runtime:$composeVersion")
