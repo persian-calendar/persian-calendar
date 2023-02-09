3a111f891105907c410992d42d55d25dcaf61ba4
Ebrahim Byagowi <ebrahim@gnu.org>
2023-02-09 08:39:55 +0330

    Upgrade Compose Compiler and Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
 }
 
