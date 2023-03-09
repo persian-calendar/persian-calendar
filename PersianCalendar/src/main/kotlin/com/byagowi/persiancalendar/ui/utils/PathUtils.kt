9fa5d7192915406379bf8b69a4d642bee302fe3d
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2023-03-09 15:41:55 +0330

    Minor

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/utils/PathUtils.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/utils/PathUtils.kt
@@ -4,7 +4,7 @@ import android.graphics.Matrix
 import android.graphics.Path
 
 fun List<Pair<Float, Float>>.toPath(close: Boolean) = Path().also {
-    if (size >= 1) it.moveTo(this[0].first, this[0].second)
+    if (isNotEmpty()) it.moveTo(this[0].first, this[0].second)
     this.drop(1).map { (x, y) -> it.lineTo(x, y) }
     if (close) it.close()
 }
