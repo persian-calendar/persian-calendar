104a31168cf31c9a1baa34becd1d001719a3990a
Ebrahim Byagowi <ebrahim@gnu.org>
2023-03-23 19:48:57 +0330

    v7.9.5

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.byagowi.persiancalendar"
         minSdk = 17
         targetSdk = 33
-        versionCode = 790
-        versionName = "7.9.0"
+        versionCode = 795
+        versionName = "7.9.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         if (!isMinApi21Build) vectorDrawables.useSupportLibrary = true
         resourceConfigurations += listOf(
