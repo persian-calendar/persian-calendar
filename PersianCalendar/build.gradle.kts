c4923697836ed110ad081a8acdf7727a01967d77
Ebrahim Byagowi <ebrahim@gnu.org>
2023-04-14 12:41:36 +0330

    Upgrade Android Studio and AGP

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -134,8 +134,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
 
         // isCoreLibraryDesugaringEnabled = true
         //   Actually could be useful as makes use of java.time.Duration possible instead
@@ -146,7 +146,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     lint { disable += listOf("MissingTranslation") }
