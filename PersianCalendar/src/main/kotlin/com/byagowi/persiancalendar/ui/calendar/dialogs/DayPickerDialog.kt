eeda0518ea3829e8015e375ab0b6a101fbb843f0
Ebrahim Byagowi <ebrahim@gnu.org>
2023-04-18 13:49:27 +0330

    Add a bring today button in day picker dialog

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/dialogs/DayPickerDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/dialogs/DayPickerDialog.kt
@@ -1,11 +1,15 @@
 package com.byagowi.persiancalendar.ui.calendar.dialogs
 
+import android.content.DialogInterface
 import androidx.annotation.StringRes
+import androidx.core.view.isVisible
 import androidx.core.view.setPadding
 import androidx.fragment.app.FragmentActivity
+import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.entities.Jdn
 import com.byagowi.persiancalendar.ui.common.DayPickerView
 import com.byagowi.persiancalendar.ui.utils.dp
+import com.byagowi.persiancalendar.variants.debugAssertNotNull
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 fun showDayPickerDialog(
@@ -18,8 +22,15 @@ fun showDayPickerDialog(
         it.jdn = jdn
         it.setPadding(16.dp.toInt())
     }
-    MaterialAlertDialogBuilder(activity)
+    val dialog = MaterialAlertDialogBuilder(activity)
         .setView(dayPickerView)
         .setPositiveButton(positiveButtonTitle) { _, _ -> onSuccess(dayPickerView.jdn) }
+        .setNeutralButton(R.string.today, null)
         .show()
+
+    val today = Jdn.today()
+    val todayButton = dialog.getButton(DialogInterface.BUTTON_NEUTRAL).debugAssertNotNull
+    todayButton?.setOnClickListener { dayPickerView.jdn = today }
+    todayButton.isVisible = jdn != today
+    dayPickerView.selectedDayListener = { todayButton.isVisible = it != today }
 }
