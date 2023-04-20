0f5b46790042c063f4f04657c6c9a27500b4cf85
Ebrahim Byagowi <ebrahim@gnu.org>
2023-04-21 02:07:41 +0330

    Put currently selected language on top of other languages
    As one might don't know more exist above the current selection

--- PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguagePreferenceDialog.kt
+++ PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguagePreferenceDialog.kt
@@ -12,16 +12,20 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.TimeZone
 
 fun showLanguagePreferenceDialog(activity: FragmentActivity) {
+    val currentLanguage = language
     val languages = enumValues<Language>().toList().let { languages ->
         if (TimeZone.getDefault().id in listOf(IRAN_TIMEZONE_ID, AFGHANISTAN_TIMEZONE_ID)) languages
         else languages.sortedBy { it.code }
+    }.let { languages ->
+        // Put the current language on top as one might don't know more exist above the current selection
+        listOf(currentLanguage) + languages.filter { it != currentLanguage }
     }
     val names = languages.map { it.nativeName }.toTypedArray()
     MaterialAlertDialogBuilder(activity)
         .setTitle(R.string.language)
-        .setSingleChoiceItems(names, languages.indexOf(language)) { dialog, which ->
+        .setSingleChoiceItems(names, languages.indexOf(currentLanguage)) { dialog, which ->
             val chosenLanguage = languages[which]
-            if (language != chosenLanguage) activity.appPrefs.saveLanguage(chosenLanguage)
+            if (currentLanguage != chosenLanguage) activity.appPrefs.saveLanguage(chosenLanguage)
             dialog.cancel()
         }
         .setNegativeButton(R.string.cancel, null)
