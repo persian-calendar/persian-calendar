bbe4459b9c5b3aa4e1014c56c5ab6baec790bc4b
Ebrahim Byagowi <ebrahim@gnu.org>
2023-04-20 22:09:20 +0330

    Sort languages by their language code outside Iran and Afghanistan timezone

--- PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguagePreferenceDialog.kt
+++ PersianCalendar/src/minApi17/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguagePreferenceDialog.kt
@@ -1,15 +1,21 @@
 package com.byagowi.persiancalendar.ui.settings.interfacecalendar
 
 import androidx.fragment.app.FragmentActivity
+import com.byagowi.persiancalendar.AFGHANISTAN_TIMEZONE_ID
+import com.byagowi.persiancalendar.IRAN_TIMEZONE_ID
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.entities.Language
 import com.byagowi.persiancalendar.global.language
 import com.byagowi.persiancalendar.utils.appPrefs
 import com.byagowi.persiancalendar.utils.saveLanguage
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.util.TimeZone
 
 fun showLanguagePreferenceDialog(activity: FragmentActivity) {
-    val languages = enumValues<Language>().toList()
+    val languages = enumValues<Language>().toList().let { languages ->
+        if (TimeZone.getDefault().id in listOf(IRAN_TIMEZONE_ID, AFGHANISTAN_TIMEZONE_ID)) languages
+        else languages.sortedBy { it.code }
+    }
     val names = languages.map { it.nativeName }.toTypedArray()
     MaterialAlertDialogBuilder(activity)
         .setTitle(R.string.language)
