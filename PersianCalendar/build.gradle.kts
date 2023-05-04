bbed9b31b014e7f4dc3172559aaed37b937480e6
Ebrahim Byagowi <ebrahim@gnu.org>
2023-05-05 01:48:32 +0330

    Use stable version of material library

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -169,7 +169,7 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.dynamicanimation:dynamicanimation:1.0.0")
-    implementation("com.google.android.material:material:1.9.0-rc01")
+    implementation("com.google.android.material:material:1.9.0")
 
     val navVersion = "2.5.1"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
