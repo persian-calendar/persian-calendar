1466cd14e2d6b58eb0e3decc6708703d8a72768e
Ebrahim Byagowi <ebrahim@gnu.org>
2023-05-25 20:21:56 +0330

    Upgrade AGP to 8.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.0.1" apply false
+    id("com.android.application") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
 }
