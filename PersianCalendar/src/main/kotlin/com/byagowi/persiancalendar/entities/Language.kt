da6c8de369738a452ff7ad844548f53e65c94551
Ebrahim Byagowi <ebrahim@gnu.org>
2023-06-15 06:38:48 +0330

    Minor orthography fix in weekdays names
    Thanks to Mahmoud Arasteh's suggestion.
    
    Making it match with https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201402.pdf

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/Language.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/Language.kt
@@ -479,7 +479,7 @@ enum class Language(val code: String, val nativeName: String) {
             "تشرين الأول", "تشرين الثاني", "كانون الأول"
         )
         private val weekDaysInPersian = listOf7Items(
-            "شنبه", "یکشنبه", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنجشنبه", "جمعه"
+            "شنبه", "یک‌شنبه", "دوشنبه", "سه‌شنبه", "چهارشنبه", "پنج‌شنبه", "جمعه"
         )
         private val weekDaysInitialsInPersian = listOf7Items(
             "ش", "ی", "د", "س", "چ", "پ", "ج"
