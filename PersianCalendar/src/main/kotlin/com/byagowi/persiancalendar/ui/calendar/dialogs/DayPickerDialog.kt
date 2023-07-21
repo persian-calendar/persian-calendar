ef5e471eeefbe40060ec7bd7865810eb9befa17e
Ebrahim Byagowi <ebrahim@gnu.org>
2023-07-21 09:12:00 +0330

    Show days distance in day picker dialog

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/dialogs/DayPickerDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/dialogs/DayPickerDialog.kt
@@ -3,12 +3,13 @@ package com.byagowi.persiancalendar.ui.calendar.dialogs
 import android.content.DialogInterface
 import androidx.annotation.StringRes
 import androidx.core.view.isVisible
-import androidx.core.view.setPadding
 import androidx.fragment.app.FragmentActivity
 import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.databinding.DayPickerDialogBinding
 import com.byagowi.persiancalendar.entities.Jdn
-import com.byagowi.persiancalendar.ui.common.DayPickerView
-import com.byagowi.persiancalendar.ui.utils.dp
+import com.byagowi.persiancalendar.global.spacedColon
+import com.byagowi.persiancalendar.ui.utils.setupLayoutTransition
+import com.byagowi.persiancalendar.utils.calculateDaysDifference
 import com.byagowi.persiancalendar.variants.debugAssertNotNull
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -18,19 +19,25 @@ fun showDayPickerDialog(
     @StringRes positiveButtonTitle: Int,
     onSuccess: (jdn: Jdn) -> Unit
 ) {
-    val dayPickerView = DayPickerView(activity).also {
-        it.jdn = jdn
-        it.setPadding((16 * activity.resources.dp).toInt())
-    }
+    val binding = DayPickerDialogBinding.inflate(activity.layoutInflater)
+    binding.dayPickerView.jdn = jdn
+    binding.daysDistanceParent.setupLayoutTransition()
     val dialog = MaterialAlertDialogBuilder(activity)
-        .setView(dayPickerView)
-        .setPositiveButton(positiveButtonTitle) { _, _ -> onSuccess(dayPickerView.jdn) }
+        .setView(binding.root)
+        .setPositiveButton(positiveButtonTitle) { _, _ -> onSuccess(binding.dayPickerView.jdn) }
         .setNeutralButton(R.string.today, null)
         .show()
 
     val today = Jdn.today()
     val todayButton = dialog.getButton(DialogInterface.BUTTON_NEUTRAL).debugAssertNotNull
-    todayButton?.setOnClickListener { dayPickerView.jdn = today }
+    todayButton?.setOnClickListener { binding.dayPickerView.jdn = today }
     todayButton?.isVisible = jdn != today
-    dayPickerView.selectedDayListener = { todayButton?.isVisible = it != today }
+    binding.dayPickerView.selectedDayListener = {
+        todayButton?.isVisible = it != today
+        binding.daysDistance.text = if (it == today) "" else listOf(
+            activity.getString(R.string.days_distance),
+            spacedColon,
+            calculateDaysDifference(activity.resources, it)
+        ).joinToString("")
+    }
 }
