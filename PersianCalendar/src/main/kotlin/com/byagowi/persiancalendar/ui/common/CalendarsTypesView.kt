7a9179118fcfce67c99fc8682b55398e245b5104
Ebrahim Byagowi <ebrahim@gnu.org>
2023-07-31 21:42:04 +0330

    converter: Form CalendarsTypesView

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/CalendarsTypesView.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/CalendarsTypesView.kt
@@ -3,13 +3,14 @@ package com.byagowi.persiancalendar.ui.common
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.HorizontalScrollView
 import com.byagowi.persiancalendar.databinding.CalendarTypeBinding
 import com.byagowi.persiancalendar.entities.CalendarType
 import com.byagowi.persiancalendar.ui.utils.layoutInflater
 import com.google.android.material.button.MaterialButtonToggleGroup
 
-class DayPickerButtonsGroup(context: Context, attrs: AttributeSet? = null) :
-    MaterialButtonToggleGroup(context, attrs) {
+class CalendarsTypesView(context: Context, attrs: AttributeSet? = null) :
+    HorizontalScrollView(context, attrs) {
 
     var changeSelection = fun(_: CalendarType) {}
         private set
@@ -18,6 +19,8 @@ class DayPickerButtonsGroup(context: Context, attrs: AttributeSet? = null) :
         calendarTypes: List<Pair<CalendarType, String>>,
         onItemClick: (CalendarType) -> Unit
     ) {
+        val buttonGroup = MaterialButtonToggleGroup(context)
+        buttonGroup.isSingleSelection = true
         val buttons = calendarTypes.map { (_, title) ->
             CalendarTypeBinding.inflate(context.layoutInflater).also {
                 it.root.id = View.generateViewId()
@@ -26,7 +29,7 @@ class DayPickerButtonsGroup(context: Context, attrs: AttributeSet? = null) :
         }
         changeSelection = { calendarType ->
             buttons.forEachIndexed { i, button ->
-                if (calendarType == calendarTypes[i].first) check(button.id)
+                if (calendarType == calendarTypes[i].first) buttonGroup.check(button.id)
             }
         }
         buttons.forEachIndexed { i, button ->
@@ -34,8 +37,9 @@ class DayPickerButtonsGroup(context: Context, attrs: AttributeSet? = null) :
                 onItemClick(calendarTypes[i].first)
                 changeSelection(calendarTypes[i].first)
             }
-            addView(button)
+            buttonGroup.addView(button)
         }
         changeSelection(calendarTypes[0].first)
+        addView(buttonGroup)
     }
 }
