14bee14104707b5939c98d913d6e5621b65163ee
Ebrahim Byagowi <ebrahim@gnu.org>
2023-08-22 20:24:03 +0330

    Upgrade Android Studio's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
 }
