804ae453c585cd112cee6f25e89b6b455e56cea0
Ebrahim Byagowi <ebrahim@gnu.org>
2023-10-02 12:05:21 +0330

    Add annotation processor of room
    Both needed as far I understand
    https://developer.android.com/jetpack/androidx/releases/room#declaring_dependencies

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -179,6 +179,7 @@ dependencies {
 
     // Not used directly on the app but is used by work manager anyway
     implementation(libs.bundles.room)
+    annotationProcessor(libs.room.compiler)
     ksp(libs.room.compiler)
 
     // Only needed for debug builds for now, won't be needed for minApi21 builds either
