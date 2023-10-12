0d94e8c803f3ce77c0642f7451190e5b8d68bf60
Ebrahim Byagowi <ebrahim@gnu.org>
2023-10-12 23:48:39 +0330

    globe: Use bilinear filter for map texture
    At least smoothes the map when it's zoomed in.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/GLRenderer.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/map/GLRenderer.kt
@@ -156,10 +156,10 @@ class GLRenderer(
         if (textureHandle[0] != 0) {
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0])
             GLES20.glTexParameteri(
-                GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST
+                GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR
             )
             GLES20.glTexParameteri(
-                GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST
+                GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR
             )
             GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
         }
