01504504cb8f3d8de4e5ee7c56573e3d541d324c
Ebrahim Byagowi <ebrahim@gnu.org>
2023-11-10 23:49:04 +0330

    minor

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/DeviceInformationScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/DeviceInformationScreen.kt
@@ -367,15 +367,14 @@ private class DeviceInformationAdapter(private val activity: FragmentActivity) :
         ),
         Item(
             "Rounded corners", if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) run {
-                val windowInsets = activity.window?.decorView?.rootWindowInsets ?: return@run "None"
+                val insets = activity.window?.decorView?.rootWindowInsets ?: return@run "None"
                 listOf(
                     "Top left corner" to RoundedCorner.POSITION_TOP_LEFT,
                     "Top right corner" to RoundedCorner.POSITION_TOP_RIGHT,
                     "Bottom right corner" to RoundedCorner.POSITION_BOTTOM_RIGHT,
                     "Bottom left corner" to RoundedCorner.POSITION_BOTTOM_LEFT,
                 ).joinToString("\n") { (title, id) ->
-                    val corner =
-                        windowInsets.getRoundedCorner(id) ?: return@joinToString "$title: null"
+                    val corner = insets.getRoundedCorner(id) ?: return@joinToString "$title: null"
                     "$title: radius=${corner.radius} center=${corner.center}"
                 }
             } else "None"
