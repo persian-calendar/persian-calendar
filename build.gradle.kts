a70854d49ae2990a324613fff2da68878ce6f283
Ebrahim Byagowi <ebrahim@gnu.org>
2023-11-20 18:16:28 +0330

    List all the plugins listed in subprojects on root
    As https://github.com/square/kotlinpoet/issues/1730#issuecomment-1819118527
    
      "Remember, 100% of the plugins you use need to be specified in the root with apply false or else you'll get incredibly cryptic behavior."

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
+    // All the plugins used in subprojects and plugins should be listed here with "apply false"
+
+    // PersianCalendar plugins
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
     alias(libs.plugins.ksp) apply false
     id("io.github.persiancalendar.appbuildplugin") apply false
+
+    // gradlePlugins plugins
+    `kotlin-dsl` apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
 }
 
 task("clean") {
