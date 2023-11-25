5d3b0ed8c1883aa446193a89a64c5cdc1fe109b1
Ebrahim Byagowi <ebrahim@gnu.org>
2023-11-25 11:56:03 +0330

    licenses: Consume insets correctly

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/LicensesScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/about/LicensesScreen.kt
@@ -11,15 +11,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
@@ -31,9 +37,11 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -47,6 +55,7 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
@@ -55,19 +64,50 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.byagowi.persiancalendar.R
-import com.byagowi.persiancalendar.ui.utils.createUpNavigationComposeView
+import com.byagowi.persiancalendar.databinding.AppBarBinding
+import com.byagowi.persiancalendar.ui.utils.layoutInflater
+import com.byagowi.persiancalendar.ui.utils.setupUpNavigation
+import com.google.accompanist.themeadapter.material3.Mdc3Theme
 
 class LicensesScreen : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?
     ): View {
-        return createUpNavigationComposeView(inflater) @Composable { setTitle, _ ->
-            setTitle(stringResource(R.string.about_license_title))
-            Row { Rail(); Licenses() }
+        val root = ComposeView(inflater.context)
+        root.setContent {
+            Mdc3Theme {
+                Column {
+                    Spacer(Modifier.windowInsetsTopHeight(WindowInsets.safeDrawing))
+                    AndroidView(modifier = Modifier.fillMaxWidth(), factory = {
+                        val bar = AppBarBinding.inflate(it.layoutInflater)
+                        bar.toolbar.title = it.getString(R.string.about_license_title)
+                        bar.toolbar.setupUpNavigation()
+                        bar.root
+                    })
+                    Surface(shape = RoundedCornerShape(24.dp)) {
+                        Box(modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp)) {
+                            Surface {
+                                // TODO: Remove this rtl use, horizontalArrangement = Arrangement.End didn't do the trick,
+                                //  the latter ltr is ok
+                                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                                    Row {
+                                        Rail()
+                                        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                                            Licenses()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+        return root
     }
 }
 
@@ -82,11 +122,10 @@ class LicensesScreen : Fragment() {
 //   ${EventType.entries.joinToString("\n") { "${it.name}: ${it.source}" }}"""
 //  And as the result has link, it should be linkified https://stackoverflow.com/q/66130513
 
-
 @Composable
 private fun Rail() {
     var selectedItem by remember { mutableIntStateOf(0) }
-    NavigationRail {
+    NavigationRail(windowInsets = WindowInsets(0, 0, 0, 0)) {
         listOf(
             Triple(
                 "GPLv3", Icons.Default.Info, ::showShaderSandboxDialog
@@ -139,48 +178,44 @@ private fun Rail() {
 private fun Licenses() {
     val sections = remember { getCreditsSections() }
     val expansionsState = remember { List(sections.size) { false }.toMutableStateList() }
-    val initialDegree = if (LocalLayoutDirection.current == LayoutDirection.Rtl) -90f else 90f
-    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
-        LazyColumn {
-            itemsIndexed(sections) { i, (title, license, text) ->
-                val isExpanded = expansionsState[i]
-                val angle = animateFloatAsState(
-                    if (isExpanded) 0f else initialDegree, label = "angle"
-                ).value
-                Column(modifier = Modifier
-                    .clickable { expansionsState[i] = !expansionsState[i] }
-                    .padding(6.dp)
-                    .fillMaxWidth()
-                    .animateContentSize()) {
-                    FlowRow(
-                        horizontalArrangement = Arrangement.Start,
-                        verticalArrangement = Arrangement.Center,
-                    ) {
-                        Image(
-                            imageVector = Icons.Default.KeyboardArrowDown,
-                            contentDescription = stringResource(R.string.more),
-                            modifier = Modifier.rotate(angle),
-                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(title)
-                        Spacer(modifier = Modifier.width(4.dp))
-                        if (license != null) Text(
-                            // TODO: Linkify them just like https://stackoverflow.com/q/66130513
-                            //  Linkify.addLinks(it, Linkify.WEB_URLS or Linkify.EMAIL_ADDRESSES)
-                            license,
-                            modifier = Modifier
-                                .background(Color.LightGray, RoundedCornerShape(CornerSize(3.dp)))
-                                .padding(horizontal = 4.dp),
-                            style = TextStyle(Color.DarkGray, 12.sp)
-                        )
-                    }
-                    if (isExpanded) SelectionContainer { Text(text) }
-
+    val initialDegree = -90f
+    LazyColumn {
+        itemsIndexed(sections) { i, (title, license, text) ->
+            if (i > 0) Divider(color = MaterialTheme.colorScheme.outline.copy(alpha = .5f))
+            val isExpanded = expansionsState[i]
+            val angle = animateFloatAsState(
+                if (isExpanded) 0f else initialDegree, label = "angle"
+            ).value
+            Column(modifier = Modifier
+                .clickable { expansionsState[i] = !expansionsState[i] }
+                .padding(6.dp)
+                .fillMaxWidth()
+                .animateContentSize()) {
+                FlowRow(
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Image(
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = stringResource(R.string.more),
+                        modifier = Modifier.rotate(angle),
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(title)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    if (license != null) Text(
+                        // TODO: Linkify them just like https://stackoverflow.com/q/66130513
+                        //  Linkify.addLinks(it, Linkify.WEB_URLS or Linkify.EMAIL_ADDRESSES)
+                        license,
+                        modifier = Modifier
+                            .background(Color.LightGray, RoundedCornerShape(CornerSize(3.dp)))
+                            .padding(horizontal = 4.dp),
+                        style = TextStyle(Color.DarkGray, 12.sp)
+                    )
                 }
-                if (i != sections.size - 1)
-                    Divider(color = MaterialTheme.colorScheme.outline.copy(alpha = .5f))
+                if (isExpanded) SelectionContainer { Text(text) }
             }
         }
+        item { Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.systemBars)) }
     }
 }
