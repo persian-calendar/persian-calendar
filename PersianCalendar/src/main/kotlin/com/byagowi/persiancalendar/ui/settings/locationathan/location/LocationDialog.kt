52cd9966cd47f55225f7d73ed3e269fcb3adc7dd
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-01 16:19:33 +0330

    Customize location dialog

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/locationathan/location/LocationDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/locationathan/location/LocationDialog.kt
@@ -1,25 +1,30 @@
 package com.byagowi.persiancalendar.ui.settings.locationathan.location
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.generated.citiesStore
 import com.byagowi.persiancalendar.global.language
@@ -31,48 +36,68 @@ import com.google.accompanist.themeadapter.material3.Mdc3Theme
 @Composable
 fun LocationDialog(onMoreButtonClick: () -> Unit, closeDialog: () -> Unit) {
     val cities = remember { citiesStore.values.sortCityNames }
-    AlertDialog(
-        onDismissRequest = { closeDialog() },
-        title = { Text(stringResource(R.string.location)) },
-        text = {
-            LazyColumn {
-                items(cities) { city ->
-                    val context = LocalContext.current
+    Dialog(
+        properties = DialogProperties(),
+        onDismissRequest = { closeDialog() }
+    ) {
+        Surface(
+            shape = AlertDialogDefaults.shape,
+            color = AlertDialogDefaults.containerColor,
+            tonalElevation = AlertDialogDefaults.TonalElevation,
+        ) {
+            Column {
+                run {
                     Text(
-                        buildAnnotatedString {
-                            withStyle(
-                                LocalTextStyle.current.copy(fontSize = 18.sp)
-                                    .toSpanStyle()
-                            ) { append(language.getCityName(city)) }
-                            append(" ")
-                            withStyle(
-                                LocalTextStyle.current.copy(color = Color(0xFF888888))
-                                    .toSpanStyle()
-                            ) { append(language.getCountryName(city)) }
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                closeDialog()
-                                context.appPrefs.saveCity(city)
-                            }
-                            .padding(vertical = 16.dp)
+                        stringResource(R.string.location),
+                        modifier = Modifier.padding(top = 24.dp, start = 24.dp),
+                        style = MaterialTheme.typography.headlineSmall,
                     )
                 }
-            }
-        },
-        confirmButton = {
-            if (language.isIranExclusive) {
-                TextButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {
-                        closeDialog()
-                        onMoreButtonClick()
-                    },
-                ) { Text(stringResource(R.string.more)) }
+                Box(
+                    Modifier
+                        .weight(weight = 1f, fill = false)
+                        .align(Alignment.Start)
+                ) {
+                    LazyColumn {
+                        items(cities) { city ->
+                            val context = LocalContext.current
+                            Text(
+                                buildAnnotatedString {
+                                    withStyle(MaterialTheme.typography.bodyLarge.toSpanStyle()) {
+                                        append(language.getCityName(city))
+                                    }
+                                    append(" ")
+                                    withStyle(
+                                        MaterialTheme.typography.bodyMedium.copy(
+                                            color = LocalTextStyle.current.color.copy(.5f)
+                                        ).toSpanStyle()
+                                    ) { append(language.getCountryName(city)) }
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        closeDialog()
+                                        context.appPrefs.saveCity(city)
+                                    }
+                                    .padding(vertical = 16.dp, horizontal = 24.dp)
+                            )
+                        }
+                    }
+                }
+                Box(modifier = Modifier.align(Alignment.End)) {
+                    if (language.isIranExclusive) {
+                        TextButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                closeDialog()
+                                onMoreButtonClick()
+                            },
+                        ) { Text(stringResource(R.string.more), Modifier.padding(8.dp)) }
+                    }
+                }
             }
         }
-    )
+    }
 }
 
 @Preview
