1ce97df4e9331945d75a3ac76d1d2d23482db749
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-02 23:56:56 +0330

    Turn theme dialog to compose

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/InterfaceCalendarSettings.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/InterfaceCalendarSettings.kt
@@ -2,16 +2,13 @@ package com.byagowi.persiancalendar.ui.settings.interfacecalendar
 
 import android.Manifest
 import android.animation.ValueAnimator
-import android.content.DialogInterface
 import android.content.pm.PackageManager
-import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +18,6 @@ import androidx.core.content.edit
 import com.byagowi.persiancalendar.DEFAULT_EASTERN_GREGORIAN_ARABIC_MONTHS
 import com.byagowi.persiancalendar.DEFAULT_ENGLISH_GREGORIAN_PERSIAN_MONTHS
 import com.byagowi.persiancalendar.DEFAULT_ISLAMIC_OFFSET
-import com.byagowi.persiancalendar.DEFAULT_THEME_GRADIENT
 import com.byagowi.persiancalendar.PREF_APP_LANGUAGE
 import com.byagowi.persiancalendar.PREF_ASTRONOMICAL_FEATURES
 import com.byagowi.persiancalendar.PREF_EASTERN_GREGORIAN_ARABIC_MONTHS
@@ -32,11 +28,9 @@ import com.byagowi.persiancalendar.PREF_LOCAL_DIGITS
 import com.byagowi.persiancalendar.PREF_SHOW_DEVICE_CALENDAR_EVENTS
 import com.byagowi.persiancalendar.PREF_SHOW_WEEK_OF_YEAR_NUMBER
 import com.byagowi.persiancalendar.PREF_THEME
-import com.byagowi.persiancalendar.PREF_THEME_GRADIENT
 import com.byagowi.persiancalendar.PREF_WEEK_ENDS
 import com.byagowi.persiancalendar.PREF_WEEK_START
 import com.byagowi.persiancalendar.R
-import com.byagowi.persiancalendar.databinding.ColorGradientSwitchBinding
 import com.byagowi.persiancalendar.entities.Theme
 import com.byagowi.persiancalendar.global.language
 import com.byagowi.persiancalendar.global.weekDays
@@ -56,7 +50,18 @@ import kotlin.random.Random
 @Composable
 fun InterfaceCalendarSettings(activity: ComponentActivity, destination: String? = null) {
     SettingsSection(stringResource(R.string.pref_interface))
-    ThemeSelect(activity)
+    val context = LocalContext.current
+    run {
+        val themeDisplayName = stringResource(run {
+            val currentKey = context.appPrefs.getString(PREF_THEME, null)
+            Theme.entries.firstOrNull { it.key == currentKey } ?: Theme.SYSTEM_DEFAULT
+        }.title)
+        var showDialog by rememberSaveable { mutableStateOf(false) }
+        SettingsClickable(
+            title = stringResource(R.string.select_skin), summary = themeDisplayName
+        ) { showDialog = true }
+        if (showDialog) ThemeDialog { showDialog = false }
+    }
     run {
         var showDialog by rememberSaveable { mutableStateOf(destination == PREF_APP_LANGUAGE) }
         SettingsClickable(
@@ -184,70 +189,4 @@ fun InterfaceCalendarSettings(activity: ComponentActivity, destination: String? 
         title = stringResource(R.string.week_ends),
         summary = stringResource(R.string.week_ends),
     )
-}
-
-@Composable
-private fun ThemeSelect(activity: ComponentActivity) {
-    val entries = Theme.entries.map { activity.getString(it.title) }
-    val entryValues = Theme.entries.map { it.key }
-    val context = LocalContext.current
-    var themeDisplayName by remember {
-        mutableStateOf(
-            entries[entryValues.indexOf(
-                context.appPrefs.getString(PREF_THEME, null) ?: Theme.SYSTEM_DEFAULT.key
-            )]
-        )
-    }
-    SettingsClickable(
-        title = stringResource(R.string.select_skin), summary = themeDisplayName
-    ) clickable@{
-        val currentValue = entryValues.indexOf(
-            context.appPrefs.getString(PREF_THEME, null) ?: Theme.SYSTEM_DEFAULT.key
-        )
-        val dialog =
-            androidx.appcompat.app.AlertDialog.Builder(context).setTitle(R.string.select_skin)
-                .setNegativeButton(R.string.cancel, null)
-                .setSingleChoiceItems(entries.toTypedArray(), currentValue) { dialog, which ->
-                    context.appPrefs.edit { putString(PREF_THEME, entryValues[which]) }
-                    themeDisplayName = entries[which]
-                    dialog.dismiss()
-                }.show()
-//    val scope = rememberCoroutineScope()
-//    val key = stringPreferencesKey(PREF_THEME)
-//    var currentIndex by remember { mutableStateOf(0) }
-//    scope.launch {
-//        context.dataStore.data.collectLatest {
-//            currentIndex = entryValues.indexOf(it[key] ?: Theme.SYSTEM_DEFAULT.key)
-//        }
-//    }
-//    SettingsClickable(
-//        title = stringResource(R.string.select_skin),
-//        summary = entries[currentIndex],
-//    ) clickable@{
-//        val dialog =
-//            androidx.appcompat.app.AlertDialog.Builder(context).setTitle(R.string.select_skin)
-//                .setNegativeButton(R.string.cancel, null)
-//                .setSingleChoiceItems(entries.toTypedArray(), currentIndex) { dialog, which ->
-//                    scope.launch { context.dataStore.edit { it[key] = entryValues[which] } }
-//                    dialog.dismiss()
-//                }.show()
-
-        if (!Theme.supportsGradient(activity)) return@clickable
-        val binding = ColorGradientSwitchBinding.inflate(activity.layoutInflater)
-        (dialog.getButton(DialogInterface.BUTTON_NEGATIVE)?.parent as? ViewGroup)?.addView(
-            binding.root,
-            0
-        )
-        if (activity.appPrefs.getBoolean(
-                PREF_THEME_GRADIENT,
-                DEFAULT_THEME_GRADIENT
-            )
-        ) binding.button.isChecked = true
-        binding.label.setOnClickListener {
-            binding.button.isChecked = !binding.button.isChecked
-        }
-        binding.button.setOnCheckedChangeListener { _, isChecked ->
-            activity.appPrefs.edit { putBoolean(PREF_THEME_GRADIENT, isChecked) }
-        }
-    }
 }
---
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/ThemeDialog.kt
@@ -0,0 +1,93 @@
+package com.byagowi.persiancalendar.ui.settings.interfacecalendar
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.edit
+import com.byagowi.persiancalendar.DEFAULT_THEME_GRADIENT
+import com.byagowi.persiancalendar.PREF_THEME
+import com.byagowi.persiancalendar.PREF_THEME_GRADIENT
+import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.entities.Theme
+import com.byagowi.persiancalendar.ui.common.Dialog
+import com.byagowi.persiancalendar.utils.appPrefs
+
+@Composable
+fun ThemeDialog(onDismissRequest: () -> Unit) {
+    val context = LocalContext.current
+    val currentTheme = remember {
+        context.appPrefs.getString(PREF_THEME, null) ?: Theme.SYSTEM_DEFAULT.key
+    }
+    Dialog(
+        title = { Text(stringResource(R.string.select_skin)) },
+        onDismissRequest = onDismissRequest,
+        negativeButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(stringResource(R.string.cancel))
+            }
+        },
+        neutralButton = {
+            if (Theme.supportsGradient(context)) {
+                var isChecked by rememberSaveable {
+                    mutableStateOf(
+                        context.appPrefs.getBoolean(
+                            PREF_THEME_GRADIENT,
+                            DEFAULT_THEME_GRADIENT
+                        )
+                    )
+                }
+
+                fun onClick() {
+                    isChecked = !isChecked
+                    context.appPrefs.edit { putBoolean(PREF_THEME_GRADIENT, isChecked) }
+                }
+                Row(
+                    Modifier.clickable { onClick() },
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Switch(checked = isChecked, onCheckedChange = { onClick() })
+                    Spacer(Modifier.width(8.dp))
+                    Text(stringResource(R.string.color_gradient))
+                }
+            }
+        },
+    ) {
+        Theme.entries.forEach { theme ->
+            fun onClick() {
+                onDismissRequest()
+                context.appPrefs.edit { putString(PREF_THEME, theme.key) }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .clickable(onClick = ::onClick)
+                    .padding(horizontal = 10.dp)
+            ) {
+                RadioButton(selected = theme.key == currentTheme, onClick = ::onClick)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(stringResource(theme.title))
+            }
+        }
+    }
+}
