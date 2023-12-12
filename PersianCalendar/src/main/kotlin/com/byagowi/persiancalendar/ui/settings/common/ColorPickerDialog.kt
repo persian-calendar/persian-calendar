3e4abef64d24da8cb6cec1693aaf6ea8e8534b0f
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-12 22:52:12 +0330

    Turn ColorPickerDialog to compose

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/common/ColorPickerDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/common/ColorPickerDialog.kt
@@ -1,52 +1,94 @@
 package com.byagowi.persiancalendar.ui.settings.common
 
-import android.app.AlertDialog
 import android.graphics.Color
 import androidx.activity.ComponentActivity
-import androidx.annotation.ColorInt
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.edit
 import androidx.core.view.setPadding
 import com.byagowi.persiancalendar.DEFAULT_SELECTED_WIDGET_BACKGROUND_COLOR
 import com.byagowi.persiancalendar.DEFAULT_SELECTED_WIDGET_TEXT_COLOR
 import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.ui.common.AppDialog
 import com.byagowi.persiancalendar.ui.utils.dp
+import com.byagowi.persiancalendar.ui.utils.showComposeDialog
 import com.byagowi.persiancalendar.utils.appPrefs
 import java.util.Locale
 
 fun showColorPickerDialog(activity: ComponentActivity, isBackgroundPick: Boolean, key: String) {
-    val initialColor = activity.appPrefs.getString(key, null)?.let(Color::parseColor)
-        ?: if (isBackgroundPick) DEFAULT_SELECTED_WIDGET_BACKGROUND_COLOR
-        else DEFAULT_SELECTED_WIDGET_TEXT_COLOR
-    showColorPickerDialog(activity, isBackgroundPick, initialColor) { colorResult ->
-        activity.appPrefs.edit { this.putString(key, colorResult) }
+    showComposeDialog(activity) { onDismissRequest ->
+        ColorPickerDialog(isBackgroundPick, key, onDismissRequest)
     }
 }
 
-private fun showColorPickerDialog(
-    activity: ComponentActivity, isBackgroundPick: Boolean, @ColorInt initialColor: Int,
-    onResult: (String) -> Unit
+@Composable
+fun ColorPickerDialog(
+    isBackgroundPick: Boolean, key: String, onDismissRequest: () -> Unit
 ) {
-    val colorPickerView = ColorPickerView(activity).also {
-        it.setColorsToPick(
-            if (isBackgroundPick) listOf(0x00000000L, 0x20000000L, 0x50000000L, 0xFF000000L)
-            else listOf(0xFFFFFFFFL, 0xFFE65100L, 0xFF00796bL, 0xFFFEF200L, 0xFF202020L)
-        )
-        if (!isBackgroundPick) it.hideAlphaSeekBar()
-        it.setPickedColor(initialColor)
-        it.setPadding((10 * activity.resources.dp).toInt())
+    val context = LocalContext.current
+    val initialColor = remember {
+        context.appPrefs.getString(key, null)?.let(Color::parseColor)
+            ?: if (isBackgroundPick) DEFAULT_SELECTED_WIDGET_BACKGROUND_COLOR
+            else DEFAULT_SELECTED_WIDGET_TEXT_COLOR
     }
-    AlertDialog.Builder(activity)
-        .setTitle(if (isBackgroundPick) R.string.widget_background_color else R.string.widget_text_color)
-        .setView(colorPickerView)
-        .setPositiveButton(R.string.accept) { _, _ ->
-            onResult(
-                if (isBackgroundPick) "#%08X".format(
-                    Locale.ENGLISH, 0xFFFFFFFF and colorPickerView.pickerColor.toLong()
-                ) else "#%06X".format(
-                    Locale.ENGLISH, 0xFFFFFF and colorPickerView.pickerColor
+    // Ugly but for now and till a rewrite in Compose
+    var colorPickerView by remember { mutableStateOf<ColorPickerView?>(null) }
+    AppDialog(
+        title = {
+            Text(
+                stringResource(
+                    if (isBackgroundPick) R.string.widget_background_color
+                    else R.string.widget_text_color
                 )
             )
-        }
-        .setNegativeButton(R.string.cancel, null)
-        .show()
+        },
+        confirmButton = {
+            TextButton(onClick = {
+                onDismissRequest()
+                val colorResult = if (isBackgroundPick) "#%08X".format(
+                    Locale.ENGLISH, 0xFFFFFFFF and (colorPickerView?.pickerColor?.toLong() ?: 0L)
+                ) else "#%06X".format(
+                    Locale.ENGLISH, 0xFFFFFF and (colorPickerView?.pickerColor ?: 0)
+                )
+                context.appPrefs.edit { this.putString(key, colorResult) }
+            }) { Text(stringResource(R.string.accept)) }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) { Text(stringResource(R.string.cancel)) }
+        },
+        onDismissRequest = onDismissRequest,
+    ) {
+        AndroidView(
+            factory = { context ->
+                ColorPickerView(context).also {
+                    colorPickerView = it
+                    it.setColorsToPick(
+                        if (isBackgroundPick) listOf(
+                            0x00000000L, 0x20000000L, 0x50000000L, 0xFF000000L
+                        ) else listOf(
+                            0xFFFFFFFFL, 0xFFE65100L, 0xFF00796bL, 0xFFFEF200L, 0xFF202020L
+                        )
+                    )
+                    if (!isBackgroundPick) it.hideAlphaSeekBar()
+                    it.setPickedColor(initialColor)
+                    it.setPadding((10 * context.resources.dp).toInt())
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
+    }
 }
