669664721d54dcc6b7d5a5772fbfd5239e259dd9
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-15 23:34:11 +0330

    Put settings icons beside the text in horizontal screen mode
    As Ammari's suggestion

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/SettingsScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/SettingsScreen.kt
@@ -4,12 +4,14 @@ import android.app.StatusBarManager
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.drawable.Icon
 import android.os.Build
 import android.provider.Settings
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
@@ -56,10 +59,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
@@ -200,7 +205,19 @@ fun SettingsScreen(
                     val title = titlesResId.joinToString(stringResource(R.string.spaced_and)) {
                         context.getString(it)
                     }
-                    Tab(
+                    val isLandscape =
+                        LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
+                    if (isLandscape) Tab(
+                        text = {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(icon, contentDescription = null)
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(title)
+                            }
+                        },
+                        selected = pagerState.currentPage == index,
+                        onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
+                    ) else Tab(
                         icon = { Icon(icon, contentDescription = null) },
                         text = { Text(title) },
                         selected = pagerState.currentPage == index,
