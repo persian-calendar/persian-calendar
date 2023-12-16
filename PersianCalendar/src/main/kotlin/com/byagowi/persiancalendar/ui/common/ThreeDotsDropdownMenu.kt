51dc44e599c37a55f0e642dbc3fcdbc1d1fd12c6
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-16 12:02:44 +0330

    Set a default minimum size for three dots menus

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ThreeDotsDropdownMenu.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ThreeDotsDropdownMenu.kt
@@ -2,6 +2,7 @@ package com.byagowi.persiancalendar.ui.common
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
@@ -18,7 +19,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.byagowi.persiancalendar.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -41,7 +44,8 @@ fun ThreeDotsDropdownMenu(content: @Composable ColumnScope.(closeMenu: () -> Uni
         DropdownMenu(
             expanded = showMenu,
             onDismissRequest = { showMenu = false },
-            content = { content { showMenu = false } }
+            modifier = Modifier.defaultMinSize(minWidth = 160.dp),
+            content = { content { showMenu = false } },
         )
     }
 }
