60c3ffdc68683f50b615ee71c72202988fcd5394
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-24 10:13:48 +0330

    Remove transparentSystemBars and use enableEdgeToEdge instead

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/MainActivity.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/MainActivity.kt
@@ -6,15 +6,13 @@ import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
 import androidx.core.content.edit
-import androidx.core.graphics.ColorUtils
-import androidx.core.view.WindowCompat
 import com.byagowi.persiancalendar.DEFAULT_NOTIFY_DATE
 import com.byagowi.persiancalendar.LAST_CHOSEN_TAB_KEY
 import com.byagowi.persiancalendar.PREF_APP_LANGUAGE
@@ -49,8 +47,9 @@ import kotlinx.coroutines.flow.StateFlow
 class MainActivity : ComponentActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Just to make sure we have an initial transparent system bars
-        // System bars are tweaked later with project's own transparentSystemBars also
-        enableEdgeToEdge()
+        // System bars are tweaked later with project's with better values
+        enableEdgeToEdge(SystemBarStyle.dark(Color.TRANSPARENT))
+
         setTheme(R.style.BaseTheme)
         applyAppLanguage(this)
         super.onCreate(savedInstanceState)
@@ -70,7 +69,14 @@ class MainActivity : ComponentActivity(), SharedPreferences.OnSharedPreferenceCh
                 val isBackgroundColorLight = MaterialTheme.colorScheme.background.isLight
                 val isSurfaceColorLight = MaterialTheme.colorScheme.surface.isLight
                 LaunchedEffect(isBackgroundColorLight, isSurfaceColorLight) {
-                    transparentSystemBars(window, isBackgroundColorLight, isSurfaceColorLight)
+                    enableEdgeToEdge(
+                        if (isBackgroundColorLight)
+                            SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                        else SystemBarStyle.dark(Color.TRANSPARENT),
+                        if (isSurfaceColorLight)
+                            SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+                        else SystemBarStyle.dark(Color.TRANSPARENT),
+                    )
                 }
 
                 App(intentStartDestination, ::finish)
@@ -141,39 +147,3 @@ class MainActivity : ComponentActivity(), SharedPreferences.OnSharedPreferenceCh
 
 private val resumeToken_ = MutableStateFlow(0)
 val resumeToken: StateFlow<Int> = resumeToken_
-
-/**
- * Make system bars (status and navigation bars) transparent as far as possible, also disables
- * decor view insets so we should consider the insets ourselves.
- *
- * From https://stackoverflow.com/a/76018821 with some modifications
- * Also have a look at [androidx.activity.enableEdgeToEdge] which provides the same functionality
- * but in non-gesture navigation is less immersive.
- */
-private fun transparentSystemBars(
-    window: Window,
-    isBackgroundColorLight: Boolean,
-    isSurfaceColorLight: Boolean,
-) {
-    val insetsController = WindowCompat.getInsetsController(window, window.decorView)
-    insetsController.isAppearanceLightStatusBars = isBackgroundColorLight
-    insetsController.isAppearanceLightNavigationBars = isSurfaceColorLight
-
-    val isLightStatusBarAvailable = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-    val isLightNavigationBarAvailable = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-
-    // Either primary color, what we use behind above status icons, isn't light so we don't need to worry
-    // about not being able to set isAppearanceLightStatusBars or let's check the sdk version so
-    // we at least use isAppearanceLightStatusBars.
-    val shouldStatusBarBeTransparent = !isBackgroundColorLight || isLightStatusBarAvailable
-
-    // Either surface color, what we use behind below navigation icons, isn't light so we don't need to worry
-    // about not being able to set isAppearanceLightNavigationBars or let's check the sdk version so
-    // we at least use isAppearanceLightStatusBars.
-    val shouldNavigationBarBeTransparent = !isSurfaceColorLight || isLightNavigationBarAvailable
-
-    val systemUiScrim = ColorUtils.setAlphaComponent(Color.BLACK, 0x40) // 25% black
-    window.statusBarColor = if (shouldStatusBarBeTransparent) Color.TRANSPARENT else systemUiScrim
-    window.navigationBarColor =
-        if (shouldNavigationBarBeTransparent) Color.TRANSPARENT else systemUiScrim
-}
