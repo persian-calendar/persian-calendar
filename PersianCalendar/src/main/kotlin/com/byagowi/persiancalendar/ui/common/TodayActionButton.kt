5a7ff8aa65ce25a0d4b80e90a851f1d1e04795b4
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-24 22:30:22 +0330

    Use Material's restore icon instead of our own restore
    Ours was based Material one except it didn't have the clock but now I think that clock can be nice.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/TodayActionButton.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/TodayActionButton.kt
@@ -3,17 +3,17 @@ package com.byagowi.persiancalendar.ui.common
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Restore
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import com.byagowi.persiancalendar.R
 
 @Composable
 fun TodayActionButton(visible: Boolean, onClick: () -> Unit) {
     AnimatedVisibility(visible, enter = scaleIn(), exit = scaleOut()) {
         AppIconButton(
-            icon = ImageVector.vectorResource(R.drawable.ic_restore_modified),
+            icon = Icons.Default.Restore,
             title = stringResource(R.string.return_to_today),
             onClick = onClick,
         )
