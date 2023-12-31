ee85330b3a4fbbbece11221dfe0cf810b1eee1fa
Ebrahim Byagowi <ebrahim@gnu.org>
2023-12-31 11:48:46 +0330

    calendar: Bring yearview on screen title click

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/CalendarScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/CalendarScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -48,6 +49,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
@@ -668,7 +670,13 @@ private fun Toolbar(openDrawer: () -> Unit, viewModel: CalendarViewModel) {
                 subtitle = monthFormatForSecondaryCalendar(selectedMonth, secondaryCalendar)
             }
             val animationTime = integerResource(android.R.integer.config_mediumAnimTime)
-            Column {
+            Column(
+                if (isYearView) Modifier else Modifier.clickable(
+                    indication = rememberRipple(bounded = false),
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClickLabel = stringResource(R.string.year_view)
+                ) { viewModel.openYearView() }
+            ) {
                 AnimatedContent(
                     title,
                     label = "title",
