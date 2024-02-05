2277fd99f14b85f942998348b2e9ae9a0d2f1e39
Ebrahim Byagowi <ebrahim@gnu.org>
2024-02-05 20:20:16 +0330

    Remove unused ApplicationService.getInstance

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/ApplicationService.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/ApplicationService.kt
@@ -8,20 +8,17 @@ import com.byagowi.persiancalendar.global.updateStoredPreference
 import com.byagowi.persiancalendar.utils.logException
 import com.byagowi.persiancalendar.utils.update
 import com.byagowi.persiancalendar.variants.debugLog
-import java.lang.ref.WeakReference
 
 /**
  * The Calendar Service that updates widget time and clock and build/update
  * calendar notification.
  */
 class ApplicationService : Service() {
-
     private val receiver = BroadcastReceivers()
 
     override fun onBind(paramIntent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        instance = WeakReference(this)
         debugLog("${ApplicationService::class.java.name} start")
 
         val intentFilter = IntentFilter().apply {
@@ -29,12 +26,12 @@ class ApplicationService : Service() {
             addAction(Intent.ACTION_TIMEZONE_CHANGED)
             addAction(Intent.ACTION_TIME_CHANGED)
             addAction(Intent.ACTION_SCREEN_ON)
-            //        addAction(Intent.ACTION_TIME_TICK)
+            // addAction(Intent.ACTION_TIME_TICK)
         }
         registerReceiver(receiver, intentFilter)
 
         updateStoredPreference(applicationContext)
-        update(applicationContext, true)
+        update(applicationContext, updateDate = true)
 
         return START_STICKY
     }
@@ -42,11 +39,5 @@ class ApplicationService : Service() {
     override fun onDestroy() {
         runCatching { unregisterReceiver(receiver) }.onFailure(logException)
         super.onDestroy()
-    }
-
-    companion object {
-        private var instance: WeakReference<ApplicationService>? = null
-
-        fun getInstance(): ApplicationService? = instance?.get()
     }
 }
