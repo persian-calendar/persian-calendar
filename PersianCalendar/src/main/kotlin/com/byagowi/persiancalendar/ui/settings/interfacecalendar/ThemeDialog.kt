8eafe30b198ef8bb8c99beb1938d6ed8be867116
Ebrahim Byagowi <ebrahim@gnu.org>
2024-03-12 17:28:25 +0330

    Use SwitchWithLabel in ThemeDialog

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/ThemeDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/ThemeDialog.kt
@@ -4,22 +4,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,6 +28,7 @@ import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.global.isGradient
 import com.byagowi.persiancalendar.global.theme
 import com.byagowi.persiancalendar.ui.common.AppDialog
+import com.byagowi.persiancalendar.ui.common.SwitchWithLabel
 import com.byagowi.persiancalendar.ui.theme.Theme
 import com.byagowi.persiancalendar.ui.utils.SettingsHorizontalPaddingItem
 import com.byagowi.persiancalendar.ui.utils.SettingsItemHeight
@@ -50,19 +47,10 @@ fun ThemeDialog(onDismissRequest: () -> Unit) {
         neutralButton = {
             AnimatedVisibility(theme.hasGradient, enter = fadeIn(), exit = fadeOut()) {
                 val isGradient by isGradient.collectAsState()
-                fun toggleIsGradient() =
-                    context.appPrefs.edit { putBoolean(PREF_THEME_GRADIENT, !isGradient) }
-                Row(
-                    Modifier.clickable(
-                        indication = rememberRipple(bounded = false),
-                        interactionSource = remember { MutableInteractionSource() },
-                    ) { toggleIsGradient() },
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Switch(checked = isGradient, onCheckedChange = { toggleIsGradient() })
-                    Spacer(Modifier.width(8.dp))
-                    Text(stringResource(R.string.color_gradient))
-                }
+                SwitchWithLabel(
+                    label = stringResource(R.string.color_gradient),
+                    checked = isGradient,
+                ) { context.appPrefs.edit { putBoolean(PREF_THEME_GRADIENT, !isGradient) } }
             }
         },
     ) {
