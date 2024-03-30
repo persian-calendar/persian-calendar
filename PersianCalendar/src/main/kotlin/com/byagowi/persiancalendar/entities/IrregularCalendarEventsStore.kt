31681da76d0229692db910204fc56ce6ee39b5b2
Ebrahim Byagowi <ebrahim@gnu.org>
2024-03-30 23:00:10 +0330

    Add tests for 'nth weekday of month'

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/IrregularCalendarEventsStore.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/IrregularCalendarEventsStore.kt
@@ -10,6 +10,7 @@ import io.github.persiancalendar.calendar.CivilDate
 import io.github.persiancalendar.calendar.IslamicDate
 import io.github.persiancalendar.calendar.NepaliDate
 import io.github.persiancalendar.calendar.PersianDate
+import org.jetbrains.annotations.VisibleForTesting
 
 class IrregularCalendarEventsStore(private val eventsRepository: EventsRepository) {
     private fun createCache(type: CalendarType) =
@@ -36,14 +37,14 @@ class IrregularCalendarEventsStore(private val eventsRepository: EventsRepositor
     // Create actually usable irregular event of a year based on defined rules and enabled holidays
     private fun generateEntry(year: Int, type: CalendarType): List<CalendarEvent<*>> {
         return irregularRecurringEvents.filter { event ->
-            val eventType = when (event["calendar"]) {
-                "Gregorian" -> CalendarType.GREGORIAN
-                "Persian" -> CalendarType.SHAMSI
-                "Hijri" -> CalendarType.ISLAMIC
-                "Nepali" -> CalendarType.NEPALI
-                else -> return@filter false
-            }
-            if (eventType != type) return@filter false
+            if (type != when (event["calendar"]) {
+                    "Gregorian" -> CalendarType.GREGORIAN
+                    "Persian" -> CalendarType.SHAMSI
+                    "Hijri" -> CalendarType.ISLAMIC
+                    "Nepali" -> CalendarType.NEPALI
+                    else -> return@filter false
+                }
+            ) return@filter false
             when {
                 event["type"] == "International" && eventsRepository.international -> true
                 event["type"] == "Iran" && eventsRepository.iranHolidays && event["holiday"] == "true" -> true
@@ -54,46 +55,7 @@ class IrregularCalendarEventsStore(private val eventsRepository: EventsRepositor
                 else -> false
             }
         }.mapNotNull { event ->
-            val date = when (event["rule"]) {
-                "single event" -> {
-                    if (event["year"]?.toIntOrNull() != year) return@mapNotNull null
-                    val month = event["month"]?.toIntOrNull() ?: return@mapNotNull null
-                    val day = event["day"]?.toIntOrNull() ?: return@mapNotNull null
-                    type.createDate(year, month, day)
-                }
-
-                "nth day from" -> {
-                    val nth = event["nth"]?.toIntOrNull() ?: return@mapNotNull null
-                    val day = event["day"]?.toIntOrNull() ?: return@mapNotNull null
-                    val month = event["month"]?.toIntOrNull() ?: return@mapNotNull null
-                    (Jdn(type, year, month, day) + nth - 1).toCalendar(type)
-                }
-
-                "end of month" -> {
-                    val month = event["month"]?.toIntOrNull() ?: return@mapNotNull null
-                    type.createDate(year, month, type.getMonthLength(year, month))
-                }
-
-                "last weekday of month" -> {
-                    val month = event["month"]?.toIntOrNull() ?: return@mapNotNull null
-                    val weekDay = event["weekday"]?.toIntOrNull() ?: return@mapNotNull null
-                    val offset = event["offset"]?.toIntOrNull() ?: 0
-                    val day = type.getLastWeekDayOfMonth(year, month, weekDay) + offset
-                    type.createDate(year, month, day)
-                }
-
-                "nth weekday of month" -> {
-                    val month = event["month"]?.toIntOrNull() ?: return@mapNotNull null
-                    val weekDay = event["weekday"]?.toIntOrNull() ?: return@mapNotNull null
-                    val nth = event["nth"]?.toIntOrNull() ?: return@mapNotNull null
-                    val monthStartWeekDay = Jdn(type, year, month, 1).dayOfWeek
-                    val day =
-                        weekDay - monthStartWeekDay + (nth - if (monthStartWeekDay < weekDay) 1 else 0) * 7
-                    type.createDate(year, month, day)
-                }
-
-                else -> return@mapNotNull null
-            }
+            val date = getDateInstance(event, year, type) ?: return@mapNotNull null
             val title = "${event["title"] ?: return@mapNotNull null} (${formatNumber(year)})"
             val isHoliday = event["holiday"] == "true"
             when (date) {
@@ -104,5 +66,49 @@ class IrregularCalendarEventsStore(private val eventsRepository: EventsRepositor
                 else -> null
             }.debugAssertNotNull
         }
+    }
+}
+
+@VisibleForTesting
+fun getDateInstance(event: Map<String, String>, year: Int, type: CalendarType): AbstractDate? {
+    return when (event["rule"]) {
+        "single event" -> {
+            if (event["year"]?.toIntOrNull() != year) return null
+            val month = event["month"]?.toIntOrNull() ?: return null
+            val day = event["day"]?.toIntOrNull() ?: return null
+            type.createDate(year, month, day)
+        }
+
+        "nth day from" -> {
+            val nth = event["nth"]?.toIntOrNull() ?: return null
+            val day = event["day"]?.toIntOrNull() ?: return null
+            val month = event["month"]?.toIntOrNull() ?: return null
+            (Jdn(type, year, month, day) + nth - 1).toCalendar(type)
+        }
+
+        "end of month" -> {
+            val month = event["month"]?.toIntOrNull() ?: return null
+            type.createDate(year, month, type.getMonthLength(year, month))
+        }
+
+        "last weekday of month" -> {
+            val month = event["month"]?.toIntOrNull() ?: return null
+            val weekDay = event["weekday"]?.toIntOrNull() ?: return null
+            val offset = event["offset"]?.toIntOrNull() ?: 0
+            val day = type.getLastWeekDayOfMonth(year, month, weekDay) + offset
+            type.createDate(year, month, day)
+        }
+
+        "nth weekday of month" -> {
+            val month = event["month"]?.toIntOrNull() ?: return null
+            val weekDay = event["weekday"]?.toIntOrNull() ?: return null
+            val nth = event["nth"]?.toIntOrNull() ?: return null
+            val monthStartWeekDay = Jdn(type, year, month, 1).dayOfWeek
+            val day =
+                weekDay - monthStartWeekDay + (nth - if (monthStartWeekDay < weekDay) 1 else 0) * 7
+            type.createDate(year, month, day)
+        }
+
+        else -> return null
     }
 }
--- PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EventsTests.kt
+++ PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EventsTests.kt
@@ -1,8 +1,10 @@
 package com.byagowi.persiancalendar
 
+import com.byagowi.persiancalendar.entities.CalendarType
 import com.byagowi.persiancalendar.entities.EventsRepository
 import com.byagowi.persiancalendar.entities.EventsStore
 import com.byagowi.persiancalendar.entities.Language
+import com.byagowi.persiancalendar.entities.getDateInstance
 import io.github.persiancalendar.calendar.CivilDate
 import io.github.persiancalendar.calendar.IslamicDate
 import org.junit.jupiter.api.Test
@@ -30,5 +32,24 @@ class EventsTests {
         (1..12).flatMap { month -> (1..31).map { day -> CivilDate(2000, month, day) } }
             .map { EventsStore.hash(it) }.toSet().toList()
             .also { assertEquals(372, it.size) }
+    }
+
+    @Test
+    fun `test createDateFromSpecification`() {
+        val title = "روز جهانی فلسفه (سومین پنج‌شنبهٔ نوامبر)"
+        val event = mapOf(
+            "rule" to "nth weekday of month", "nth" to "3", "weekday" to "6", "month" to "11",
+            "type" to "International", "title" to title, "holiday" to "false",
+        )
+        assertEquals(getDateInstance(event, 2024, CalendarType.GREGORIAN), CivilDate(2024, 11, 21))
+        assertEquals(getDateInstance(event, 2025, CalendarType.GREGORIAN), CivilDate(2025, 11, 20))
+        assertEquals(getDateInstance(event, 2026, CalendarType.GREGORIAN), CivilDate(2026, 11, 19))
+        assertEquals(getDateInstance(event, 2027, CalendarType.GREGORIAN), CivilDate(2027, 11, 18))
+        assertEquals(getDateInstance(event, 2028, CalendarType.GREGORIAN), CivilDate(2028, 11, 16))
+        assertEquals(getDateInstance(event, 2029, CalendarType.GREGORIAN), CivilDate(2029, 11, 15))
+        assertEquals(getDateInstance(event, 2030, CalendarType.GREGORIAN), CivilDate(2030, 11, 21))
+        assertEquals(getDateInstance(event, 2031, CalendarType.GREGORIAN), CivilDate(2031, 11, 20))
+        assertEquals(getDateInstance(event, 2032, CalendarType.GREGORIAN), CivilDate(2032, 11, 18))
+        assertEquals(getDateInstance(event, 2033, CalendarType.GREGORIAN), CivilDate(2033, 11, 17))
     }
 }
