c369f5f6714e1f0d73491eba6457b5f3728b98b0
Ebrahim Byagowi <ebrahim@gnu.org>
2024-03-31 21:58:39 +0330

    Add more tests for getNthWeekDayOfMonth

--- PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EventsTests.kt
+++ PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EventsTests.kt
@@ -63,6 +63,65 @@ class EventsTests {
 
     @ParameterizedTest
     @CsvSource(
+        "1400, 12, 15, 2, 3",
+        "1400, 12, 21, 1, 3",
+        "1400, 12, 20, 7, 3",
+        "1400, 12, 19, 6, 3",
+        "1400, 12, 18, 5, 3",
+        "1400, 12, 17, 4, 3",
+        "1400, 12, 16, 3, 3",
+        "1400, 12, 20, 7, 3",
+
+        "1400, 12, 7, 1, 1",
+        "1400, 12, 14, 1, 2",
+        "1400, 12, 21, 1, 3",
+        "1400, 12, 28, 1, 4",
+
+        "1400, 12, 1, 2, 1",
+        "1400, 12, 8, 2, 2",
+        "1400, 12, 15, 2, 3",
+        "1400, 12, 22, 2, 4",
+        "1400, 12, 29, 2, 5",
+
+        "1400, 12, 2, 3, 1",
+        "1400, 12, 9, 3, 2",
+        "1400, 12, 16, 3, 3",
+        "1400, 12, 23, 3, 4",
+
+        "1400, 12, 3, 4, 1",
+        "1400, 12, 10, 4, 2",
+        "1400, 12, 17, 4, 3",
+        "1400, 12, 24, 4, 4",
+
+        "1400, 12, 4, 5, 1",
+        "1400, 12, 11, 5, 2",
+        "1400, 12, 18, 5, 3",
+        "1400, 12, 25, 5, 4",
+
+        "1400, 12, 5, 6, 1",
+        "1400, 12, 12, 6, 2",
+        "1400, 12, 19, 6, 3",
+        "1400, 12, 26, 6, 4",
+
+        "1400, 12, 6, 7, 1",
+        "1400, 12, 13, 7, 2",
+        "1400, 12, 20, 7, 3",
+        "1400, 12, 27, 7, 4",
+    )
+    fun `getNthWeekDayOfMonth calculations correctness`(
+        year: Int, month: Int, day: Int, weekDay: Int, nth: Int,
+    ) {
+        val calendar = CalendarType.SHAMSI
+        assertEquals(day, calendar.getNthWeekDayOfMonth(year, month, weekDay, nth))
+        val event = mapOf(
+            "rule" to "nth weekday of month", "nth" to "$nth",
+            "weekday" to "$weekDay", "month" to "$month",
+        )
+        assertEquals(getDateInstance(event, year, calendar), PersianDate(year, month, day))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
         "1400, 12, 29, 2",
         "1400, 12, 28, 1",
         "1400, 12, 27, 7",
