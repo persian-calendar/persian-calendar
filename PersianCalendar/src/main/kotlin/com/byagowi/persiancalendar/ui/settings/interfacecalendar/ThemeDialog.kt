2e3b2e91075ca6b93451725e3cd6d1b6d076bf54
Ebrahim Byagowi <ebrahim@gnu.org>
2024-04-06 16:42:57 +0330

    Make sure invisible themes aren't clickable
    Thanks to Arasteh for finding it

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/ThemeDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/ThemeDialog.kt
@@ -100,14 +100,15 @@ fun ThemeDialog(onDismissRequest: () -> Unit) {
                         systemThemeOptions.forEach { (label, preferenceKey, selectedTheme) ->
                             // To make sure the label and radio button will take the same size
                             Box(contentAlignment = Alignment.TopCenter) {
-                                val invisibleRadio =
+                                val disableRadio =
                                     !entry.isDark.xor(preferenceKey == PREF_SYSTEM_LIGHT_THEME)
                                 RadioButton(
                                     selected = selectedTheme == entry,
+                                    enabled = !disableRadio,
                                     onClick = {
                                         context.appPrefs.edit { putString(preferenceKey, entry.key) }
                                     },
-                                    modifier = if (invisibleRadio || entry == Theme.SYSTEM_DEFAULT) invisible else Modifier,
+                                    modifier = if (disableRadio || entry == Theme.SYSTEM_DEFAULT) invisible else Modifier,
                                 )
                                 Text(
                                     stringResource(label),
