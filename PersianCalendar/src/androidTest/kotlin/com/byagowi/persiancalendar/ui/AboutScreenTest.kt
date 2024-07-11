6c1c3ebdc8709c12ecfc7270713fb4d223bef67f
Ebrahim Byagowi <ebrahim@gnu.org>
2024-07-11 13:41:44 +0330

    ci: Improve the test

--- PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
+++ PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
@@ -1,10 +1,16 @@
 package com.byagowi.persiancalendar.ui
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PermDeviceInformation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -16,6 +22,8 @@ import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_INFO
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_LICENSES
 import com.byagowi.persiancalendar.ui.common.AppIconButton
+import com.byagowi.persiancalendar.ui.common.NavigationOpenDrawerIcon
+import com.byagowi.persiancalendar.ui.theme.appTopAppBarColors
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -58,18 +66,28 @@ class AboutScreenTest {
         var licensesString = ""
         composeTestRule.setContentWithParent { scope ->
             licensesString = stringResource(R.string.about_license_title)
-            Box(
-                modifier = Modifier.sharedBounds(
-                    rememberSharedContentState(key = SHARED_CONTENT_KEY_LICENSES),
-                    animatedVisibilityScope = scope,
-                ),
-            ) {
-                AppIconButton(
-                    icon = Icons.Default.PermDeviceInformation,
-                    title = stringResource(R.string.device_information),
-                    onClick = { navigateToLicensesIsCalled = true },
-                )
-            }
+            Scaffold(
+                containerColor = Color.Transparent,
+                topBar = {
+                    @OptIn(ExperimentalMaterial3Api::class)
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.about)) },
+                        colors = appTopAppBarColors(),
+                        navigationIcon = { NavigationOpenDrawerIcon {} },
+                        actions = {
+                            Text(
+                                licensesString,
+                                modifier = Modifier
+                                    .sharedBounds(
+                                        rememberSharedContentState(key = SHARED_CONTENT_KEY_LICENSES),
+                                        animatedVisibilityScope = scope,
+                                    )
+                                    .clickable { navigateToLicensesIsCalled = true }
+                            )
+                        }
+                    )
+                }
+            ) { paddingValues -> paddingValues.let {} }
         }
         composeTestRule.onNodeWithText(licensesString)
             .assertHasClickAction()
