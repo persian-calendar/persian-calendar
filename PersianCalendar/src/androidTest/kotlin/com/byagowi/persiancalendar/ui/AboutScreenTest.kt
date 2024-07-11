f717cd80b39819db6e73df08a4e211267c946bfc
Ebrahim Byagowi <ebrahim@gnu.org>
2024-07-11 10:59:20 +0330

    ci: Remove the just added test

--- PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
+++ PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
@@ -1,19 +1,6 @@
 package com.byagowi.persiancalendar.ui
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PermDeviceInformation
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.PlainTooltip
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TooltipBox
-import androidx.compose.material3.TooltipDefaults
-import androidx.compose.material3.rememberTooltipState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
@@ -72,39 +59,6 @@ class AboutScreenTest {
             .assertHasClickAction()
             .performClick()
         assert(navigateToLicensesIsCalled)
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Test
-    fun someImportantTest() {
-        var called = false
-        var title = ""
-        composeTestRule.setContentWithParent { scope ->
-            title = stringResource(R.string.about_license_title)
-            Scaffold { padding ->
-                padding.let {}
-                Box(
-                    modifier = Modifier.sharedBounds(
-                        rememberSharedContentState(key = "SOME_SAMPLE_KEY"),
-                        animatedVisibilityScope = scope,
-                    )
-                ) {
-                    TooltipBox(
-                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
-                        tooltip = { PlainTooltip { Text(title) } },
-                        state = rememberTooltipState()
-                    ) {
-                        IconButton(onClick = { called = true }) {
-                            Icon(Icons.Default.PermDeviceInformation, contentDescription = title)
-                        }
-                    }
-                }
-            }
-        }
-        composeTestRule.onNodeWithText(title)
-            .assertHasClickAction()
-            .performClick()
-        assert(called)
     }
 
     @Test
