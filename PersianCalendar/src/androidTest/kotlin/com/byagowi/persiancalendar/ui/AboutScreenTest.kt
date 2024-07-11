f35b9390244b63562d5e7dd97552d443ddeee8cd
Ebrahim Byagowi <ebrahim@gnu.org>
2024-07-11 10:41:13 +0330

    ci: Try to replicate the issue better

--- PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
+++ PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/AboutScreenTest.kt
@@ -1,8 +1,18 @@
 package com.byagowi.persiancalendar.ui
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PermDeviceInformation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertHasClickAction
@@ -64,22 +74,34 @@ class AboutScreenTest {
         assert(navigateToLicensesIsCalled)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Test
-    fun test() {
+    fun someImportantTest() {
         var called = false
-        val licensesString = "Test"
+        var title = ""
         composeTestRule.setContentWithParent { scope ->
-            Text(
-                "Test",
-                modifier = Modifier
-                    .sharedBounds(
-                        rememberSharedContentState(key = "key"),
+            title = stringResource(R.string.about_license_title)
+            Scaffold { padding ->
+                padding.let {}
+                Box(
+                    modifier = Modifier.sharedBounds(
+                        rememberSharedContentState(key = "SOME_SAMPLE_KEY"),
                         animatedVisibilityScope = scope,
                     )
-                    .clickable { called = true }
-            )
+                ) {
+                    TooltipBox(
+                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                        tooltip = { PlainTooltip { Text(title) } },
+                        state = rememberTooltipState()
+                    ) {
+                        IconButton(onClick = { called = true }) {
+                            Icon(Icons.Default.PermDeviceInformation, contentDescription = title)
+                        }
+                    }
+                }
+            }
         }
-        composeTestRule.onNodeWithText(licensesString)
+        composeTestRule.onNodeWithText(title)
             .assertHasClickAction()
             .performClick()
         assert(called)
