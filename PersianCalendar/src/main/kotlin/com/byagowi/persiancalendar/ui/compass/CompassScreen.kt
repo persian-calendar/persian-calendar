b6a79a227079056f95c1e13531dab9b2f42cd7b1
Ebrahim Byagowi <ebrahim@gnu.org>
2024-08-22 13:30:37 +0330

    Enable map view transition again

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/compass/CompassScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/compass/CompassScreen.kt
@@ -263,10 +263,10 @@ fun SharedTransitionScope.CompassScreen(
                     )
                 }
                 Box(
-//                    modifier = Modifier.sharedBounds(
-//                        rememberSharedContentState(key = SHARED_CONTENT_KEY_MAP),
-//                        animatedVisibilityScope = animatedContentScope,
-//                    ),
+                    modifier = Modifier.sharedBounds(
+                        rememberSharedContentState(key = SHARED_CONTENT_KEY_MAP),
+                        animatedVisibilityScope = animatedContentScope,
+                    ),
                 ) {
                     AppIconButton(
                         icon = Icons.Default.Map,
