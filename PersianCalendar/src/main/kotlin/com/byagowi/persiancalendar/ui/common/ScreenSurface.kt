e68180816b8c11d12e601900da44ef784bd0819a
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-17 20:53:55 +0330

    ci: Let's try to disable shared element animation

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
@@ -1,5 +1,7 @@
 package com.byagowi.persiancalendar.ui.common
 
+import android.content.Context
+import android.provider.Settings
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExperimentalSharedTransitionApi
@@ -8,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_CARD
 import com.byagowi.persiancalendar.ui.theme.animatedSurfaceColor
 import com.byagowi.persiancalendar.ui.utils.materialCornerExtraLargeTop
@@ -23,11 +26,14 @@ fun SharedTransitionScope.ScreenSurface(
         shape = shape,
         color = animatedSurfaceColor(),
         // Workaround CI not liking shared elements
-        modifier = Modifier.sharedBounds(
+        modifier = if (LocalContext.current.animationsEnabled()) Modifier.sharedBounds(
             rememberSharedContentState(SHARED_CONTENT_KEY_CARD),
             animatedVisibilityScope = animatedContentScope,
             enter = EnterTransition.None,
-        ),
+        ) else Modifier,
         content = content,
     )
 }
+
+private fun Context.animationsEnabled(): Boolean =
+    Settings.Global.getFloat(contentResolver, Settings.Global.ANIMATOR_DURATION_SCALE, 1.0f) != 0f
