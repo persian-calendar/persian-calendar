65196df14c9286db128b877bfc83689e57ccb92e
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-20 00:59:59 +0330

    level: Make sure stop button is on the right place on different screens

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/level/LevelScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/level/LevelScreen.kt
@@ -14,10 +14,12 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.icons.Icons
@@ -38,7 +40,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.getSystemService
@@ -220,10 +224,16 @@ fun SharedTransitionScope.LevelScreen(
                         }
                     }
                 }
+
+                var bottomWindowInset by remember { mutableStateOf(Dp.Hairline) }
+                if (!isFullscreen) bottomWindowInset = with(LocalDensity.current) {
+                    WindowInsets.systemBars.getBottom(this).toDp()
+                }
+
                 if (isFullscreen) Box(
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
-                        .padding(end = 24.dp, bottom = 40.dp),
+                        .padding(end = 24.dp, bottom = bottomWindowInset + 16.dp),
                 ) { StopButton(isStopped) { isStopped = it } }
 
                 ShrinkingFloatingActionButton(
