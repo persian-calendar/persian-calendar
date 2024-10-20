cceaec1dfa83a74f9c7ffd2d43b84ac4848f586d
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-20 21:28:25 +0330

    ci: Disable share button shared element animation
    It doesn't like it apparently :/

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ShareButton.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ShareButton.kt
@@ -8,18 +8,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_SHARE_BUTTON
+import com.byagowi.persiancalendar.ui.utils.isOnCI
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun SharedTransitionScope.ShareActionButton(
     animatedContentScope: AnimatedContentScope,
-    action: () -> Unit
+    action: () -> Unit,
 ) {
     Box(
-        Modifier.sharedElement(
+        if (LocalContext.current.isOnCI()) Modifier else Modifier.sharedElement(
             rememberSharedContentState(SHARED_CONTENT_KEY_SHARE_BUTTON),
             animatedVisibilityScope = animatedContentScope,
         ),
