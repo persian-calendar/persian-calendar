fbf0913078a978d74da03c48bea6ebe866a549bb
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-21 15:31:00 +0330

    Make card content is also animated
    It separates content from card itself and animates
    the card itself with sharedElement but the content
    using sharedBounds for a better experience.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/Constants.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/Constants.kt
@@ -148,6 +148,7 @@ const val AFGHANISTAN_TIMEZONE_ID = "Asia/Kabul"
 const val NEPAL_TIMEZONE_ID = "Asia/Kathmandu"
 
 const val SHARED_CONTENT_KEY_CARD = "card" // Common card animation
+const val SHARED_CONTENT_KEY_CARD_CONTENT = "cardContent" // Common card content animation
 const val SHARED_CONTENT_KEY_OPEN_DRAWER = "openDrawer" // Make sure open drawer icon is stay put
 const val SHARED_CONTENT_KEY_THREE_DOTS_MENU = "threeDots" // Make sure menu icon is stay put
 const val SHARED_CONTENT_KEY_SHARE_BUTTON = "shareButton" // Animate placement of share button
--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
@@ -3,12 +3,19 @@ package com.byagowi.persiancalendar.ui.common
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Constraints
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_CARD
+import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_CARD_CONTENT
 import com.byagowi.persiancalendar.ui.theme.animatedSurfaceColor
 import com.byagowi.persiancalendar.ui.utils.isOnCI
 import com.byagowi.persiancalendar.ui.utils.materialCornerExtraLargeTop
@@ -20,14 +27,35 @@ fun SharedTransitionScope.ScreenSurface(
     shape: Shape = materialCornerExtraLargeTop(),
     content: @Composable () -> Unit,
 ) {
-    Surface(
-        shape = shape,
-        color = animatedSurfaceColor(),
-        // Workaround CI not liking shared elements
-        modifier = if (LocalContext.current.isOnCI()) Modifier else Modifier.sharedElement(
-            rememberSharedContentState(SHARED_CONTENT_KEY_CARD),
-            animatedVisibilityScope = animatedContentScope,
-        ),
-        content = content,
-    )
+    Layout(content = {
+        Surface(
+            shape = shape,
+            color = animatedSurfaceColor(),
+            // Workaround CI not liking shared elements
+            modifier = if (LocalContext.current.isOnCI()) Modifier else Modifier.sharedElement(
+                rememberSharedContentState(SHARED_CONTENT_KEY_CARD),
+                animatedVisibilityScope = animatedContentScope,
+            ),
+        ) {}
+        Surface(
+            if (LocalContext.current.isOnCI()) Modifier else Modifier.sharedBounds(
+                rememberSharedContentState(SHARED_CONTENT_KEY_CARD_CONTENT),
+                animatedVisibilityScope = animatedContentScope,
+            ),
+            shape = shape,
+            contentColor = animateColorAsState(
+                contentColorFor(MaterialTheme.colorScheme.surface),
+                label = "content color",
+            ).value,
+            color = Color.Transparent,
+            content = content,
+        )
+    }) { measurables, constraints ->
+        val child = measurables[1].measure(constraints)
+        val parent = measurables[0].measure(Constraints.fixed(child.width, child.height))
+        layout(child.width, child.height) {
+            parent.placeRelative(0, 0)
+            child.placeRelative(0, 0)
+        }
+    }
 }
