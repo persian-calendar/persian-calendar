2c2255e95eaf419fb6f25f2b9a679f70f9249ed4
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-22 23:43:19 +0330

    Make dialog below bar won't be have initial transition

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/AppDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/AppDialog.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import com.byagowi.persiancalendar.ui.utils.SettingsHorizontalPaddingItem
-import com.byagowi.persiancalendar.variants.debugLog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -77,11 +76,12 @@ fun AppDialog(
                 }
 
                 if (neutralButton != null || dismissButton != null || confirmButton != null) {
-                    val bottomDividerAlpha by animateFloatAsState(
-                        if (scrollState.value == scrollState.maxValue || scrollState.maxValue == Int.MAX_VALUE) 0f else 1f,
+                    val bottomDividerAlpha = if (scrollState.maxValue == Int.MAX_VALUE) 0f
+                    else animateFloatAsState(
+                        if (scrollState.value == scrollState.maxValue) 0f else 1f,
                         label = "bottom divider alpha",
                         animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
-                    )
+                    ).value
                     HorizontalDivider(Modifier.alpha(bottomDividerAlpha))
                     Spacer(modifier = Modifier.height(8.dp))
 
