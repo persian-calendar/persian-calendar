dd4782782954281a8b10bed5d381979faaa7067d
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-23 15:45:57 +0330

    v9.3.0

--- PersianCalendar/build.gradle.kts
+++ PersianCalendar/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.byagowi.persiancalendar"
         minSdk = 21
         targetSdk = 35
-        versionCode = 920
-        versionName = "9.2.0"
+        versionCode = 930
+        versionName = "9.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // It lowers the APK size and prevents crash in AboutScreen in API 21-23
         vectorDrawables.useSupportLibrary = true
