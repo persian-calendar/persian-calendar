7ef0f9e66c7879d85bd2fa11d3704ab32c4813fd
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-26 14:02:44 +0330

    Accept everything as debugLog input

--- PersianCalendar/src/debug/kotlin/com/byagowi/persiancalendar/variants/Variants.kt
+++ PersianCalendar/src/debug/kotlin/com/byagowi/persiancalendar/variants/Variants.kt
@@ -3,8 +3,8 @@ package com.byagowi.persiancalendar.variants
 import android.util.Log
 import com.byagowi.persiancalendar.LOG_TAG
 
-fun debugLog(message: String) {
-    Log.d(LOG_TAG, message)
+fun debugLog(message: Any?) {
+    Log.d(LOG_TAG, message.toString())
 }
 
 inline val <T> T.debugAssertNotNull: T inline get() = checkNotNull(this)
--- PersianCalendar/src/release/kotlin/com/byagowi/persiancalendar/variants/Variants.kt
+++ PersianCalendar/src/release/kotlin/com/byagowi/persiancalendar/variants/Variants.kt
@@ -1,7 +1,7 @@
 package com.byagowi.persiancalendar.variants
 
 @Suppress("UNUSED_PARAMETER")
-fun debugLog(message: String) = Unit // Nothing here
+fun debugLog(message: Any?) = Unit // Nothing here
 
 @Suppress("UNUSED_PARAMETER")
 inline val <T> T.debugAssertNotNull: T inline get() = this // Nothing here
