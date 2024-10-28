04ba8769576925fb0ad35e601a34ebd3d5f70db9
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-28 12:13:52 +0330

    Add an animation on three dots menu open

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ThreeDotsDropdownMenu.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ThreeDotsDropdownMenu.kt
@@ -3,6 +3,9 @@ package com.byagowi.persiancalendar.ui.common
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.icons.Icons
@@ -13,8 +16,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.LayoutDirection
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_THREE_DOTS_MENU
 import com.byagowi.persiancalendar.ui.utils.isOnCI
@@ -32,10 +38,20 @@ fun SharedTransitionScope.ThreeDotsDropdownMenu(
         )
     ) {
         var showMenu by rememberSaveable { mutableStateOf(false) }
-        AppIconButton(
-            icon = Icons.Default.MoreVert,
-            title = stringResource(R.string.more_options),
-        ) { showMenu = !showMenu }
+        run {
+            val rotate by animateFloatAsState(
+                if (!showMenu) 0f
+                else if (LocalLayoutDirection.current == LayoutDirection.Ltr) 90f else -90f,
+                animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
+                label = "rotate",
+            )
+            Box(Modifier.rotate(rotate)) {
+                AppIconButton(
+                    icon = Icons.Default.MoreVert,
+                    title = stringResource(R.string.more_options),
+                ) { showMenu = !showMenu }
+            }
+        }
         AppDropdownMenu(
             expanded = showMenu,
             onDismissRequest = { showMenu = false },
