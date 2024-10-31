3df38cd429d51a0f507fb0b7065c6013b80d8096
Ebrahim Byagowi <ebrahim@gnu.org>
2024-10-31 21:30:56 +0330

    Move pray time html report logic to its own file

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/CalendarScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/CalendarScreen.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Configuration
-import android.content.res.Resources
 import android.os.Build
 import android.os.PowerManager
 import android.provider.CalendarContract
@@ -115,8 +114,6 @@ import com.byagowi.persiancalendar.entities.Calendar
 import com.byagowi.persiancalendar.entities.CalendarEvent
 import com.byagowi.persiancalendar.entities.EventsStore
 import com.byagowi.persiancalendar.entities.Jdn
-import com.byagowi.persiancalendar.global.calculationMethod
-import com.byagowi.persiancalendar.global.cityName
 import com.byagowi.persiancalendar.global.coordinates
 import com.byagowi.persiancalendar.global.enabledCalendars
 import com.byagowi.persiancalendar.global.isIranHolidaysEnabled
@@ -125,7 +122,6 @@ import com.byagowi.persiancalendar.global.isTalkBackEnabled
 import com.byagowi.persiancalendar.global.language
 import com.byagowi.persiancalendar.global.mainCalendar
 import com.byagowi.persiancalendar.global.secondaryCalendar
-import com.byagowi.persiancalendar.global.spacedComma
 import com.byagowi.persiancalendar.global.updateStoredPreference
 import com.byagowi.persiancalendar.ui.calendar.calendarpager.CalendarPager
 import com.byagowi.persiancalendar.ui.calendar.calendarpager.calendarPagerState
@@ -151,46 +147,24 @@ import com.byagowi.persiancalendar.ui.common.TodayActionButton
 import com.byagowi.persiancalendar.ui.theme.appCrossfadeSpec
 import com.byagowi.persiancalendar.ui.theme.appTopAppBarColors
 import com.byagowi.persiancalendar.ui.utils.bringMarketPage
-import com.byagowi.persiancalendar.ui.utils.isRtl
 import com.byagowi.persiancalendar.ui.utils.materialCornerExtraLargeNoBottomEnd
 import com.byagowi.persiancalendar.ui.utils.openHtmlInBrowser
 import com.byagowi.persiancalendar.utils.TWO_SECONDS_IN_MILLIS
-import com.byagowi.persiancalendar.utils.calculatePrayTimes
 import com.byagowi.persiancalendar.utils.calendar
 import com.byagowi.persiancalendar.utils.dayTitleSummary
 import com.byagowi.persiancalendar.utils.formatNumber
 import com.byagowi.persiancalendar.utils.getA11yDaySummary
 import com.byagowi.persiancalendar.utils.getEnabledAlarms
-import com.byagowi.persiancalendar.utils.getFromStringId
-import com.byagowi.persiancalendar.utils.getTimeNames
 import com.byagowi.persiancalendar.utils.hasAnyWidgetUpdateRecently
 import com.byagowi.persiancalendar.utils.logException
 import com.byagowi.persiancalendar.utils.monthFormatForSecondaryCalendar
 import com.byagowi.persiancalendar.utils.monthName
 import com.byagowi.persiancalendar.utils.preferences
 import com.byagowi.persiancalendar.utils.supportedYearOfIranCalendar
-import com.byagowi.persiancalendar.utils.titleStringId
 import com.byagowi.persiancalendar.utils.update
 import com.byagowi.persiancalendar.variants.debugAssertNotNull
-import io.github.persiancalendar.calendar.AbstractDate
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
-import kotlinx.html.body
-import kotlinx.html.h1
-import kotlinx.html.head
-import kotlinx.html.html
-import kotlinx.html.meta
-import kotlinx.html.script
-import kotlinx.html.stream.createHTML
-import kotlinx.html.style
-import kotlinx.html.table
-import kotlinx.html.tbody
-import kotlinx.html.td
-import kotlinx.html.tfoot
-import kotlinx.html.th
-import kotlinx.html.thead
-import kotlinx.html.tr
-import kotlinx.html.unsafe
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
@@ -869,7 +843,7 @@ private fun SharedTransitionScope.Menu(
                 val selectedMonthOffset = viewModel.selectedMonthOffset.value
                 val selectedMonth =
                     mainCalendar.getMonthStartFromMonthsDistance(Jdn.today(), selectedMonthOffset)
-                context.openHtmlInBrowser(createOwghatHtmlReport(context.resources, selectedMonth))
+                context.openHtmlInBrowser(prayTimeHtmlReport(context.resources, selectedMonth))
             },
         )
 
@@ -914,70 +888,6 @@ private fun SharedTransitionScope.Menu(
                     closeMenu()
                 }
             }
-        }
-    }
-}
-
-private fun createOwghatHtmlReport(resources: Resources, date: AbstractDate): String {
-    return createHTML().html {
-        val coordinates = coordinates.value ?: return@html
-        attributes["lang"] = language.value.language
-        attributes["dir"] = if (resources.isRtl) "rtl" else "ltr"
-        head {
-            meta(charset = "utf8")
-            style {
-                unsafe {
-                    +"""
-                        body { font-family: system-ui }
-                        th, td { padding: 0 .5em; text-align: center }
-                        td { border-top: 1px solid lightgray; font-size: 95% }
-                        h1 { text-align: center; font-size: 110% }
-                        table { margin: 0 auto; }
-                    """.trimIndent()
-                }
-            }
-        }
-        body {
-            h1 {
-                +listOfNotNull(
-                    cityName.value,
-                    language.value.my.format(date.monthName, formatNumber(date.year))
-                ).joinToString(spacedComma)
-            }
-            table {
-                thead {
-                    tr {
-                        th { +resources.getString(R.string.day) }
-                        getTimeNames().forEach { th { +resources.getString(it) } }
-                    }
-                }
-                tbody {
-                    (0..<mainCalendar.getMonthLength(date.year, date.month)).forEach { day ->
-                        tr {
-                            val prayTimes = coordinates.calculatePrayTimes(
-                                Jdn(
-                                    mainCalendar.createDate(date.year, date.month, day)
-                                ).toGregorianCalendar()
-                            )
-                            th { +formatNumber(day + 1) }
-                            getTimeNames().forEach {
-                                td { +prayTimes.getFromStringId(it).toBasicFormatString() }
-                            }
-                        }
-                    }
-                }
-                if (calculationMethod.value != language.value.preferredCalculationMethod) {
-                    tfoot {
-                        tr {
-                            td {
-                                colSpan = "10"
-                                +resources.getString(calculationMethod.value.titleStringId)
-                            }
-                        }
-                    }
-                }
-            }
-            script { unsafe { +"print()" } }
         }
     }
 }
---
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/PrayTimeHtmlReport.kt
@@ -0,0 +1,99 @@
+package com.byagowi.persiancalendar.ui.calendar
+
+import android.content.res.Resources
+import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.entities.Jdn
+import com.byagowi.persiancalendar.global.calculationMethod
+import com.byagowi.persiancalendar.global.cityName
+import com.byagowi.persiancalendar.global.coordinates
+import com.byagowi.persiancalendar.global.language
+import com.byagowi.persiancalendar.global.mainCalendar
+import com.byagowi.persiancalendar.global.spacedComma
+import com.byagowi.persiancalendar.ui.utils.isRtl
+import com.byagowi.persiancalendar.utils.calculatePrayTimes
+import com.byagowi.persiancalendar.utils.formatNumber
+import com.byagowi.persiancalendar.utils.getFromStringId
+import com.byagowi.persiancalendar.utils.getTimeNames
+import com.byagowi.persiancalendar.utils.monthName
+import com.byagowi.persiancalendar.utils.titleStringId
+import io.github.persiancalendar.calendar.AbstractDate
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.html
+import kotlinx.html.meta
+import kotlinx.html.script
+import kotlinx.html.stream.createHTML
+import kotlinx.html.style
+import kotlinx.html.table
+import kotlinx.html.tbody
+import kotlinx.html.td
+import kotlinx.html.tfoot
+import kotlinx.html.th
+import kotlinx.html.thead
+import kotlinx.html.tr
+import kotlinx.html.unsafe
+
+fun prayTimeHtmlReport(resources: Resources, date: AbstractDate): String {
+    return createHTML().html {
+        val coordinates = coordinates.value ?: return@html
+        attributes["lang"] = language.value.language
+        attributes["dir"] = if (resources.isRtl) "rtl" else "ltr"
+        head {
+            meta(charset = "utf8")
+            style {
+                unsafe {
+                    +"""
+                        body { font-family: system-ui }
+                        th, td { padding: 0 .5em; text-align: center }
+                        td { border-top: 1px solid lightgray; font-size: 95% }
+                        h1 { text-align: center; font-size: 110% }
+                        table { margin: 0 auto; }
+                    """.trimIndent()
+                }
+            }
+        }
+        body {
+            h1 {
+                +listOfNotNull(
+                    cityName.value,
+                    language.value.my.format(date.monthName, formatNumber(date.year))
+                ).joinToString(spacedComma)
+            }
+            table {
+                thead {
+                    tr {
+                        th { +resources.getString(R.string.day) }
+                        getTimeNames().forEach { th { +resources.getString(it) } }
+                    }
+                }
+                tbody {
+                    (0..<mainCalendar.getMonthLength(date.year, date.month)).forEach { day ->
+                        tr {
+                            val prayTimes = coordinates.calculatePrayTimes(
+                                Jdn(
+                                    mainCalendar.createDate(date.year, date.month, day)
+                                ).toGregorianCalendar()
+                            )
+                            th { +formatNumber(day + 1) }
+                            getTimeNames().forEach {
+                                td { +prayTimes.getFromStringId(it).toBasicFormatString() }
+                            }
+                        }
+                    }
+                }
+                if (calculationMethod.value != language.value.preferredCalculationMethod) {
+                    tfoot {
+                        tr {
+                            td {
+                                colSpan = "10"
+                                +resources.getString(calculationMethod.value.titleStringId)
+                            }
+                        }
+                    }
+                }
+            }
+            script { unsafe { +"print()" } }
+        }
+    }
+}
