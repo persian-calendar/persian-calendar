c366f63ceb39117aff8fbe20f79692aee2c27778
Ebrahim Byagowi <ebrahim@gnu.org>
2024-11-03 10:22:56 +0330

    Minor code tweak

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/calendarpager/FixedSizeHorizontalGrid.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/calendarpager/FixedSizeHorizontalGrid.kt
@@ -2,26 +2,26 @@ package com.byagowi.persiancalendar.ui.calendar.calendarpager
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.util.fastForEachIndexed
 import kotlin.math.roundToInt
 
 @Composable
 fun FixedSizeHorizontalGrid(columnsCount: Int, rowsCount: Int, content: @Composable () -> Unit) {
-    Layout(content = content) { measurables, constraints ->
-        val widthPx = constraints.maxWidth
-        val heightPx = constraints.maxHeight
-        val cellWidthPx = widthPx / columnsCount.toFloat()
-        val cellHeightPx = heightPx / rowsCount.toFloat()
-        val cellsConstraints =
-            Constraints.fixed(cellWidthPx.roundToInt(), cellHeightPx.roundToInt())
-        layout(widthPx, heightPx) {
-            measurables.fastForEachIndexed { cellIndex, measurable ->
+    Layout(content) { measurables: List<Measurable>, constraints: Constraints ->
+        val totalWidth = constraints.maxWidth
+        val totalHeight = constraints.maxHeight
+        val cellWidth = totalWidth / columnsCount.toFloat()
+        val cellHeight = totalHeight / rowsCount.toFloat()
+        val cellsConstraints = Constraints.fixed(cellWidth.roundToInt(), cellHeight.roundToInt())
+        layout(totalWidth, totalHeight) {
+            measurables.fastForEachIndexed { cellIndex: Int, measurable: Measurable ->
                 val column = cellIndex % columnsCount
                 val row = cellIndex / columnsCount
                 measurable.measure(cellsConstraints).placeRelative(
-                    (column * cellWidthPx).roundToInt(),
-                    (row * cellHeightPx).roundToInt(),
+                    (column * cellWidth).roundToInt(),
+                    (row * cellHeight).roundToInt(),
                 )
             }
         }
