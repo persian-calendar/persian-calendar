cfa486bf8c3a8afa9db4b4d5f55ddf51894ff3ec
Ebrahim Byagowi <ebrahim@gnu.org>
2024-11-06 22:01:15 +0330

    Add a tooltip for agenda's more button

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/EventsTab.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/EventsTab.kt
@@ -29,9 +29,14 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -173,21 +178,28 @@ fun SharedTransitionScope.EventsTab(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MoreButton(contentDescription: String, action: () -> Unit) {
-    Box(
-        Modifier
-            .clip(MaterialTheme.shapes.large)
-            .background(MaterialTheme.colorScheme.surfaceVariant)
-            .clickable { action() }
-            .padding(horizontal = 12.dp, vertical = 1.dp),
+fun MoreButton(title: String, action: () -> Unit) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        tooltip = { PlainTooltip { Text(title) } },
+        state = rememberTooltipState()
     ) {
-        Icon(
-            Icons.Default.MoreHoriz,
-            tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            modifier = Modifier.size(24.dp),
-            contentDescription = contentDescription,
-        )
+        Box(
+            Modifier
+                .clip(MaterialTheme.shapes.large)
+                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .clickable { action() }
+                .padding(horizontal = 12.dp, vertical = 1.dp),
+        ) {
+            Icon(
+                Icons.Default.MoreHoriz,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.size(24.dp),
+                contentDescription = title,
+            )
+        }
     }
 }
 
