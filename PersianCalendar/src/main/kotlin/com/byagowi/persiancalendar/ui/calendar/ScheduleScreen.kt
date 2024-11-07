1334c3f8630ebbe2d13f53b4976362bac63595a9
Ebrahim Byagowi <ebrahim@gnu.org>
2024-11-07 16:08:56 +0330

    schedule: Open year view on months click
    As Arasteh's suggestion

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/ScheduleScreen.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/ScheduleScreen.kt
@@ -278,6 +278,7 @@ fun SharedTransitionScope.ScheduleScreen(
                                                         Jdn.today(), Jdn(nextMonth),
                                                     )
                                                 )
+                                                calendarViewModel.openYearView()
                                                 navigateUp()
                                             },
                                     )
