fde3456aa9a30f5ca5bc83de87e0ec11c6dd3e06
Ebrahim Byagowi <ebrahim@gnu.org>
2024-12-24 13:18:47 +0330

    graph: Use adjaceny size instead of storing verticies separately

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/Graph.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/Graph.kt
@@ -2,7 +2,7 @@ package com.byagowi.persiancalendar.ui.calendar
 
 // These aren't the most optimized implementations, probably going to be replaced with
 // better one in the future though they should be enough for our use
-class Graph(private val verticesCount: Int) {
+class Graph(verticesCount: Int) {
     private val adjacency = List(verticesCount) { mutableSetOf<Int>() }
 
     override fun toString(): String = adjacency.toString()
@@ -14,8 +14,8 @@ class Graph(private val verticesCount: Int) {
 
     // https://www.geeksforgeeks.org/connected-components-in-an-undirected-graph/
     fun connectedComponents() = sequence {
-        val visited = BooleanArray(verticesCount)
-        (0..<verticesCount).forEach { if (!visited[it]) yield(sequence { dfs(it, visited) }) }
+        val visited = BooleanArray(adjacency.size)
+        adjacency.indices.forEach { if (!visited[it]) yield(sequence { dfs(it, visited) }) }
     }
 
     private suspend fun SequenceScope<Int>.dfs(v: Int, visited: BooleanArray) {
@@ -26,13 +26,13 @@ class Graph(private val verticesCount: Int) {
 
     // https://www.geeksforgeeks.org/graph-coloring-set-2-greedy-algorithm/
     fun colors(): List<Int> {
-        if (verticesCount == 0) return emptyList()
-        val result = MutableList(verticesCount) { -1 }
+        if (adjacency.isEmpty()) return emptyList()
+        val result = MutableList(adjacency.size) { -1 }
         result[0] = 0
-        val available = BooleanArray(verticesCount)
-        (1..<verticesCount).forEach { x ->
+        val available = BooleanArray(adjacency.size)
+        adjacency.indices.drop(1).forEach { x ->
             adjacency[x].forEach { if (result[it] != -1) available[result[it]] = true }
-            result[x] = (0..<verticesCount).firstOrNull { !available[it] } ?: verticesCount
+            result[x] = adjacency.indices.firstOrNull { !available[it] } ?: adjacency.size
             available.fill(false)
         }
         return result
