a7b7774fec79ce7a2bb4eb8546884f8fa25a8ab2
Ebrahim Byagowi <ebrahim@gnu.org>
2024-12-28 03:40:22 +0330

    calendar: Make week view default swipe up action

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/SwipeUpAction.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/SwipeUpAction.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import com.byagowi.persiancalendar.R
 
 enum class SwipeUpAction(@StringRes val titleId: Int) {
-    Schedule(R.string.schedule),
-    DayView(R.string.day_view),
     WeekView(R.string.week_view),
+    DayView(R.string.day_view),
+    Schedule(R.string.schedule),
 }
