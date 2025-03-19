d33aabe2b1daf260f6f4015d4ba49780e356dfb0
Ebrahim Byagowi <ebrahim@gnu.org>
2025-03-19 15:26:10 +0330

    wear: 0.0.6

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.byagowi.persiancalendar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
     }
 
     buildTypes {
