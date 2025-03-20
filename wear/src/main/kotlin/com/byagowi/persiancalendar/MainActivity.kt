e96aed29ecbeb95f8c6553499a16efe41953daf3
Ebrahim Byagowi <ebrahim@gnu.org>
2025-03-20 22:06:52 +0330

    wear: Correctly switch to Material 3
    And add a sample settings screen

--- wear/src/main/kotlin/com/byagowi/persiancalendar/MainActivity.kt
+++ wear/src/main/kotlin/com/byagowi/persiancalendar/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,12 +25,15 @@ import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.foundation.lazy.items
 import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Switch
-import androidx.wear.compose.material.Text
 import androidx.wear.compose.material3.AppScaffold
-import androidx.wear.compose.material3.Button
+import androidx.wear.compose.material3.EdgeButton
+import androidx.wear.compose.material3.EdgeButtonSize
+import androidx.wear.compose.material3.Icon
+import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ScreenScaffold
+import androidx.wear.compose.material3.SwitchButton
+import androidx.wear.compose.material3.Text
+import androidx.wear.compose.material3.dynamicColorScheme
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
@@ -40,7 +49,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun WearApp() {
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-        MaterialTheme {
+        MaterialTheme(
+            colorScheme = dynamicColorScheme(LocalContext.current) ?: MaterialTheme.colorScheme
+        ) {
             AppScaffold {
                 val navController = rememberSwipeDismissableNavController()
                 val mainRoute = "app"
@@ -63,19 +74,24 @@ private fun WearApp() {
 private fun SettingsScreen() {
     ScreenScaffold {
         ScalingLazyColumn {
-            item {
-                Button(
-                    onClick = {},
-                    label = {
-                        Row {
-                            Text("Item ${1 + 1}")
-                            Switch(true)
-                        }
-                    },
-                )
-            }
+            item { Text("نمایش رویدادها") }
+            item { EventsSwitch("غیرتعطیل رسمی\nدانشگاه تهران") }
+            item { EventsSwitch("بین‌المللی") }
         }
     }
+}
+
+@Composable
+private fun EventsSwitch(title: String) {
+    var checked by remember { mutableStateOf(false) }
+    SwitchButton(
+        checked,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        label = { Text(title) },
+        onCheckedChange = { checked = it }
+    )
 }
 
 @Composable
@@ -90,8 +106,7 @@ private fun MainScreen(navigateToSettings: () -> Unit) {
 //            ) {
 //                Icon(
 //                    Icons.Default.Settings,
-//                    contentDescription = "تنظیمات",
-//                    tint = MaterialTheme.colors.onPrimary
+//                    contentDescription = "تنظیمات"
 //                )
 //            }
 //        },
@@ -106,21 +121,21 @@ private fun MainScreen(navigateToSettings: () -> Unit) {
 private fun EntryView(it: Entry) {
     if (it.type == EntryType.Date) Text(
         text = it.title,
-        style = MaterialTheme.typography.caption1,
+        style = MaterialTheme.typography.titleLarge,
         modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
     ) else {
         val isHoliday = it.type == EntryType.Holiday
         Text(
             it.title,
-            color = if (isHoliday) MaterialTheme.colors.onPrimary
-            else MaterialTheme.colors.onSurface,
+            color = if (isHoliday) MaterialTheme.colorScheme.onPrimary
+            else MaterialTheme.colorScheme.onSurface,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
             modifier = Modifier
                 .padding(top = 4.dp, start = 8.dp, end = 8.dp)
                 .background(
-                    if (isHoliday) MaterialTheme.colors.primary
-                    else MaterialTheme.colors.surface,
+                    if (isHoliday) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.surfaceContainer,
                     RoundedCornerShape(24.dp),
                 )
                 .fillMaxWidth()
