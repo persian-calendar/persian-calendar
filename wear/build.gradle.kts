2f8d5193a27c9bb4ad538eb6fc2b77bc6f56e503
Ebrahim Byagowi <ebrahim@gnu.org>
2025-03-22 14:16:53 +0330

    wear: 0.1.1

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.byagowi.persiancalendar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.1.0"
+        versionCode = 11
+        versionName = "0.1.1"
         androidResources.localeFilters += listOf("en", "fa")
     }
 
