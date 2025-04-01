6db52b7141984e2ecce23fda2aadaa3d8247207f
Ebrahim Byagowi <ebrahim@gnu.org>
2025-04-01 23:48:52 +0330

    Make it possible to click on each event

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarCarService.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarCarService.kt
@@ -2,23 +2,16 @@ package com.byagowi.persiancalendar.service
 
 import android.content.Intent
 import androidx.car.app.CarAppService
-import androidx.car.app.Screen
 import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
 import com.byagowi.persiancalendar.ui.car.CalendarCarScreen
 
 class PersianCalendarCarService : CarAppService() {
-    override fun createHostValidator(): HostValidator {
-        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
-    }
+    override fun createHostValidator(): HostValidator = HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
 
     override fun onCreateSession(): Session {
-        return PersianCalendarSession()
-    }
-}
-
-class PersianCalendarSession : Session() {
-    override fun onCreateScreen(intent: Intent): Screen {
-        return CalendarCarScreen(carContext)
+        return object : Session() {
+            override fun onCreateScreen(intent: Intent) = CalendarCarScreen(carContext)
+        }
     }
 }
