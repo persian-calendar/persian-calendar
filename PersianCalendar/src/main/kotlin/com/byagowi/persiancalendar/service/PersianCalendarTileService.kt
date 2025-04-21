1e5cbeac2abbca5dd1a81a6f23ea14ccb41206f7
Ebrahim Byagowi <ebrahim@gnu.org>
2025-04-21 21:02:48 +0330

    Bring back click to bring the app
    As the complains of a number of users.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarTileService.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarTileService.kt
@@ -1,15 +1,19 @@
 package com.byagowi.persiancalendar.service
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.Icon
 import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
-import androidx.core.content.edit
 import com.byagowi.persiancalendar.PREF_TILE_STATE
 import com.byagowi.persiancalendar.entities.Jdn
 import com.byagowi.persiancalendar.global.mainCalendar
+import com.byagowi.persiancalendar.ui.MainActivity
 import com.byagowi.persiancalendar.utils.getDayIconResource
+import com.byagowi.persiancalendar.utils.launchAppPendingIntent
+import com.byagowi.persiancalendar.utils.logException
 import com.byagowi.persiancalendar.utils.monthName
 import com.byagowi.persiancalendar.utils.preferences
 
@@ -22,18 +26,20 @@ class PersianCalendarTileService : TileService() {
     private var preferredState = Tile.STATE_ACTIVE
 
     override fun onClick() {
-        preferredState =
-            if (preferredState == Tile.STATE_ACTIVE) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
-        preferences.edit { putInt(PREF_TILE_STATE, preferredState) }
-        updateTile()
+        runCatching {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                launchAppPendingIntent()?.let(::startActivityAndCollapse)
+            } else @SuppressLint("StartActivityAndCollapseDeprecated") {
+                @Suppress("DEPRECATION")
+                startActivityAndCollapse(
+                    Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+            }
+        }.onFailure(logException)
     }
 
     override fun onStartListening() {
         preferredState = preferences.getInt(PREF_TILE_STATE, Tile.STATE_ACTIVE)
-        updateTile()
-    }
-
-    private fun updateTile() {
         val tile = qsTile ?: return
         val jdn = Jdn.today()
         val today = jdn on mainCalendar
