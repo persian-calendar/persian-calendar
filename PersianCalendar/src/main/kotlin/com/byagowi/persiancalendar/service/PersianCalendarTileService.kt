f4cad127408146ea8dcafdb024ed58768a7675a3
Ebrahim Byagowi <ebrahim@gnu.org>
2025-04-21 22:10:38 +0330

    Make tile long click to toggle tile state

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarTileService.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/service/PersianCalendarTileService.kt
@@ -1,12 +1,15 @@
 package com.byagowi.persiancalendar.service
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.Icon
 import android.os.Build
+import android.os.Bundle
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.annotation.RequiresApi
+import androidx.core.content.edit
 import com.byagowi.persiancalendar.PREF_TILE_STATE
 import com.byagowi.persiancalendar.entities.Jdn
 import com.byagowi.persiancalendar.global.mainCalendar
@@ -48,5 +51,20 @@ class PersianCalendarTileService : TileService() {
         tile.contentDescription = today.monthName
         tile.state = preferredState
         tile.updateTile()
+    }
+}
+
+class ToggleTileActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && intent.action == TileService.ACTION_QS_TILE_PREFERENCES) {
+            val preferredState = preferences.getInt(PREF_TILE_STATE, Tile.STATE_ACTIVE)
+            preferences.edit {
+                val state =
+                    if (preferredState == Tile.STATE_ACTIVE) Tile.STATE_INACTIVE else Tile.STATE_ACTIVE
+                putInt(PREF_TILE_STATE, state)
+            }
+        }
+        finish()
     }
 }
