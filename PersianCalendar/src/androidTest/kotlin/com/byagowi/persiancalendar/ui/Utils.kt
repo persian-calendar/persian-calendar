a75b37e153ad6ec7a15f918bc524967d6b42cd18
Ebrahim Byagowi <ebrahim@gnu.org>
2025-04-30 23:40:41 +0330

    ci: Set LocalTextStyle in the tests

--- PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/Utils.kt
+++ PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/Utils.kt
@@ -5,18 +5,25 @@ import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.unit.LayoutDirection
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 fun ComposeContentTestRule.setContentWithParent(
     content: @Composable SharedTransitionScope.(AnimatedContentScope) -> Unit
 ) {
     setContent {
-        SharedTransitionLayout {
-            AnimatedContent(targetState = Unit, label = "") { state ->
-                state.let {}
-                content(this)
+        CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.bodyMedium) {
+            SharedTransitionLayout {
+                AnimatedContent(targetState = Unit, label = "") { state ->
+                    state.let {}
+                    content(this)
+                }
             }
         }
     }
