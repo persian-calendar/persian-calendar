93d8e8748361a2eff0ec5c58ffa3082da25895cb
Ebrahim Byagowi <ebrahim@gnu.org>
2025-06-18 09:07:22 +0330

    test: Unify use of asserts

--- PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/AstronomyTests.kt
+++ PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/AstronomyTests.kt
@@ -8,7 +8,6 @@ import com.byagowi.persiancalendar.ui.astronomy.LunarAge
 import com.byagowi.persiancalendar.ui.astronomy.Zodiac
 import com.byagowi.persiancalendar.ui.astronomy.calculateAscendant
 import com.byagowi.persiancalendar.ui.astronomy.calculateMidheaven
-import com.google.common.truth.Truth.assertThat
 import io.github.cosinekitty.astronomy.seasons
 import io.github.persiancalendar.calendar.CivilDate
 import io.github.persiancalendar.calendar.PersianDate
@@ -30,23 +29,23 @@ class AstronomyTests {
             1.84566, 5.53699, 9.22831, 12.91963, 16.61096, 20.30228, 23.99361, 27.68493
         ).forEachIndexed { index, it ->
             val lunarAge = LunarAge.fromDegrees((index + .5) * (360 / 8))
-            assertThat(lunarAge.days).isWithin(1.0e-5).of(it)
-            assertThat(lunarAge.toPhase().ordinal).isEqualTo((index + 1) % 8)
+            assertEquals(it, lunarAge.days, 1.0e-5)
+            assertEquals((index + 1) % 8, lunarAge.toPhase().ordinal)
         }
         val time = GregorianCalendar(TimeZone.getTimeZone("UTC"))
         time.clear()
         time.set(2022, 1, 13, 0, 0, 0)
-//        assertThat($(time, null, 0.0).lunarAge.days).isWithin(1.0e-2).of(11.31)
-//        assertThat($(time, null, 0.0).lunarAge.tithi).isEqualTo(12)
-//        assertThat($(time, null, 0.0).lunarSunlitTilt).isWithin(1.0e0).of(180.0)
+//        assertEquals(11.31, $(time, null, 0.0).lunarAge.days, 1.0e-2)
+//        assertEquals(12, $(time, null, 0.0).lunarAge.tithi)
+//        assertEquals(180.0, $(time, null, 0.0).lunarSunlitTilt, 1.0e0)
 
 //        val kathmandu = Coordinates(27.7172, 85.324, 1_400.0)
-//        assertThat($(time, kathmandu, 0.0).lunarAge.days).isWithin(1.0e-2).of(11.31)
-//        assertThat($(time, kathmandu, 0.0).lunarAge.tithi).isEqualTo(12)
-//        assertThat($(time, kathmandu, 0.0).lunarSunlitTilt).isWithin(1.0e0).of(180.0)
+//        assertEquals(11.31, $(time, kathmandu, 0.0).lunarAge.days, 1.0e-2)
+//        assertEquals(12, $(time, kathmandu, 0.0).lunarAge.tithi)
+//        assertEquals(180.0, $(time, kathmandu, 0.0).lunarSunlitTilt, 1.0e0)
 
 //        val nirobi = Coordinates(-1.286389, 36.817222, 1_795.0)
-//        assertThat($(time, nirobi, 0.0).lunarSunlitTilt).isWithin(1.0e0).of(.0)
+//        assertEquals(.0, $(time, nirobi, 0.0).lunarSunlitTilt, 1.0e0)
     }
 
     @Test
@@ -83,10 +82,10 @@ class AstronomyTests {
             42.17, 72.3, 106.46, 127.39, 154.32, 198.755,
             233.37, 256.915, 286.875, 307.105, 330.15, 370.88,
         ).zip(Zodiac.entries) { centerOfZodiac, zodiac ->
-            assertThat(zodiac.iauRange.average()).isWithin(1.0e-10).of(centerOfZodiac)
+            assertEquals(centerOfZodiac, zodiac.iauRange.average(), 1.0e-10)
         }
         (0..11).zip(enumValues<Zodiac>()) { it, zodiac ->
-            assertThat(zodiac.tropicalRange.average()).isWithin(1.0e-10).of(it * 30.0 + 15)
+            assertEquals(it * 30.0 + 15, zodiac.tropicalRange.average(), 1.0e-10)
         }
     }
 
@@ -99,13 +98,13 @@ class AstronomyTests {
             1..3 to Season.SPRING, 4..6 to Season.SUMMER,
             7..9 to Season.AUTUMN, 10..12 to Season.WINTER
         ).forEach { (range, season) ->
-            range.forEach { assertThat(noLocationSeasons[it - 1]).isEqualTo(season) }
+            range.forEach { assertEquals(season, noLocationSeasons[it - 1]) }
         }
         val northernHemisphereSeason = (1..12).map {
             val kathmandu = Coordinates(27.7172, 85.324, 1_400.0)
             Season.fromDate(Jdn(PersianDate(1400, it, 28)).toGregorianCalendar().time, kathmandu)
         }
-        assertThat(noLocationSeasons).isEqualTo(northernHemisphereSeason)
+        assertEquals(northernHemisphereSeason, noLocationSeasons)
 
         val southernHemisphereSeasons = (1..12).map {
             val nirobi = Coordinates(-1.286389, 36.817222, 1_795.0)
@@ -115,7 +114,7 @@ class AstronomyTests {
             1..3 to Season.AUTUMN, 4..6 to Season.WINTER,
             7..9 to Season.SPRING, 10..12 to Season.SUMMER
         ).forEach { (range, season) ->
-            range.forEach { assertThat(southernHemisphereSeasons[it - 1]).isEqualTo(season) }
+            range.forEach { assertEquals(season, southernHemisphereSeasons[it - 1]) }
         }
     }
 
@@ -125,7 +124,7 @@ class AstronomyTests {
         listOf(
             seasons.marchEquinox to 1584676196290, seasons.juneSolstice to 1592689411284,
             seasons.septemberEquinox to 1600781459379, seasons.decemberSolstice to 1608544962334
-        ).map { (it, time) -> assertThat(it.toMillisecondsSince1970()).isEqualTo(time) }
+        ).map { (it, time) -> assertEquals(time, it.toMillisecondsSince1970()) }
     }
 
     @Test
@@ -134,15 +133,17 @@ class AstronomyTests {
 //            it.clear()
 //            it.set(2021, Calendar.JANUARY, 10, 4, 0, 0)
 //        }
-//        assertThat(
+//        assertEquals(
+//            -0.9376037778203803,
 //            lunarSunlitTilt(
 //                // Equatorial(19.451750, -21.930057, 0.983402),
 //                doubleArrayOf(0.338427, -0.847146, -0.367276),
 //                Horizontal(187.958846, 40.777947),
 //                time,
 //                Coordinates(27.0, 85.0, .0)
-//            )
-//        ).isWithin(1.0e-10).of(-0.9376037778203803)
+//            ),
+//            1.0e-10
+//        )
     }
 
     @Test
@@ -212,7 +213,7 @@ class AstronomyTests {
         assertAll((1380..1400).mapIndexed { i, year ->
             val time = seasons(CivilDate(PersianDate(year, 1, 1)).year).marchEquinox
             val result = calculateAscendant(35.68, 51.42, time);
-            { assertEquals(expected[i], result, .1, "$year") }
+            { assertEquals(expected[i], result, 1.0e-5, "$year") }
         })
         // Smoke test
         (1300..1500).forEach { year ->
@@ -235,7 +236,7 @@ class AstronomyTests {
         println((1380..1400).mapIndexed { i, year ->
             val time = seasons(CivilDate(PersianDate(year, 1, 1)).year).marchEquinox
             val result = calculateMidheaven(51.42, time);
-            { assertEquals(expected[i], result, .1, "$year") }
+            { assertEquals(expected[i], result, 1.0e-5, "$year") }
         })
         // Smoke test
         (1300..1500).forEach { year ->
--- PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EarthPositionTests.kt
+++ PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EarthPositionTests.kt
@@ -1,8 +1,8 @@
 package com.byagowi.persiancalendar
 
 import com.byagowi.persiancalendar.entities.EarthPosition
-import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class EarthPositionTests {
 
@@ -14,20 +14,20 @@ class EarthPositionTests {
         // https://www.movable-type.co.uk/scripts/latlong.html
         val points = cambridge.intermediatePoints(paris, 4).toList()
         // include start and end and three points, not the best API yet
-        assertThat(points.size).isEqualTo(5)
-        assertThat(points[1].latitude).isWithin(1.0e-4).of(51.3721)
-        assertThat(points[1].longitude).isWithin(1.0e-4).of(.7073)
-        assertThat(points[2].latitude).isWithin(1.0e-4).of(50.5363)
-        assertThat(points[2].longitude).isWithin(1.0e-4).of(1.2746)
+        assertEquals(5, points.size)
+        assertEquals(51.3721, points[1].latitude, 1.0e-4)
+        assertEquals(.7073, points[1].longitude, 1.0e-4)
+        assertEquals(50.5363, points[2].latitude, 1.0e-4)
+        assertEquals(1.2746, points[2].longitude, 1.0e-4)
     }
 
     @Test
     fun rectangularBoundsOfRadius() {
         // https://gist.github.com/ebraminio/f20acf3bf605066d069bf313f52e2b68
         val (from, to) = EarthPosition(51.0217, 35.67719).rectangularBoundsOfRadius(.2)
-        assertThat(from.latitude).isWithin(1.0e-7).of(51.01990333083999)
-        assertThat(from.longitude).isWithin(1.0e-7).of(35.674333728359954)
-        assertThat(to.latitude).isWithin(1.0e-7).of(51.023496669160025)
-        assertThat(to.longitude).isWithin(1.0e-7).of(35.68004627164005)
+        assertEquals(51.01990333083999, from.latitude, 1.0e-7)
+        assertEquals(35.674333728359954, from.longitude, 1.0e-7)
+        assertEquals(51.023496669160025, to.latitude, 1.0e-7)
+        assertEquals(35.68004627164005, to.longitude, 1.0e-7)
     }
 }
