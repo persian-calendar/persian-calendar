d594d0378608c0256909bdb4fa44bd96b3e146be
Ebrahim Byagowi <ebrahim@gnu.org>
2025-07-22 00:35:46 +0330

    astronomy: Tweak houses style

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/astronomy/Horoscopes.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/astronomy/Horoscopes.kt
@@ -282,22 +282,19 @@ private fun AscendantZodiac(time: Time, coordinates: Coordinates, isYearEquinox:
     val houses = houses(coordinates.latitude, coordinates.longitude, time)
     val ascendantZodiac = Zodiac.fromTropical(houses[0])
     val resources = LocalContext.current.resources
-    val language by language.collectAsState()
     EasternHoroscopePattern { i ->
         val zodiac = Zodiac.entries[(i + ascendantZodiac.ordinal) % 12]
         zodiac.emoji + "\n" + zodiac.format(
             resources,
             withEmoji = false,
             short = true,
-        ) + " " + run {
+        ) + run {
             val houseZodiac = Zodiac.fromTropical(houses[i])
-            val value = formatAngle(houses[i] % 30)
-            if (houseZodiac == zodiac) value else language.inParentheses.format(
-                value,
-                houseZodiac.format(resources, withEmoji = false, short = true)
-            ).replace(" ", NBSP)
-        } + bodiesZodiac[zodiac]?.joinToString("\n") { (body, longitude) ->
-            resources.getString(body.titleStringId) + " " + formatAngle(longitude % 30)
+            if (zodiac != houseZodiac) "/" + houseZodiac.format(
+                resources, withEmoji = false, short = true
+            ) else ""
+        } + NBSP + formatAngle(houses[i] % 30) + bodiesZodiac[zodiac]?.joinToString("\n") { (body, longitude) ->
+            resources.getString(body.titleStringId) + NBSP + formatAngle(longitude % 30)
         }?.let { "\n" + it }.orEmpty()
     }
 }
