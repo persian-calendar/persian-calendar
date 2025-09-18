7af2b916f0a897e9e0613ebee625d5f2d5e36b00
Ebrahim Byagowi <ebrahim@gnu.org>
2025-09-18 14:43:44 +0330

    wear: Migrate from glance for tiles to protolayout material3

--- wear/src/main/kotlin/com/byagowi/persiancalendar/MainTileService.kt
+++ wear/src/main/kotlin/com/byagowi/persiancalendar/MainTileService.kt
@@ -1,93 +1,105 @@
 package com.byagowi.persiancalendar
 
-import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.glance.GlanceComposable
-import androidx.glance.GlanceModifier
-import androidx.glance.Image
-import androidx.glance.ImageProvider
-import androidx.glance.action.actionStartActivity
-import androidx.glance.action.clickable
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
-import androidx.glance.layout.Column
-import androidx.glance.layout.padding
-import androidx.glance.layout.size
-import androidx.glance.text.FontWeight
-import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
-import androidx.glance.wear.tiles.GlanceTileService
+import android.content.Intent
+import androidx.wear.protolayout.ActionBuilders.launchAction
+import androidx.wear.protolayout.DimensionBuilders.expand
+import androidx.wear.protolayout.DimensionBuilders.wrap
+import androidx.wear.protolayout.LayoutElementBuilders
+import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.protolayout.TimelineBuilders
+import androidx.wear.protolayout.material3.Typography
+import androidx.wear.protolayout.material3.materialScope
+import androidx.wear.protolayout.material3.primaryLayout
+import androidx.wear.protolayout.material3.text
+import androidx.wear.protolayout.material3.textEdgeButton
+import androidx.wear.protolayout.modifiers.clickable
+import androidx.wear.protolayout.types.layoutString
 import androidx.wear.tiles.EventBuilders
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.TileBuilders
+import androidx.wear.tiles.TileService
 import com.byagowi.persiancalendar.ui.MainActivity
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
 
-class MainTileService : GlanceTileService() {
-
-    override fun onDestroy() {
-        runCatching { super.onDestroy() }.onFailure { Log.e("", "", it) }
+class MainTileService : TileService() {
+    override fun onRecentInteractionEventsAsync(
+        events: MutableList<EventBuilders.TileInteractionEvent?>
+    ): ListenableFuture<Void?> {
+        if (events.any { it?.eventType == EventBuilders.TileInteractionEvent.ENTER }) {
+            getUpdater(this).requestUpdate(MainTileService::class.java)
+        }
+        return super.onRecentInteractionEventsAsync(events)
     }
 
-    override fun onTileEnterEvent(requestParams: EventBuilders.TileEnterEvent) {
-        super.onTileEnterEvent(requestParams)
-        getUpdater(this).requestUpdate(MainTileService::class.java)
-    }
-
-    @Composable
-    @GlanceComposable
-    override fun Content() {
-        Box(contentAlignment = Alignment.Center) {
-            // LocalConfiguration doesn't work here
-            val screenHeightDp = resources.configuration.screenHeightDp
-            val localeUtils = LocaleUtils()
-            val todayEntries = run {
-                val preferences = runBlocking { dataStore.data.firstOrNull() }
-                val enabledEvents = preferences?.get(enabledEventsKey) ?: emptySet()
-                generateEntries(localeUtils, Jdn.today(), enabledEvents, 1, false)
-            }
-            Text(
-                todayEntries[0].title,
-                GlanceModifier.padding(bottom = (screenHeightDp / 1.7).dp),
-                style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
-            )
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    private fun tileLayout(requestParams: RequestBuilders.TileRequest) = materialScope(
+        applicationContext,
+        deviceConfiguration = requestParams.deviceConfiguration,
+    ) {
+        val localeUtils = LocaleUtils()
+        val todayEntries = run {
+            val preferences = runBlocking { dataStore.data.firstOrNull() }
+            val enabledEvents = preferences?.get(enabledEventsKey) ?: emptySet()
+            generateEntries(localeUtils, Jdn.today(), enabledEvents, 1, false)
+        }
+        primaryLayout(
+            titleSlot = {
+                text(
+                    text = todayEntries[0].title.layoutString,
+                    typography = Typography.TITLE_SMALL,
+                )
+            },
+            mainSlot = {
+                val column = LayoutElementBuilders.Column.Builder()
+                    .setWidth(expand())
+                    .setHeight(wrap())
+                    .setHorizontalAlignment(LayoutElementBuilders.HORIZONTAL_ALIGN_CENTER)
                 todayEntries.drop(1).take(if (todayEntries.size > 4) 2 else 3).forEach {
-                    Text(
-                        it.title,
-                        modifier = GlanceModifier.padding(vertical = 2.dp, horizontal = 8.dp),
-                        style = TextStyle(
-                            fontSize = 16.sp,
-                            color = ResourceColorProvider(
-                                if (it.type == EntryType.Holiday) R.color.tile_holidays
-                                else R.color.tile_default_color
-                            ),
-                        ),
-                        maxLines = 1,
+                    column.addContent(
+                        if (it.type == EntryType.Holiday) {
+                            text(it.title.layoutString, color = colorScheme.primary)
+                        } else text(it.title.layoutString)
                     )
                 }
-                if (todayEntries.size > 4) Text("…", style = TextStyle(fontSize = 16.sp))
+                if (todayEntries.size > 4) column.addContent(text("…".layoutString))
+                column.build()
+            },
+            bottomSlot = {
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                ).component?.let {
+                    textEdgeButton(
+                        onClick = clickable(launchAction(it))
+                    ) { text("تقویم".layoutString, typography = Typography.BODY_SMALL) }
+                } ?: text("".layoutString)
             }
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = GlanceModifier.padding(top = (screenHeightDp / 1.35).dp),
-            ) {
-                Image(
-                    provider = ImageProvider(R.drawable.button),
-                    contentDescription = "تقویم",
-                    modifier = GlanceModifier
-                        .size(56.dp, 32.dp)
-                        .clickable(actionStartActivity<MainActivity>())
+        )
+    }
+
+    override fun onTileRequest(requestParams: RequestBuilders.TileRequest): ListenableFuture<TileBuilders.Tile> {
+        return Futures.immediateFuture(
+            TileBuilders.Tile.Builder()
+                .setResourcesVersion(RESOURCES_VERSION)
+                .setTileTimeline(
+                    TimelineBuilders.Timeline.fromLayoutElement(tileLayout(requestParams))
                 )
-                Text(
-                    "تقویم",
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        color = ResourceColorProvider(R.color.tile_on_button_color)
-                    )
-                )
-            }
-        }
+                .build()
+        )
+    }
+
+    override fun onTileResourcesRequest(requestParams: RequestBuilders.ResourcesRequest)
+            : ListenableFuture<ResourceBuilders.Resources> {
+        return Futures.immediateFuture(
+            ResourceBuilders.Resources.Builder()
+                .setVersion(RESOURCES_VERSION)
+                .build()
+        )
+    }
+
+    companion object {
+        private const val RESOURCES_VERSION = "1"
     }
 }
