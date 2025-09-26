d4bed8b580bf3a3097cc3ec9ad9e7054902a4106
Ebrahim Byagowi <ebrahim@gnu.org>
2025-09-26 11:07:04 +0330

    a11y: Improve language select dialog

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguageDialog.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/interfacecalendar/LanguageDialog.kt
@@ -1,6 +1,5 @@
 package com.byagowi.persiancalendar.ui.settings.interfacecalendar
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.byagowi.persiancalendar.AFGHANISTAN_TIMEZONE_ID
@@ -57,7 +58,7 @@ fun LanguageDialog(onDismissRequest: () -> Unit) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(SettingsItemHeight.dp)
-                    .clickable {
+                    .selectable(item == currentLanguage, role = Role.RadioButton) {
                         if (item != currentLanguage) context.preferences.saveLanguage(item)
                         onDismissRequest()
                     }
