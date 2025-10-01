2fc9e81af708696f2c515673d596b7d82c6ac586
Ebrahim Byagowi <ebrahim@gnu.org>
2025-10-01 17:41:25 +0330

    Minor simplification in ScreenSurface implementation

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/common/ScreenSurface.kt
@@ -3,21 +3,22 @@ package com.byagowi.persiancalendar.ui.common
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import com.byagowi.persiancalendar.SHARED_CONTENT_KEY_CARD
@@ -40,19 +41,22 @@ fun SharedTransitionScope.ScreenSurface(
     drawBehindSurface: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val outlineColor = MaterialTheme.colorScheme.outline
-    val needsScreenSurfaceDragHandle = mayNeedDragHandleToDivide && needsScreenSurfaceDragHandle()
-    val surfaceColor by animateColor(MaterialTheme.colorScheme.surface)
     Layout(content = {
         // Parent
-        Surface(
-            shape = shape,
-            color = surfaceColor,
-            modifier = (if (disableSharedContent) Modifier else Modifier.sharedElement(
-                rememberSharedContentState(SHARED_CONTENT_KEY_CARD),
-                animatedVisibilityScope = animatedContentScope,
-                boundsTransform = appBoundsTransform,
-            )).drawBehind {
+        run {
+            val outlineColor = MaterialTheme.colorScheme.outline
+            val needsScreenSurfaceDragHandle =
+                mayNeedDragHandleToDivide && needsScreenSurfaceDragHandle()
+            val surfaceColor by animateColor(MaterialTheme.colorScheme.surface)
+            val density = LocalDensity.current
+            Canvas(
+                modifier = if (disableSharedContent) Modifier else Modifier.sharedElement(
+                    rememberSharedContentState(SHARED_CONTENT_KEY_CARD),
+                    animatedVisibilityScope = animatedContentScope,
+                    boundsTransform = appBoundsTransform,
+                )
+            ) {
+                val size = this.size
                 if (needsScreenSurfaceDragHandle) drawRoundRect(
                     outlineColor,
                     Offset(size.width / 2f - 24.dp.toPx(), -6.dp.toPx()),
@@ -60,12 +64,15 @@ fun SharedTransitionScope.ScreenSurface(
                     CornerRadius(4.dp.toPx()),
                     alpha = .375f,
                 )
+                run {
+                    val outline = shape.createOutline(size, this.layoutDirection, density)
+                    drawOutline(outline, surfaceColor)
+                }
                 // Ugly but in order to support overshoot animations let's draw surface color
                 // under the content
-                if (drawBehindSurface)
-                    drawRect(surfaceColor, Offset(0f, size.height / 2), size)
-            },
-        ) {}
+                if (drawBehindSurface) drawRect(surfaceColor, Offset(0f, size.height / 2), size)
+            }
+        }
         // Content
         Box(
             (if (disableSharedContent) Modifier else Modifier.sharedBounds(
