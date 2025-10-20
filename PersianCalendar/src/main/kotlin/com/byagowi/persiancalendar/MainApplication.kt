276f6cb0d38ec9101774d35fae0cd2cc60ac5b23
Ebrahim Byagowi <ebrahim@gnu.org>
2025-10-21 00:48:09 +0330

    Move preference following code from main app to main activity

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
@@ -1,67 +1,17 @@
 package com.byagowi.persiancalendar
 
 import android.app.Application
-import android.content.Intent
-import android.content.SharedPreferences
 import androidx.compose.runtime.Composer
 import androidx.compose.runtime.ExperimentalComposeRuntimeApi
-import androidx.core.content.edit
-import com.byagowi.persiancalendar.entities.Jdn
-import com.byagowi.persiancalendar.global.configureCalendarsAndLoadEvents
 import com.byagowi.persiancalendar.global.initGlobal
-import com.byagowi.persiancalendar.global.loadLanguageResources
-import com.byagowi.persiancalendar.global.updateStoredPreference
-import com.byagowi.persiancalendar.service.ApplicationService
-import com.byagowi.persiancalendar.utils.applyAppLanguage
-import com.byagowi.persiancalendar.utils.preferences
-import com.byagowi.persiancalendar.utils.putJdn
-import com.byagowi.persiancalendar.utils.startWorker
 import com.byagowi.persiancalendar.utils.update
 
-class MainApplication : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
+class MainApplication : Application() {
     @OptIn(ExperimentalComposeRuntimeApi::class)
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEVELOPMENT) Composer.setDiagnosticStackTraceEnabled(BuildConfig.DEBUG)
         initGlobal(applicationContext) // mostly used for things should be provided in locale level
-        preferences.registerOnSharedPreferenceChangeListener(this)
-        update(this, true)
-    }
-
-    override fun onSharedPreferenceChanged(preferences: SharedPreferences?, key: String?) {
-        when (key) {
-            PREF_LAST_APP_VISIT_VERSION -> return // nothing needs to be updated
-            EXPANDED_TIME_STATE_KEY -> return // nothing needs to be updated
-            LAST_PLAYED_ATHAN_JDN, LAST_PLAYED_ATHAN_KEY -> return // nothing needs to be updated
-            LAST_CHOSEN_TAB_KEY -> return // don't run the expensive update and etc on tab changes
-            PREF_ISLAMIC_OFFSET -> {
-                this.preferences.edit { putJdn(PREF_ISLAMIC_OFFSET_SET_DATE, Jdn.today()) }
-            }
-
-            PREF_PRAY_TIME_METHOD -> this.preferences.edit { remove(PREF_MIDNIGHT_METHOD) }
-            PREF_NOTIFY_DATE -> {
-                if (!this.preferences.getBoolean(PREF_NOTIFY_DATE, DEFAULT_NOTIFY_DATE)) {
-                    stopService(Intent(this, ApplicationService::class.java))
-                    startWorker(applicationContext)
-                }
-            }
-        }
-
-        configureCalendarsAndLoadEvents(this)
-        updateStoredPreference(this)
-
-        if (key == PREF_APP_LANGUAGE) {
-            applyAppLanguage(this)
-            loadLanguageResources(this.resources)
-        }
-
-        if (key == PREF_EASTERN_GREGORIAN_ARABIC_MONTHS ||
-            key == PREF_ENGLISH_GREGORIAN_PERSIAN_MONTHS ||
-            key == PREF_AZERI_ALTERNATIVE_PERSIAN_MONTHS
-        ) {
-            loadLanguageResources(this.resources)
-        }
-
         update(this, true)
     }
 }
--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/MainActivity.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.byagowi.persiancalendar.ui
 
 import android.content.ContentUris
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Build
@@ -15,12 +16,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalView
+import androidx.core.content.edit
 import androidx.core.content.getSystemService
+import com.byagowi.persiancalendar.DEFAULT_NOTIFY_DATE
+import com.byagowi.persiancalendar.EXPANDED_TIME_STATE_KEY
+import com.byagowi.persiancalendar.LAST_CHOSEN_TAB_KEY
+import com.byagowi.persiancalendar.LAST_PLAYED_ATHAN_JDN
+import com.byagowi.persiancalendar.LAST_PLAYED_ATHAN_KEY
+import com.byagowi.persiancalendar.PREF_APP_LANGUAGE
+import com.byagowi.persiancalendar.PREF_AZERI_ALTERNATIVE_PERSIAN_MONTHS
+import com.byagowi.persiancalendar.PREF_EASTERN_GREGORIAN_ARABIC_MONTHS
+import com.byagowi.persiancalendar.PREF_ENGLISH_GREGORIAN_PERSIAN_MONTHS
+import com.byagowi.persiancalendar.PREF_ISLAMIC_OFFSET
+import com.byagowi.persiancalendar.PREF_ISLAMIC_OFFSET_SET_DATE
+import com.byagowi.persiancalendar.PREF_LAST_APP_VISIT_VERSION
+import com.byagowi.persiancalendar.PREF_MIDNIGHT_METHOD
+import com.byagowi.persiancalendar.PREF_NOTIFY_DATE
+import com.byagowi.persiancalendar.PREF_PRAY_TIME_METHOD
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.entities.Jdn
+import com.byagowi.persiancalendar.global.configureCalendarsAndLoadEvents
 import com.byagowi.persiancalendar.global.initGlobal
 import com.byagowi.persiancalendar.global.language
+import com.byagowi.persiancalendar.global.loadLanguageResources
 import com.byagowi.persiancalendar.global.updateAccessibilityFlows
+import com.byagowi.persiancalendar.global.updateStoredPreference
+import com.byagowi.persiancalendar.service.ApplicationService
 import com.byagowi.persiancalendar.ui.theme.AppTheme
 import com.byagowi.persiancalendar.ui.utils.isLight
 import com.byagowi.persiancalendar.utils.applyAppLanguage
@@ -28,6 +49,8 @@ import com.byagowi.persiancalendar.utils.applyLanguageToConfiguration
 import com.byagowi.persiancalendar.utils.eventKey
 import com.byagowi.persiancalendar.utils.jdnActionKey
 import com.byagowi.persiancalendar.utils.logException
+import com.byagowi.persiancalendar.utils.preferences
+import com.byagowi.persiancalendar.utils.putJdn
 import com.byagowi.persiancalendar.utils.readAndStoreDeviceCalendarEventsOfTheDay
 import com.byagowi.persiancalendar.utils.startWorker
 import com.byagowi.persiancalendar.utils.toCivilDate
@@ -38,7 +61,7 @@ import kotlinx.coroutines.flow.StateFlow
 import java.util.Date
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Just to make sure we have an initial transparent system bars
         // System bars are tweaked later with project's with real values
@@ -109,6 +132,45 @@ class MainActivity : ComponentActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             window.isNavigationBarContrastEnforced = false
         }
+
+        preferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(preferences: SharedPreferences?, key: String?) {
+        when (key) {
+            PREF_LAST_APP_VISIT_VERSION -> return // nothing needs to be updated
+            EXPANDED_TIME_STATE_KEY -> return // nothing needs to be updated
+            LAST_PLAYED_ATHAN_JDN, LAST_PLAYED_ATHAN_KEY -> return // nothing needs to be updated
+            LAST_CHOSEN_TAB_KEY -> return // don't run the expensive update and etc on tab changes
+            PREF_ISLAMIC_OFFSET -> {
+                this.preferences.edit { putJdn(PREF_ISLAMIC_OFFSET_SET_DATE, Jdn.today()) }
+            }
+
+            PREF_PRAY_TIME_METHOD -> this.preferences.edit { remove(PREF_MIDNIGHT_METHOD) }
+            PREF_NOTIFY_DATE -> {
+                if (!this.preferences.getBoolean(PREF_NOTIFY_DATE, DEFAULT_NOTIFY_DATE)) {
+                    stopService(Intent(this, ApplicationService::class.java))
+                    startWorker(applicationContext)
+                }
+            }
+        }
+
+        configureCalendarsAndLoadEvents(this)
+        updateStoredPreference(this)
+
+        if (key == PREF_APP_LANGUAGE) {
+            applyAppLanguage(this)
+            loadLanguageResources(this.resources)
+        }
+
+        if (key == PREF_EASTERN_GREGORIAN_ARABIC_MONTHS ||
+            key == PREF_ENGLISH_GREGORIAN_PERSIAN_MONTHS ||
+            key == PREF_AZERI_ALTERNATIVE_PERSIAN_MONTHS
+        ) {
+            loadLanguageResources(this.resources)
+        }
+
+        update(this, true)
     }
 
     private fun applyEdgeToEdge(isBackgroundColorLight: Boolean, isSurfaceColorLight: Boolean) {
