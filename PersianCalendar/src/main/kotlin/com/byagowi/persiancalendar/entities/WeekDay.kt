3bc7085621faf48a9c0fe87a04305d1b266e68e8
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-01 11:37:42 +0330

    Make WeekDay.ruledBy lazy load

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/WeekDay.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/WeekDay.kt
@@ -7,16 +7,27 @@ import io.github.cosinekitty.astronomy.Body
 // Order of week day is a legacy for this code base
 // *DIFFERS* from ISO-8601 standard, 1 (Monday) to 7 (Sunday), unfortunately
 enum class WeekDay(
-    // A concept in planetary hours, see PlanetaryHours.kt, note how similar they are btw
-    val ruledBy: Body,
     @param:StringRes val titleId: Int,
     @param:StringRes val shortTitleId: Int,
 ) {
-    SATURDAY(Body.Saturn, R.string.saturday, R.string.saturday_short),
-    SUNDAY(Body.Sun, R.string.sunday, R.string.sunday_short),
-    MONDAY(Body.Moon, R.string.monday, R.string.monday_short),
-    TUESDAY(Body.Mars, R.string.tuesday, R.string.tuesday_short),
-    WEDNESDAY(Body.Mercury, R.string.wednesday, R.string.wednesday_short),
-    THURSDAY(Body.Jupiter, R.string.thursday, R.string.thursday_short),
-    FRIDAY(Body.Venus, R.string.friday, R.string.friday_short)
+    SATURDAY(R.string.saturday, R.string.saturday_short),
+    SUNDAY(R.string.sunday, R.string.sunday_short),
+    MONDAY(R.string.monday, R.string.monday_short),
+    TUESDAY(R.string.tuesday, R.string.tuesday_short),
+    WEDNESDAY(R.string.wednesday, R.string.wednesday_short),
+    THURSDAY(R.string.thursday, R.string.thursday_short),
+    FRIDAY(R.string.friday, R.string.friday_short);
+
+    // A concept in planetary hours, see PlanetaryHours.kt, note how similar they are btw
+    // Not defined in the entry so it can be lazy loaded as previous issues we have with Body enum
+    val ruledBy
+        get() = when (this) {
+            SATURDAY -> Body.Saturn
+            SUNDAY -> Body.Sun
+            MONDAY -> Body.Moon
+            TUESDAY -> Body.Mars
+            WEDNESDAY -> Body.Mercury
+            THURSDAY -> Body.Jupiter
+            FRIDAY -> Body.Venus
+        }
 }
