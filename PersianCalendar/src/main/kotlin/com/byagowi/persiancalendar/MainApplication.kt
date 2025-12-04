2dc7e3da2d132d10a27da03b59b1564ae72d987e
Ebrahim Byagowi <ebrahim@gnu.org>
2025-12-04 11:02:53 +0330

    Update dependencies

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
@@ -3,6 +3,7 @@ package com.byagowi.persiancalendar
 import android.app.Application
 import androidx.compose.runtime.Composer
 import androidx.compose.runtime.ExperimentalComposeRuntimeApi
+import androidx.compose.runtime.tooling.ComposeStackTraceMode
 import com.byagowi.persiancalendar.global.initGlobal
 import com.byagowi.persiancalendar.utils.update
 
@@ -10,7 +11,9 @@ class MainApplication : Application() {
     @OptIn(ExperimentalComposeRuntimeApi::class)
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEVELOPMENT) Composer.setDiagnosticStackTraceEnabled(BuildConfig.DEBUG)
+        if (BuildConfig.DEVELOPMENT) {
+            Composer.setDiagnosticStackTraceMode(ComposeStackTraceMode.SourceInformation)
+        }
         initGlobal(applicationContext) // mostly used for things should be provided in locale level
         update(this, true)
     }
