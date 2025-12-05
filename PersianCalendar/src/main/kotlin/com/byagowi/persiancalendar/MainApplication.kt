e05e5cf7d99abeef2786c5da8eac2c429d880653
Ebrahim Byagowi <ebrahim@gnu.org>
2025-12-05 11:31:02 +0330

    Remove compose trace mode settings settings
    Let's make the development build more similar to the production.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/MainApplication.kt
@@ -1,19 +1,12 @@
 package com.byagowi.persiancalendar
 
 import android.app.Application
-import androidx.compose.runtime.Composer
-import androidx.compose.runtime.ExperimentalComposeRuntimeApi
-import androidx.compose.runtime.tooling.ComposeStackTraceMode
 import com.byagowi.persiancalendar.global.initGlobal
 import com.byagowi.persiancalendar.utils.update
 
 class MainApplication : Application() {
-    @OptIn(ExperimentalComposeRuntimeApi::class)
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEVELOPMENT) {
-            Composer.setDiagnosticStackTraceMode(ComposeStackTraceMode.SourceInformation)
-        }
         initGlobal(applicationContext) // mostly used for things should be provided in locale level
         update(this, true)
     }
