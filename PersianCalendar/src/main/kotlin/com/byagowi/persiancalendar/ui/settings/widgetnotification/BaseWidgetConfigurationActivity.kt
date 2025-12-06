2a056e4487f025cf98ee775dde141956294d993f
Ebrahim Byagowi <ebrahim@gnu.org>
2025-12-06 21:23:54 +0330

    Clarify appWidgetId implementation

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/widgetnotification/BaseWidgetConfigurationActivity.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/settings/widgetnotification/BaseWidgetConfigurationActivity.kt
@@ -17,11 +17,25 @@ abstract class BaseWidgetConfigurationActivity : BaseConfigurationActivity(
         finish()
     }
 
+    @Suppress("KotlinConstantConditions")
     protected fun appWidgetId(): Int {
-        return intent?.extras?.getInt(
-            AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID
-        ) ?: intent?.action?.takeIf { it.startsWith(AppWidgetManager.EXTRA_APPWIDGET_ID) }
-            ?.replace(AppWidgetManager.EXTRA_APPWIDGET_ID, "")?.toIntOrNull()
-        ?: AppWidgetManager.INVALID_APPWIDGET_ID
+        val defaultValue = AppWidgetManager.INVALID_APPWIDGET_ID
+        val intent = intent ?: return defaultValue
+
+        run {
+            // first try to get it from intent's extras, most of the times it is here
+            val id = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, defaultValue)
+            if (id != defaultValue) return id
+        }
+
+        run {
+            // on age widget plain clicks, id is set as the action so next try that
+            val id = intent.action?.takeIf { it.startsWith(AppWidgetManager.EXTRA_APPWIDGET_ID) }
+                ?.replace(AppWidgetManager.EXTRA_APPWIDGET_ID, "")?.toIntOrNull()
+            if (id != null) return id
+        }
+
+        // Shouldn't happen but if everything fails, return invalid id
+        return defaultValue
     }
 }
