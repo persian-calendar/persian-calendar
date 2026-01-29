554a593d42b7a46002bac816b3d825aa20f11efa
Ebrahim Byagowi <ebrahim@gnu.org>
2026-01-30 01:46:52 +0330

    qr: Invoke Android path APIs directly
    As that won't need data clas initialization on every call.

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/converter/QrView.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/converter/QrView.kt
@@ -1,5 +1,6 @@
 package com.byagowi.persiancalendar.ui.converter
 
+import android.graphics.Path.Direction
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
@@ -14,11 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.graphics.applyCanvas
@@ -53,27 +53,21 @@ fun QrView(text: String, setShareAction: (() -> Unit) -> Unit) {
             }
         }
         path.rewind()
-        run {
-            val r = CornerRadius(cellSize * roundness * 2)
-            path.addRoundRect(
-                RoundRect(0f, 0f, cellSize * 7, cellSize * 7, r, r, r, r),
-                Path.Direction.Clockwise,
-            )
-        }
-        run {
-            val r = CornerRadius(cellSize * roundness * 1.5f)
-            path.addRoundRect(
-                RoundRect(cellSize, cellSize, cellSize * 6, cellSize * 6, r, r, r, r),
-                Path.Direction.CounterClockwise,
-            )
-        }
-        run {
-            val r = CornerRadius(cellSize * roundness)
-            path.addRoundRect(
-                RoundRect(cellSize * 2, cellSize * 2, cellSize * 5, cellSize * 5, r, r, r, r),
-                Path.Direction.Clockwise,
-            )
-        }
+        path.asAndroidPath().addRoundRect(
+            0f, 0f, cellSize * 7, cellSize * 7,
+            cellSize * roundness * 2, cellSize * roundness * 2,
+            Direction.CW,
+        )
+        path.asAndroidPath().addRoundRect(
+            cellSize, cellSize, cellSize * 6, cellSize * 6,
+            cellSize * roundness * 1.5f, cellSize * roundness * 1.5f,
+            Direction.CCW,
+        )
+        path.asAndroidPath().addRoundRect(
+            cellSize * 2, cellSize * 2, cellSize * 5, cellSize * 5,
+            cellSize * roundness, cellSize * roundness,
+            Direction.CW,
+        )
         canvas.drawPath(path, paint)
         val d = cellSize * (qr.size - 7)
         canvas.translate(0f, d)
