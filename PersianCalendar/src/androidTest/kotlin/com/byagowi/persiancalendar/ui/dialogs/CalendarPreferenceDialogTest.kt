977f36b7e98c59869332040493cfc3c652258514
Ebrahim <ebrahim@gnu.org>
2026-02-27 11:24:26 +0330

    Add a smoke test for CalendarPreferenceDialog

---
+++ PersianCalendar/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/dialogs/CalendarPreferenceDialogTest.kt
@@ -0,0 +1,43 @@
+package com.byagowi.persiancalendar.ui.dialogs
+
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.byagowi.persiancalendar.R
+import com.byagowi.persiancalendar.ui.settings.interfacecalendar.calendarsorder.CalendarPreferenceDialog
+import org.junit.Rule
+import org.junit.Test
+
+class CalendarPreferenceDialogTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun cancelButtonTest() {
+        var showDialog = true
+        var cancelString = ""
+        composeTestRule.setContent {
+            cancelString = stringResource(R.string.accept)
+            if (showDialog) CalendarPreferenceDialog { showDialog = false }
+        }
+        assert(showDialog)
+        composeTestRule.onNodeWithText(cancelString)
+            .assertHasClickAction()
+            .performClick()
+        assert(!showDialog)
+    }
+
+    @Test
+    fun saveRestoreTest() {
+        val restorationTester = StateRestorationTester(composeTestRule)
+
+//        restorationTester.setContent {
+//            CalendarPreferenceDialog {}
+//        }
+
+        restorationTester.emulateSavedInstanceStateRestore()
+    }
+}
