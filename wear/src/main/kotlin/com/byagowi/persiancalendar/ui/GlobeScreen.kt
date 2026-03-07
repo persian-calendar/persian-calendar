0f568f94c718469924a2f5edac350e6bf4c78219
Ebrahim <ebrahim@gnu.org>
2026-03-07 20:37:36 +0330

    wear: Add fling to globe

--- wear/src/main/kotlin/com/byagowi/persiancalendar/ui/GlobeScreen.kt
+++ wear/src/main/kotlin/com/byagowi/persiancalendar/ui/GlobeScreen.kt
@@ -4,14 +4,18 @@ import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.SplineBasedFloatDecayAnimationSpec
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDecay
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.calculatePan
+import androidx.compose.foundation.gestures.calculateZoom
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.aspectRatio
@@ -20,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,13 +34,19 @@ import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.addPathNodes
 import androidx.compose.ui.graphics.vector.toPath
+import androidx.compose.ui.input.pointer.PointerId
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.input.pointer.util.addPointerInputChange
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.util.fastAny
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ScreenScaffold
 import com.byagowi.persiancalendar.R
 import com.byagowi.persiancalendar.generated.globeRuntimeShader
+import kotlinx.coroutines.launch
 import kotlin.math.PI
 
 @Composable
@@ -86,20 +97,50 @@ fun GlobeScreen(modifier: Modifier = Modifier) {
             val width = this.maxWidth
             val height = this.maxHeight
             val density = LocalDensity.current
+            val coroutineScope = rememberCoroutineScope()
             Box(
                 Modifier
-                    .transformable(
-                        rememberTransformableState { zoomChange, panChange, _ ->
-                            zoom = (zoom * zoomChange).coerceAtLeast(.25f)
-                            if (x.floatValue.isNaN()) x.floatValue = rotation
-                            x.floatValue += panChange.x / with(density) { width.toPx() } * 4 / zoom
-                            y =
-                                (y + panChange.y / with(density) { height.toPx() } * -4 / zoom).coerceIn(
-                                    -PI.toFloat() / 3,
-                                    PI.toFloat() / 3,
-                                )
-                        },
-                    )
+                    .pointerInput(Unit) {
+                        var lastPointerId: PointerId? = null
+                        awaitEachGesture {
+                            val down = awaitFirstDown(requireUnconsumed = false)
+                            lastPointerId = down.id
+                            val tracker = VelocityTracker()
+                            do {
+                                val event = awaitPointerEvent()
+                                val zoomChange = event.calculateZoom()
+                                if (zoomChange != 1f) {
+                                    zoom = (zoom * zoomChange).coerceIn(.5f, 3f)
+                                } else {
+                                    event.changes.forEach {
+                                        tracker.addPointerInputChange(it)
+                                        it.consume()
+                                    }
+                                    val panChange = event.calculatePan()
+                                    if (x.floatValue.isNaN()) x.floatValue = rotation
+                                    x.floatValue += panChange.x / with(density) { width.toPx() } * 4 / zoom
+                                    y =
+                                        (y + panChange.y / with(density) { height.toPx() } * -4 / zoom).coerceIn(
+                                            -PI.toFloat() / 3,
+                                            PI.toFloat() / 3,
+                                        )
+                                }
+                            } while (event.changes.fastAny { it.pressed })
+                            val velocity = tracker.calculateVelocity().x
+                            tracker.resetTracking()
+                            coroutineScope.launch {
+                                animateDecay(
+                                    initialValue = 0f,
+                                    initialVelocity = velocity,
+                                    animationSpec = SplineBasedFloatDecayAnimationSpec(density),
+                                ) { _, velocity ->
+                                    if (down.id == lastPointerId && velocity.isFinite()) {
+                                        x.floatValue += velocity / 10 / with(density) { width.toPx() } / zoom
+                                    }
+                                }
+                            }
+                        }
+                    }
                     .fillMaxSize(.7f),
             )
         }
