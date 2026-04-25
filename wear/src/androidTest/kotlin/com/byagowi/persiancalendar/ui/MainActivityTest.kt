b1c1ed126bf5da6527fe7f2c79e86ef2c637df3e
Ebrahim <ebrahim@gnu.org>
2026-04-25 15:49:34 +0330

    Minor build fix

--- wear/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/MainActivityTest.kt
+++ wear/src/androidTest/kotlin/com/byagowi/persiancalendar/ui/MainActivityTest.kt
@@ -2,7 +2,7 @@ package com.byagowi.persiancalendar.ui
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
