adc2265589f9941f68d5f6b233aa35e48e051286
Ebrahim <ebrahim@gnu.org>
2026-06-06 00:49:14 +0330

    Enable wikipedia entry at least for historical events

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/EventsTab.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/calendar/EventsTab.kt
@@ -317,8 +317,12 @@ private fun DayEventContent(
                     TooltipAnchorPosition.Above,
                 ),
                 tooltip = {
-                    val wikipediaEntry: String? =
-                        if (BuildConfig.DEVELOPMENT) event.metadata["wikipedia"] else null
+                    val wikipediaEntry: String? = if (when {
+                            BuildConfig.DEVELOPMENT -> true
+                            event.source == EventSource.AncientIran -> true
+                            else -> false
+                        }
+                    ) event.metadata["wikipedia"] else null
                     val text = listOfNotNull(
                         when {
                             event is CalendarEvent.DeviceCalendarEvent -> "این رویداد شخصی از تقویم دستگاه می‌آید، تقویمی که پیش از این برنامه به‌صورت پیش‌فرض نصب بوده است."
