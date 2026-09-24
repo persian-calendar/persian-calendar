8c80703eea2ac11cb174aacf76870d579c43e49f
Ebrahim <ebrahim@gnu.org>
2026-09-25 00:29:16 +0330

    Remove foojay
    F-Droid removes it while building the project so let's reduce their trouble

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
