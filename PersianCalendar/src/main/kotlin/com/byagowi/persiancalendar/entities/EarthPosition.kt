477039c40ae324303c35cafb4ead629ca7357b87
Ebrahim <ebrahim@gnu.org>
2026-09-26 02:15:00 +0330

    Revert use of android.location.Location.distanceBetween
    To ease KMP use
    
    Partially reverts https://github.com/persian-calendar/persian-calendar/commit/d9211ec829245570af46eaf0103cd9db47e228dc

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/EarthPosition.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/entities/EarthPosition.kt
@@ -1,11 +1,11 @@
 package com.byagowi.persiancalendar.entities
 
-import android.location.Location
 import android.os.Parcelable
 import com.byagowi.persiancalendar.global.language
 import io.github.persiancalendar.praytimes.Coordinates
 import kotlinx.parcelize.Parcelize
 import kotlin.math.PI
+import kotlin.math.acos
 import kotlin.math.asin
 import kotlin.math.atan2
 import kotlin.math.cos
@@ -17,16 +17,33 @@ import kotlin.math.sqrt
 
 @Parcelize
 data class EarthPosition(val latitude: Double, val longitude: Double) : Parcelable {
-    class EarthHeading(metres: Float, val heading: Float) {
+    class EarthHeading(val metres: Float, val heading: Float) {
         val km = language.formatKm((metres / 1000).roundToLong())
     }
 
     fun toCoordinates(): Coordinates = Coordinates(latitude, longitude, .0)
 
     fun toEarthHeading(target: EarthPosition): EarthHeading {
-        val result = FloatArray(3)
-        Location.distanceBetween(latitude, longitude, target.latitude, target.longitude, result)
-        return EarthHeading(result[0], result[1])
+        // great circle formula from:
+        // https://web.archive.org/web/20161209044600/http://williams.best.vwh.net/avform.htm
+        val lat1 = Math.toRadians(latitude) //7155849931833333333e-19 0.71
+        val lat2 = Math.toRadians(target.latitude) //3737913479489224943e-19 0.373
+        val lon1 = Math.toRadians(-longitude) //-5055637064497558276 e-19 -0.505
+        val lon2 = Math.toRadians(-target.longitude) //-69493192920839161e-17  -0.69
+        val a = sin((lat1 - lat2) / 2)
+        val b = sin((lon1 - lon2) / 2)
+        // https://en.wikipedia.org/wiki/Haversine_formula
+        val d = 2 * asin(sqrt(a * a + cos(lat1) * cos(lat2) * b * b)) //3774840207564380360e-19
+        //d=2*asin(sqrt((sin((lat1-lat2)/2))^2 + cos(lat1)*cos(lat2)*(sin((lon1-lon2)/2))^2))
+        // double c=a*a+Math.cos(lat1)*Math.cos(lat2))*b*b
+        val tc1 = if (d > 0) {
+            //tc1=acos((sin(lat2)-sin(lat1)*cos(d))/(sin(d)*cos(lat1)))
+            val x =
+                acos((sin(lat2) - sin(lat1) * cos(d)) / (sin(d) * cos(lat1)))/*2646123918118404228e-18*/
+            if (sin(lon2 - lon1) < 0) x else 2 * PI - x
+        } else 0.0
+        //  tc1=2*pi-acos((sin(lat2)-sin(lat1)*cos(d))/(sin(d)*cos(lat1)))
+        return EarthHeading((d * 6371e3).toFloat(), (tc1 * 180 / PI).toFloat())
     }
 
     // Ported from https://www.movable-type.co.uk/scripts/latlong.html MIT License
--- PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EarthPositionTests.kt
+++ PersianCalendar/src/test/kotlin/com/byagowi/persiancalendar/EarthPositionTests.kt
@@ -10,6 +10,14 @@ class EarthPositionTests {
     private val paris = EarthPosition(48.857, 2.351)
 
     @Test
+    fun toEarthHeading() {
+        // https://www.movable-type.co.uk/scripts/latlong.html
+        val heading = cambridge.toEarthHeading(paris)
+        assertEquals(404.3e3f, heading.metres, 1.0e2f)
+        assertEquals(156.2f, heading.heading, 1.0e-1f)
+    }
+
+    @Test
     fun intermediatePoints() {
         // https://www.movable-type.co.uk/scripts/latlong.html
         val points = cambridge.intermediatePoints(paris, 4).toList()
