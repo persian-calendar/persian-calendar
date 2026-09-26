544608dea049f61cca65a501976df6bd445ea286
Ebrahim <ebrahim@gnu.org>
2026-09-26 12:56:22 +0330

    Use Compose only graphics in QrView

--- PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/converter/QrView.kt
+++ PersianCalendar/src/main/kotlin/com/byagowi/persiancalendar/ui/converter/QrView.kt
@@ -1,6 +1,5 @@
 package com.byagowi.persiancalendar.ui.converter
 
-import android.graphics.Path.Direction
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
@@ -19,11 +18,11 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,20 +82,26 @@ fun QrView(
         lines(vertical = false)
         lines(vertical = true)
         path.rewind()
-        path.asAndroidPath().addRoundRect(
-            0f, 0f, cellSize * 7, cellSize * 7,
-            cellSize * roundness * 2, cellSize * roundness * 2,
-            Direction.CW,
+        path.addRoundRect(
+            RoundRect(
+                0f, 0f, cellSize * 7, cellSize * 7,
+                cellSize * roundness * 2, cellSize * roundness * 2,
+            ),
+            Path.Direction.Clockwise,
         )
-        path.asAndroidPath().addRoundRect(
-            cellSize, cellSize, cellSize * 6, cellSize * 6,
-            cellSize * roundness * 1.5f, cellSize * roundness * 1.5f,
-            Direction.CCW,
+        path.addRoundRect(
+            RoundRect(
+                cellSize, cellSize, cellSize * 6, cellSize * 6,
+                cellSize * roundness * 1.5f, cellSize * roundness * 1.5f,
+            ),
+            Path.Direction.CounterClockwise,
         )
-        path.asAndroidPath().addRoundRect(
-            cellSize * 2, cellSize * 2, cellSize * 5, cellSize * 5,
-            cellSize * roundness, cellSize * roundness,
-            Direction.CW,
+        path.addRoundRect(
+            RoundRect(
+                cellSize * 2, cellSize * 2, cellSize * 5, cellSize * 5,
+                cellSize * roundness, cellSize * roundness,
+            ),
+            Path.Direction.Clockwise,
         )
         canvas.drawPath(path, paint)
         val d = cellSize * (qr.size - 7)
